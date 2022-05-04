c45a2336df5b58897d48c801246325a0756f308f
James-Mart <jmartx93@gmail.com>
2022-05-04 14:54:59 +0000

    benchmarking utils: add usage description

--- libraries/psibase/tester/include/psibase/testUtils.hpp
+++ libraries/psibase/tester/include/psibase/testUtils.hpp
@@ -9,6 +9,32 @@ namespace psibase
 {
    namespace benchmarking
    {
+      /* Usage
+         // Create the object that will collect all benchmark data
+         auto d = std::make_shared<BenchmarkData>();
+         
+         // Call actions that touch each of the contracts you're interested in without benchmarking
+         // The first time a test accesses a contract should be ignored
+         alice.action1();
+         alice.action2();
+
+         // Call actions to benchmark in a loop, creating a Benchmarker in the scope of each transaction
+         for (int i = 0; i < 30; ++i, d->nextColumn())
+         {
+            {
+               Benchmarker t(d);
+               alice.action1();
+            }
+            {
+               Benchmarker t(d);
+               alice.action2();
+            }
+         }
+
+         // Print the data collected by the BenchmarkData object
+         d->print();
+      */
+
       class BenchmarkData
       {
         public:
