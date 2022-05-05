e5c6d37032102005cac51a52209d3e62183a21ab
Steven Watanabe <steven@clarionos.com>
2022-05-05 12:12:16 -0400

    Split tests

---
+++ libraries/psidb/test/test_cursor.cpp
@@ -0,0 +1,150 @@
+#include <psidb/cursor.hpp>
+#include <psidb/database.hpp>
+
+#include "test_util.hpp"
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+TEST_CASE("lower_bound empty", "[lower_bound]")
+{
+   tmp_file file;
+
+   psidb::database db(file.native_handle(), 1024);
+   auto            trx    = db.start_transaction();
+   auto            cursor = trx.get_cursor();
+   cursor.lower_bound("a");
+   CHECK(!cursor.valid());
+}
+
+TEST_CASE("lower_bound simple", "[lower_bound]")
+{
+   tmp_file file;
+
+   psidb::database db(file.native_handle(), 1024);
+   auto            trx    = db.start_transaction();
+   auto            cursor = trx.get_cursor();
+   cursor.insert("a", "v1");
+   cursor.lower_bound("a");
+   CHECK_CURSOR(cursor, "a", "v1");
+   cursor.lower_bound(std::string_view{"a", 2});
+   CHECK(!cursor.valid());
+   cursor.lower_bound(" ");
+   CHECK_CURSOR(cursor, "a", "v1");
+}
+
+TEST_CASE("lower_bound many", "[lower_bound]")
+{
+   tmp_file file;
+
+   psidb::database db(file.native_handle(), 1024);
+   auto            trx    = db.start_transaction();
+   auto            cursor = trx.get_cursor();
+   for (int i = 100; i < 900; i += 2)
+   {
+      auto [k, v] = make_kv(i);
+      cursor.insert(k, v);
+   }
+   cursor.lower_bound("");
+   CHECK_CURSOR(cursor, "100", "102400");
+   for (int i = 100; i < 900; i += 2)
+   {
+      auto [k, v] = make_kv(i);
+      cursor.lower_bound(k);
+      CHECK_CURSOR(cursor, k, v);
+      auto [mid, _] = make_kv(i + 1);
+      cursor.lower_bound(mid);
+      if (i < 898)
+      {
+         auto [nextk, nextv] = make_kv(i + 2);
+         CHECK_CURSOR(cursor, nextk, nextv);
+      }
+      else
+      {
+         CHECK(!cursor.valid());
+      }
+   }
+}
+
+TEST_CASE("lower bound large value", "[lower_bound]")
+{
+   tmp_file file;
+
+   psidb::database db(file.native_handle(), 1024);
+
+   auto        trx    = db.start_transaction();
+   auto        cursor = trx.get_cursor();
+   std::string value(65536, static_cast<char>(0xCC));
+   cursor.insert("a", value);
+   cursor.lower_bound("a");
+   CHECK_CURSOR(cursor, "a", value);
+}
+
+TEST_CASE("next", "[next]")
+{
+   tmp_file file;
+
+   psidb::database db(file.native_handle(), 1024);
+
+   auto trx    = db.start_transaction();
+   auto cursor = trx.get_cursor();
+   cursor.insert("a", "v1");
+   cursor.insert("b", "v2");
+   cursor.lower_bound("");
+   cursor.next();
+   CHECK_CURSOR(cursor, "b", "v2");
+   cursor.next();
+   CHECK(!cursor.valid());
+}
+
+TEST_CASE("previous", "[previous]")
+{
+   tmp_file file;
+
+   psidb::database db(file.native_handle(), 1024);
+
+   auto trx    = db.start_transaction();
+   auto cursor = trx.get_cursor();
+   cursor.insert("a", "v1");
+   cursor.lower_bound("b");
+   cursor.previous();
+   CHECK_CURSOR(cursor, "a", "v1");
+}
+
+TEST_CASE("erase simple", "[erase]")
+{
+   tmp_file file;
+
+   psidb::database db(file.native_handle(), 1024);
+
+   auto trx    = db.start_transaction();
+   auto cursor = trx.get_cursor();
+
+   cursor.insert("a", "v1");
+   cursor.erase("a");
+   cursor.lower_bound("");
+   CHECK(!cursor.valid());
+}
+
+TEST_CASE("erase many", "[erase]")
+{
+   tmp_file file;
+
+   psidb::database db(file.native_handle(), 1024);
+
+   auto trx    = db.start_transaction();
+   auto cursor = trx.get_cursor();
+   for (int i = 100; i < 500; ++i)
+   {
+      auto [k, v] = make_kv(i);
+      cursor.insert(k, v);
+   }
+
+   for (int i = 100; i < 400; ++i)
+   {
+      auto [k, v] = make_kv(i);
+      cursor.erase(k);
+   }
+   cursor.lower_bound("");
+   CHECK_CURSOR(cursor, "400", "409600");
+}
---
+++ libraries/psidb/test/test_util.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <fcntl.h>
+#include <filesystem>
+#include <string>
+#include <utility>
+
+class tmp_file
+{
+  public:
+   tmp_file()
+       : _fd(::open(std::filesystem::temp_directory_path().c_str(), O_RDWR | O_TMPFILE | O_EXCL))
+   {
+   }
+   ~tmp_file() { ::close(_fd); }
+   int native_handle() { return _fd; }
+
+  private:
+   int _fd;
+};
+
+auto make_kv(int i)
+{
+   return std::pair{std::to_string(i), std::to_string(i * 1024)};
+}
+
+#define CHECK_CURSOR(cursor, k, v)         \
+   do                                      \
+   {                                       \
+      CHECK(cursor.valid());               \
+      if (cursor.valid())                  \
+      {                                    \
+         CHECK(cursor.get_key() == (k));   \
+         CHECK(cursor.get_value() == (v)); \
+      }                                    \
+   } while (false)
