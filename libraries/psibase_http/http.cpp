38d75f4603a0e23676b68839ba3b9559f7eaaaba
Todd Fleming <tbfleming@gmail.com>
2022-05-16 19:54:27 -0400

    cors

--- libraries/psibase_http/http.cpp
+++ libraries/psibase_http/http.cpp
@@ -191,14 +191,24 @@ namespace psibase::http
       unsigned req_version    = req.version();
       bool     req_keep_alive = req.keep_alive();
 
+      const auto set_cors = [&server](auto& res)
+      {
+         if (!server.http_config->allow_origin.empty())
+         {
+            res.set(bhttp::field::access_control_allow_origin, server.http_config->allow_origin);
+            res.set(bhttp::field::access_control_allow_methods, "POST, GET, OPTIONS");
+            res.set(bhttp::field::access_control_allow_headers, "*");
+         }
+      };
+
       // Returns a bad request response
-      const auto bad_request = [&server, req_version, req_keep_alive](beast::string_view why)
+      const auto bad_request =
+          [&server, set_cors, req_version, req_keep_alive](beast::string_view why)
       {
          bhttp::response<bhttp::string_body> res{bhttp::status::bad_request, req_version};
          res.set(bhttp::field::server, BOOST_BEAST_VERSION_STRING);
          res.set(bhttp::field::content_type, "text/html");
-         if (!server.http_config->allow_origin.empty())
-            res.set(bhttp::field::access_control_allow_origin, server.http_config->allow_origin);
+         set_cors(res);
          res.keep_alive(req_keep_alive);
          res.body() = why.to_string();
          res.prepare_payload();
@@ -206,13 +216,13 @@ namespace psibase::http
       };
 
       // Returns a not found response
-      const auto not_found = [&server, req_version, req_keep_alive](beast::string_view target)
+      const auto not_found =
+          [&server, set_cors, req_version, req_keep_alive](beast::string_view target)
       {
          bhttp::response<bhttp::string_body> res{bhttp::status::not_found, req_version};
          res.set(bhttp::field::server, BOOST_BEAST_VERSION_STRING);
          res.set(bhttp::field::content_type, "text/html");
-         if (!server.http_config->allow_origin.empty())
-            res.set(bhttp::field::access_control_allow_origin, server.http_config->allow_origin);
+         set_cors(res);
          res.keep_alive(req_keep_alive);
          res.body() = "The resource '" + target.to_string() + "' was not found.";
          res.prepare_payload();
@@ -221,30 +231,38 @@ namespace psibase::http
 
       // Returns an error response
       const auto error =
-          [&server, req_version, req_keep_alive](bhttp::status status, beast::string_view why,
-                                                 const char* content_type = "text/html")
+          [&server, set_cors, req_version, req_keep_alive](
+              bhttp::status status, beast::string_view why, const char* content_type = "text/html")
       {
          bhttp::response<bhttp::string_body> res{status, req_version};
          res.set(bhttp::field::server, BOOST_BEAST_VERSION_STRING);
          res.set(bhttp::field::content_type, content_type);
-         if (!server.http_config->allow_origin.empty())
-            res.set(bhttp::field::access_control_allow_origin, server.http_config->allow_origin);
+         set_cors(res);
          res.keep_alive(req_keep_alive);
          res.body() = why.to_string();
          res.prepare_payload();
          return res;
       };
 
-      const auto ok =
-          [&server, req_version, req_keep_alive](std::vector<char> reply, const char* content_type)
+      const auto ok = [&server, set_cors, req_version, req_keep_alive](std::vector<char> reply,
+                                                                       const char* content_type)
       {
          bhttp::response<bhttp::vector_body<char>> res{bhttp::status::ok, req_version};
          res.set(bhttp::field::server, BOOST_BEAST_VERSION_STRING);
          res.set(bhttp::field::content_type, content_type);
-         if (!server.http_config->allow_origin.empty())
-            res.set(bhttp::field::access_control_allow_origin, server.http_config->allow_origin);
+         set_cors(res);
          res.keep_alive(req_keep_alive);
          res.body() = std::move(reply);
+         res.prepare_payload();
+         return res;
+      };
+
+      const auto ok_no_content = [&server, set_cors, req_version, req_keep_alive]()
+      {
+         bhttp::response<bhttp::vector_body<char>> res{bhttp::status::ok, req_version};
+         res.set(bhttp::field::server, BOOST_BEAST_VERSION_STRING);
+         set_cors(res);
+         res.keep_alive(req_keep_alive);
          res.prepare_payload();
          return res;
       };
@@ -256,11 +274,16 @@ namespace psibase::http
          if (colon != host.npos)
             host.remove_suffix(host.size() - colon);
 
+         if (req.method() == bhttp::verb::options)
+         {
+            return send(ok_no_content());
+         }
+
          // TODO: simplify rule for separating native vs contract
-         if (req.target() == "/" || req.target().starts_with("/rpc") ||
-             req.target().starts_with("/common") || req.target().starts_with("/ui") ||
-             host != server.http_config->host && host.ends_with(server.http_config->host) &&
-                 !req.target().starts_with("/native"))
+         else if (req.target() == "/" || req.target().starts_with("/rpc") ||
+                  req.target().starts_with("/common") || req.target().starts_with("/ui") ||
+                  host != server.http_config->host && host.ends_with(server.http_config->host) &&
+                      !req.target().starts_with("/native"))
          {
             rpc_request_data data;
             if (req.method() == bhttp::verb::get)
