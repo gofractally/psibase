df2babcf2ec0376bb10975fd3930a26c99492115
Todd Fleming <tbfleming@gmail.com>
2022-06-08 21:03:57 -0400

    Magic number macros

--- rust/psi_macros/src/lib.rs
+++ rust/psi_macros/src/lib.rs
@@ -1,10 +1,12 @@
 use contract_macro::contract_macro_impl;
 use fracpack_macro::fracpack_macro_impl;
+use number_macro::{account_macro_impl, method_macro_impl};
 use proc_macro::TokenStream;
 use proc_macro_error::proc_macro_error;
 
 mod contract_macro;
 mod fracpack_macro;
+mod number_macro;
 
 #[proc_macro_derive(Fracpack, attributes(fracpack))]
 pub fn derive_fracpack(input: TokenStream) -> TokenStream {
@@ -15,4 +17,16 @@ pub fn derive_fracpack(input: TokenStream) -> TokenStream {
 #[proc_macro_attribute]
 pub fn contract(attr: TokenStream, item: TokenStream) -> TokenStream {
     contract_macro_impl(attr, item)
+}
+
+#[proc_macro_error]
+#[proc_macro]
+pub fn account(item: TokenStream) -> TokenStream {
+    account_macro_impl(item)
+}
+
+#[proc_macro_error]
+#[proc_macro]
+pub fn method(item: TokenStream) -> TokenStream {
+    method_macro_impl(item)
 }
---
+++ rust/psi_macros/src/number_macro.rs
@@ -0,0 +1,23 @@
+use libpsibase_names::{account_number_from_str, account_number_to_string, method_number_from_str};
+use proc_macro::TokenStream;
+use proc_macro_error::emit_error;
+use quote::quote;
+use syn::{parse_macro_input, LitStr};
+
+pub fn account_macro_impl(item: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(item as LitStr);
+    let n = account_number_from_str(&item.value());
+    if account_number_to_string(n) != item.value() {
+        emit_error!(item, "{} is not a valid AccountNumber", item.value());
+    }
+    TokenStream::from(quote! {::libpsibase::AccountNumber::new(#n)})
+}
+
+pub fn method_macro_impl(item: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(item as LitStr);
+    let n = method_number_from_str(&item.value());
+    if n == 0 && item.value() != "" {
+        emit_error!(item, "{} is not a valid MethodNumber", item.value());
+    }
+    TokenStream::from(quote! {::libpsibase::MethodNumber::new(#n)})
+}
