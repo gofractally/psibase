c98336db3cfca3bb794ade079cf1df531d5c8c3e
Steven Watanabe <steven@clarionos.com>
2022-07-26 13:44:25 -0600

    Factor out duplicated code

--- libraries/net/test/test_integrated.cpp
+++ libraries/net/test/test_integrated.cpp
@@ -7,7 +7,7 @@
 #include <boost/asio/steady_timer.hpp>
 #include <boost/asio/io_context.hpp>
 
-#include <map>
+#include "mapdb.hpp"
 
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
@@ -18,42 +18,6 @@ using namespace psibase::test;
 
 template<typename Derived>
 struct null_link {};
-
-struct prefix_compare
-{
-   template<typename T0, typename T1, std::size_t... I>
-   bool compare(const T0& lhs, const T1& rhs, std::index_sequence<I...>) const
-   {
-      return std::tie(std::get<I>(lhs)...) < std::tie(std::get<I>(rhs)...);
-   }
-   using is_transparent = void;
-   template<typename... T, typename... U>
-   bool operator()(const std::tuple<T...>& lhs, const std::tuple<U...>& rhs) const
-   {
-      return compare(lhs, rhs, std::make_index_sequence<std::min(sizeof...(T), sizeof...(U))>{});
-   }
-   template<typename... T, typename U>
-   bool operator()(const std::tuple<T...>& lhs, const U& rhs) const
-   {
-      return std::get<0>(lhs) < rhs;
-   };
-   template<typename T, typename... U>
-   bool operator()(const T& lhs, const std::tuple<U...>& rhs) const
-   {
-      return lhs < std::get<0>(rhs);
-   }
-   template<typename T, typename U>
-   bool operator()(const T& lhs, const U& rhs) const
-   {
-      return lhs < rhs;
-   }
-};
-
-struct mapdb
-{
-   template<typename K, typename V>
-   using index = std::map<K, V, prefix_compare>;
-};
 
 //using timer_type = mock_timer;
 using timer_type = boost::asio::steady_timer;
