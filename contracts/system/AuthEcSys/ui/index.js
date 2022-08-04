b6382038f2579334f46b5169f7cc29dd5f7e69d6
James-Mart <jmartx93@gmail.com>
2022-08-04 17:35:18 +0000

    finish key import in auth-ec frontend

--- contracts/system/AuthEcSys/ui/index.js
+++ contracts/system/AuthEcSys/ui/index.js
@@ -3,6 +3,7 @@ import { siblingUrl } from "/common/rootdomain.mjs";
 import { initializeApplet, 
   getJson, action, addRoute, push, 
   getLocalResource, CommonResources } from "/common/rpc.mjs";
+import { keyPairStrings, privateStringToKeyPair } from "/common/keyConversions.mjs";
 
 const html = htm.bind(React.createElement);
 const { useEffect, useState, useCallback } = React;
@@ -10,7 +11,7 @@ const { Segment, Header, Icon, Modal, Form, Table, Input, Button, Message, Tab, 
 
 await initializeApplet();
 
-//export const thisApplet = await getJson('/common/thiscontract');
+const thisApplet = await getJson('/common/thiscontract');
 
 // const transactionTypes = {
 //   credit: 0,
@@ -47,12 +48,13 @@ function useLocalStorage(key, initialValue) {
   return [storedValue, setValue];
 }
 
-
-
-function Imported(props) {
-  if (props.imported)
+function ErrorPane(props) 
+{
+  if (props.show === true)
   {
-    return html`Imported`;
+    return html`
+      <${Message} negative header='Import failure' content='Failed to import accounts for given private key.' />
+    `;
   }
   else return html``;
 }
@@ -60,17 +62,60 @@ function Imported(props) {
 function App() {
 
   const [priv, setPriv] = useState("");
+  const [impErr, setImpErr] = useState(false);
 
   const [keys, setKeys] = useLocalStorage("keys", []);
+  const [accounts, setAccounts] = useLocalStorage("accounts", []);
 
-  let doImport = (privateKey) => {
-    setKeys(keys.concat([privateKey]));
+  let showImportError = () => {
+    setImpErr(true);
+    setTimeout(()=>{
+      setImpErr(false);
+    }, 5000);
   };
 
-  useEffect(()=>{
-    console.log("Keys changed: ");
-    console.log(keys);
-  }, [keys]);
+  let Accounts = () => {
+    console.log("rerendering with " + accounts.length + " accounts.");
+    let accountRows = accounts.map((a)=>{
+      return html`
+        <${Table.Row}>
+          <${Table.Cell}>${a.account}</${Table.Cell}>
+          <${Table.Cell}>${a.pubkey}</${Table.Cell}>
+          <${Table.Cell}><${Button} fluid>Login</${Button}></${Table.Cell}>
+        </${Table.Row}>
+      `;
+    });
+    console.log(accountRows);
+    if (accountRows) 
+    {
+        return accountRows;
+    }
+    else return html``;
+  };
+
+  let doImport = async (privateKey) => {
+    if (privateKey === null || !privateKey)
+    {
+      console.error("Failed to import private key");
+      return;
+    }
+    if (keys.includes(privateKey))
+    {
+      console.error("Already imported that private key");
+      return;
+    }
+
+    try {
+      let keypair = keyPairStrings(privateStringToKeyPair(privateKey));
+      let res = await getJson(siblingUrl(null, thisApplet, "accwithkey/" + keypair.pub));
+      setKeys(keys.concat([privateKey]));
+      setAccounts(accounts.concat(res));
+    } catch(e)
+    {
+      showImportError();
+      console.error(e);
+    }
+  };
 
   return html`
     <${Container}>
@@ -81,19 +126,13 @@ function App() {
           <${Table.Header}>
             <${Table.Row}>
               <${Table.HeaderCell}collapsing>Account</${Table.HeaderCell}>
+              <${Table.HeaderCell}>Public key</${Table.HeaderCell}>
               <${Table.HeaderCell} collapsing />
             </${Table.Row}>
           </${Table.Header}>
 
           <${Table.Body}>
-            <${Table.Row}>
-              <${Table.Cell}>alice</${Table.Cell}>
-              <${Table.Cell}><${Button} fluid>Login</${Button}></${Table.Cell}>
-            </${Table.Row}>
-            <${Table.Row}>
-              <${Table.Cell}>bob</${Table.Cell}>
-              <${Table.Cell}><${Button} fluid>Login</${Button}></${Table.Cell}>
-            </${Table.Row}>
+            <${Accounts} />
           </${Table.Body}>
         </${Table}>
 
@@ -116,6 +155,7 @@ function App() {
             } }
           ]}
         />
+        <${ErrorPane} show=${impErr}/>
 
       </${Segment}>
     </${Container}>
