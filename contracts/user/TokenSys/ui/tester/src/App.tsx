2e8cfed1556964dab6a167ad39d442bd71271277
John Williamson <johnjwilliamson@live.com>
2022-08-20 04:09:04 +0000

    tidy up

--- contracts/user/TokenSys/ui/tester/src/App.tsx
+++ contracts/user/TokenSys/ui/tester/src/App.tsx
@@ -1,71 +1,65 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-// import wait from 'waait'
+import wait from 'waait'
 // @ts-ignore
 import { initializeApplet, action, query, operation, siblingUrl, getJson, setOperations } from "/common/rpc.mjs";
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   useEffect(() => {
-    console.log('initilizing the applet..')
-    initializeApplet(async() => {
+    initializeApplet(async () => {
       const thisApplet = await getJson('/common/thiscontract') as string;
 
       setOperations([
         {
-            id: "credit",
-            exec: async ({symbol, receiver, amount, memo}: { symbol: string, receiver: string, amount: string, memo: string}) => {
-      
-              //TODO: let tokenId = query("symbol-sys", "getTokenId", {symbol});
-              let tokens = await getJson(await siblingUrl(null, thisApplet, "getTokenTypes"));
-              let token = tokens.find((t: any)=>t.symbolId === symbol.toLowerCase());
-              if (!token)
+          id: "credit",
+          exec: async ({ symbol, receiver, amount, memo }: { symbol: string, receiver: string, amount: string, memo: string }) => {
+
+            //TODO: let tokenId = query("symbol-sys", "getTokenId", {symbol});
+            let tokens = await getJson(await siblingUrl(null, thisApplet, "getTokenTypes"));
+            let token = tokens.find((t: any) => t.symbolId === symbol.toLowerCase());
+            if (!token) {
+              console.error("No token with symbol " + symbol);
+              return;
+            }
+            let tokenId = token.id;
+
+            await action(thisApplet, "credit",
               {
-                console.error("No token with symbol " + symbol);
-                return;
-              }
-              let tokenId = token.id;
-    
-              await action(thisApplet, "credit", 
-                { 
-                  tokenId, 
-                  receiver, 
-                  amount: {value: amount }, 
-                  memo: {contents: memo},
+                tokenId,
+                receiver,
+                amount: { value: amount },
+                memo: { contents: memo },
               });
-            },
+          },
         }
       ]);
     });
 
-    query("account-sys", "", "getLoggedInUser", {}, (x: any) => {
-      console.log('THIS HAS RESOLVED!', x)
+    query("account-sys", "", "getLoggedInUser", {}, (loggedInUser: any) => {
+      console.log('getLoggedInUser:', loggedInUser)
     });
   }, [])
 
   const [userBalance, setUserBalance] = useState('');
 
-  const getBalance = async() => {
-    let res = await getJson(await siblingUrl(null, 'token-sys', "balances/" + 'alice')) as {account: string; balance: string; precision: number, token: number, symbol: string}[]
+  const getBalance = async () => {
+    let res = await getJson(await siblingUrl(null, 'token-sys', "balances/" + 'alice')) as { account: string; balance: string; precision: number, token: number, symbol: string }[]
     return res;
   }
-  const amountToSend = String(Number(33) * Math.pow(10, 8))
+  const amountToSend = String(Number(5) * Math.pow(10, 8))
 
-  const triggerTx = () => {
-    operation('token-sys', '', 'credit', {symbol: 'PSI', receiver: 'bob', amount: amountToSend, memo: 'Working'})
-    // await wait(2000);
-    console.log('getting balance...');
-    getBalance().then((x) => {
-      console.log('new balance is', x)
-      setUserBalance(x[0].balance)
-    })
+  const triggerTx = async () => {
+    operation('token-sys', '', 'credit', { symbol: 'PSI', receiver: 'bob', amount: amountToSend, memo: 'Working' })
+    await wait(2000);
+    const balances = await getBalance()
+    setUserBalance(balances[0].balance)
   };
 
 
-  
+
   return (
     <div className="App">
       <div>
@@ -78,8 +72,8 @@ function App() {
       </div>
       <h1>alices balance is {userBalance}</h1>
       <div className="card">
-        <button onClick={() => {triggerTx()}}>
-          trigger transaction
+        <button onClick={() => { triggerTx() }}>
+          send 5 tokens to bob
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
