0feb18034dedba1a852bb1f0038f33cf5ad51daa
Valentine Zavgorodnev <valentine.zavgorodnev@gmail.com>
2022-09-07 10:30:35 -0600

    Add ExploreSys applet implemented with SvelteKit

---
+++ services/user/ExploreSys/ui/src/components/Loader.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Icon from "/src/assets/icons/loader.svg";
+</script>
+
+<div aria-modal="true">
+    <Icon class="h-8 w-6 animate-spin" />
+</div>
+
+<style>
+    div {
+        overflow-y: auto;
+        overflow-x: hidden;
+        position: fixed;
+        height: 100vh;
+        z-index: 50;
+        justify-content: center;
+        display: flex;
+        @apply items-center;
+        @apply inset-0;
+        @apply bg-opacity-0;
+        @apply opacity-0;
+        @apply transition;
+        @apply ease-in-out;
+        @apply duration-200;
+        @apply bg-opacity-50;
+        @apply opacity-100;
+    }
+</style>
