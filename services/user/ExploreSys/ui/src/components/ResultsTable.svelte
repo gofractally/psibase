117dbdb6afe362bf302a4c1aab86021335a7e138
Valentine Zavgorodnev <valentine.zavgorodnev@gmail.com>
2022-09-07 10:30:35 -0600

    Add ExploreSys applet implemented with SvelteKit

---
+++ services/user/ExploreSys/ui/src/components/ResultsTable.svelte
@@ -0,0 +1,54 @@
+<script>
+    import classNames from "classnames";
+    import { goto } from "$app/navigation";
+    export let blocks = [];
+    export let clickable = true;
+
+    const onClick = (block) => {
+        if (clickable) goto(`/block/${block.header.blockNum}`);
+    };
+</script>
+
+<table class={classNames($$props.class, "w-full table-fixed")}>
+    <tbody>
+        {#if blocks.length > 0}
+            <tr class="border-b border-b-black">
+                <th class="w-20">Block<br />No.</th>
+                <th class="w-16"># of<br />trans.</th>
+                <th>Previous<br />Block</th>
+                <th><br />Time</th>
+            </tr>
+        {/if}
+        {#each blocks as block}
+            <tr
+                on:click={onClick(block)}
+                class={classNames({ "cursor-pointer": clickable })}
+            >
+                <td class="w-20">{block.header.blockNum}</td>
+                <td class="w-16 whitespace-nowrap"
+                    >{block.transactions.length}</td
+                >
+                <td class="max-w-lg truncate">
+                    {block.header.previous}
+                </td>
+                <td class="whitespace-nowrap">{block.header.time}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
+
+<style>
+    th,
+    td {
+        @apply font-mono;
+        @apply text-sm;
+        @apply p-2;
+        @apply text-left;
+    }
+    th {
+        @apply font-bold;
+    }
+    tr:hover {
+        @apply bg-gray-100;
+    }
+</style>
---
+++ services/user/ExploreSys/ui/vite.config.ts
@@ -0,0 +1,94 @@
+import { defineConfig } from "vite";
+import path from "path";
+import alias from "@rollup/plugin-alias";
+// @ts-ignore
+import { sveltekit } from "@sveltejs/kit/vite";
+import { isoImport } from "vite-plugin-iso-import";
+import svg from "@poppanator/sveltekit-svg";
+
+const psibase = (appletContract: string) => {
+    return [
+        {
+            name: "psibase",
+            config: () => {
+                return {
+                    build: {
+                        assetsDir: "",
+                        cssCodeSplit: false,
+                        rollupOptions: {
+                            external: [
+                                "/common/rootdomain.mjs",
+                                "/common/rpc.mjs",
+                                "/common/useGraphQLQuery.mjs",
+                                "/common/iframeResizer.js",
+                            ],
+                            makeAbsoluteExternalsRelative: false,
+                        },
+                    },
+                    server: {
+                        host: "psibase.127.0.0.1.sslip.io",
+                        port: 8081,
+                        proxy: {
+                            "/": {
+                                target: "http://psibase.127.0.0.1.sslip.io:8080",
+                                bypass: (
+                                    req: any,
+                                    _res: any,
+                                    _opt: any
+                                ): any => {
+                                    const host = req.headers.host || "";
+                                    const subdomain = host.split(".")[0];
+                                    if (
+                                        subdomain === appletContract &&
+                                        req.method !== "POST" &&
+                                        req.headers.accept !==
+                                            "application/json" &&
+                                        !req.url.startsWith("/common") &&
+                                        !req.url.startsWith("/api")
+                                    ) {
+                                        return req.url;
+                                    }
+                                },
+                            },
+                        },
+                    },
+                    resolve: {
+                        alias: {
+                            "/common/iframeResizer.contentWindow.js":
+                                path.resolve(
+                                    "../../CommonSys/common/thirdParty/src/iframeResizer.contentWindow.js"
+                                ),
+                            "/common": path.resolve("../../CommonSys/common"),
+                        },
+                    },
+                };
+            },
+        },
+        alias({
+            entries: [
+                { find: "common/rpc.mjs", replacement: "/common/rpc.mjs" },
+            ],
+        }),
+    ];
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        isoImport(),
+        sveltekit(),
+        psibase("explore-sys"),
+        // @ts-ignore
+        svg({
+            svgoOptions: {
+                multipass: true,
+                plugins: [
+                    {
+                        name: "preset-default",
+                        params: { overrides: { removeViewBox: false } },
+                    },
+                ],
+            },
+        }),
+    ],
+});
