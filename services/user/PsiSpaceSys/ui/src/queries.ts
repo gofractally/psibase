fdc0bc3f730097e1c1515648f4a1578d7350bada
sparkplug0025 <sparkplug0025@gmail.com>
2022-09-09 11:59:45 -0400

    Implements deploy and fallback default profile

--- services/user/PsiSpaceSys/ui/src/queries.ts
+++ services/user/PsiSpaceSys/ui/src/queries.ts
@@ -4,8 +4,8 @@ import wait from "waait";
 import { useGraphQLQuery } from "./helpers";
 
 export const getContentQuery = (account = "") => {
-    account = "psispace-sys";
-    console.info("querying acc >>", account);
+    // account = "psispace-sys";
+    // console.info("querying acc >>", account);
     if (account) {
         return `
         {
