af2ef618cec624218ee069d4fb02cb6245bad026
Valentine Zavgorodnev <valentine.zavgorodnev@gmail.com>
2022-09-23 12:51:38 -0400

    Get accounts by pub key

--- services/user/ExploreSys/ui/src/routes/search/+page.svelte
+++ services/user/ExploreSys/ui/src/routes/search/+page.svelte
@@ -1,16 +1,24 @@
 <script>
     import {goto} from "$app/navigation";
-    import {loadBlockData} from "/src/lib/loadData.js";
+    import {loadBlockData, getAccountsByKey} from "/src/lib/loadData.js";
     import { ExplorerIcon, LeftArrowIcon } from "/src/assets/icons";
-    import { Blocks, Button, TextInput } from "/src/components";
+    import { Blocks, Button, Error, TextInput } from "/src/components";
     import searchIconUrl from "/src/assets/icons/search.svg?url";
 
     let searchTerm;
+    let currentSearch;
     let blocks = [];
-    let account = null;
+    let accounts = [];
+    let error;
 
-    const search = async () => {
-        if (searchTerm.match(/[0-9]+/)) {
+    const search = async (e) => {
+        e.preventDefault();
+        error = "";
+        if (searchTerm.match(/^PUB_/)) {
+            const data = await getAccountsByKey(searchTerm);
+            if (data.error) error = data.error;
+            else accounts = data;
+        } else if (searchTerm.match(/[0-9]+/)) {
             const data = await loadBlockData(searchTerm);
             if (data.block) {
                 blocks = [
@@ -20,10 +28,11 @@
                     },
                 ];
             }
-            account = null;
         } else {
             await goto(`/account/${searchTerm}`)
         }
+        currentSearch = searchTerm;
+        return null;
     };
 </script>
 
@@ -31,13 +40,13 @@
     <div class="mb-6 flex items-center gap-2">
         <ExplorerIcon />
         <h1 class="text-6xl text-gray-600">Block Explorer</h1>
-        <div class="flex-grow"/>
+        <div class="flex-grow" />
     </div>
     <Button on:click={() => history.back()} leftIcon={LeftArrowIcon} class="mb-2">
         Block explorer
     </Button>
     <h4 class="my-2 text-gray-600">
-        Search account / <span class="text-gray-400">public key</span> / block
+        Search account / public key / block
     </h4>
     <form on:submit|preventDefault={search} class="mb-4 flex items-center">
         <TextInput
@@ -49,8 +58,22 @@
         />
         <Button on:click={search}>Search</Button>
     </form>
-    {#if blocks.length > 0}
-        <h5 class="mb-4 text-xl">Search results</h5>
+    {#if currentSearch && !error && blocks.length === 0 && accounts.length === 0}
+        <h4 class="my-4 text-xl">No results</h4>
     {/if}
-    <Blocks {blocks}/>
+    {#if error}
+        <Error value={error} />
+    {/if}
+    {#if blocks.length > 0}
+        <h5 class="my-4 text-xl">Search results</h5>
+        <Blocks {blocks}/>
+    {/if}
+    {#if accounts.length > 0}
+        <h5 class="my-4 text-xl">Search results</h5>
+        <ul>
+            {#each accounts as a}
+                <li><a href="#" on:click={() => goto(`/account/${a.account}`)}>{a.account}</a></li>
+            {/each}
+        </ul>
+    {/if}
 </div>
