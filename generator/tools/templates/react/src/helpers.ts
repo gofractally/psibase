82abc2a7da309ac90fc5faecfe65669d8cd35bb1
John Williamson <johnjwilliamson@live.com>
2022-09-25 09:39:47 +0000

    fix query error

--- generator/tools/templates/react/src/helpers.ts
+++ generator/tools/templates/react/src/helpers.ts
@@ -1,13 +1,19 @@
-import { query } from "common/rpc.mjs";
+import { AppletId, getJson, query } from "common/rpc.mjs";
 
-export const fetchQuery = <T>(
+
+export const fetchQuery = async <T>(
     queryName: string,
-    contract: string,
     params: any = {},
     subPath: string = ""
 ): Promise<T> => {
-    return query(contract, subPath, queryName, params);
+
+    const thisApplet = await getJson<string>("/common/thisservice");
+    const applet = new AppletId(thisApplet, subPath);
+
+    return query(applet, queryName, params);
 };
 
 export const getLoggedInUser = () =>
-    fetchQuery<string>("getLoggedInUser", "account-sys");
+    fetchQuery<string>("getLoggedInUser");
+
+
