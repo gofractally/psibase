1500721b97912cfbf8f1d2fbe3b895852ad9e86a
Brandon Fancher <brandonfancher@gmail.com>
2022-09-29 19:59:30 +0000

    Make ExploreSys more responsive.

--- services/user/ExploreSys/ui/src/components/Blocks.svelte
+++ services/user/ExploreSys/ui/src/components/Blocks.svelte
@@ -7,12 +7,19 @@
     const onClick = (block) => {
         if (clickable) goto(`/block/${block.header.blockNum}`);
     };
+
+    const formatDateTime = (datetime) => {
+        const segments = datetime.split("T");
+        const date = segments[0];
+        const time = segments[1].replace("Z", "");
+        return `${date} ${time}`;
+    };
 </script>
 
 <table class={classNames($$props.class, "w-full table-fixed")}>
     <thead>
         {#if blocks.length > 0}
-            <tr class="border-b border-b-black">
+            <tr class="border-b border-b-black select-none">
                 <th class="w-20">Block<br />No.</th>
                 <th class="w-16"># of<br />trans.</th>
                 <th>Previous<br />Block</th>
@@ -25,18 +32,20 @@
             <tr
                 on:click={onClick(block)}
                 class={classNames(
-                    {clickable},
-                    {"text-gray-500": block.transactions.length === 0 && blocks.length > 1}
-                )}
-            >
+                    { clickable },
+                    {
+                        "text-gray-500":
+                            block.transactions.length === 0 &&
+                            blocks.length > 1,
+                    }
+                )}>
                 <td class="w-20">{block.header.blockNum}</td>
                 <td class="w-16 whitespace-nowrap"
-                    >{block.transactions.length}</td
-                >
+                    >{block.transactions.length}</td>
                 <td class="max-w-lg truncate">
                     {block.header.previous}
                 </td>
-                <td class="whitespace-nowrap">{block.header.time}</td>
+                <td>{formatDateTime(block.header.time)}</td>
             </tr>
         {/each}
     </tbody>
