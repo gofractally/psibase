93ceb6ec4403387b11fae8f0c6adfe1ffa12821b
John Williamson <johnjwilliamson@live.com>
2022-09-30 07:12:59 +0000

    rename flie

--- generator/tools/templates/react/deploy.sh
+++ generator/tools/templates/react/deploy.sh
@@ -5,4 +5,6 @@ read -sp 'Private key: ' privkey
 echo
 echo Deploying __contract__Service to account $accname ...
 
+yarn
+yarn run build
 psibase -s $privkey upload-tree -S $accname $accname / dist/
