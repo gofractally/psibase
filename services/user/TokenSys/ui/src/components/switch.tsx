4a18defccdd2eb6ba9fbaa768247c75bf0df7ad0
Valentine Zavgorodnev <valentine.zavgorodnev@gmail.com>
2022-10-03 14:51:48 -0400

    Style Switch component according to Figma design

--- services/user/TokenSys/ui/src/components/switch.tsx
+++ services/user/TokenSys/ui/src/components/switch.tsx
@@ -1,27 +1,29 @@
-import React, { forwardRef, HTMLProps, useState } from "react";
+import React, { forwardRef, HTMLProps } from "react";
 
 export interface SwitchProps extends HTMLProps<HTMLInputElement> {
+    checked: boolean;
+    value?: string;
     disabled?: boolean;
     label?: string;
 }
 
 export const Switch = forwardRef<HTMLInputElement, SwitchProps>(
     (props, ref) => {
-
-        const [stateOn, set] = useState(true);
-        const stateOnClass = ' transform translate-x-5';
+        const checkedClassContainer = props.checked ? 'bg-gray-700 border-gray-700' : 'bg-transparent border-gray-400';
+        const checkedClassInput = props.checked ? 'translate-x-6' : '';
 
         return (
             <label
                 htmlFor={props.id}
-                className="Switch flex place-items-center gap-4 bg-gray-100 p-3 font-medium text-gray-700"
+                className="Switch flex place-items-center gap-4 p-3 font-medium text-gray-700"
             >
                 <div className="flex-0">{props.label}</div>
-                <div className="md:w-14 md:h-7 w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer">
+                <div
+                    className={`w-14 h-7 flex items-center border-2 rounded-full p-1 cursor-pointer ${checkedClassContainer}`}
+                >
                     <input
                         type="checkbox"
-                        className={`bg-white md:w-6 md:h-6 h-5 w-5 rounded-full shadow-md transform ${stateOn ? null : stateOnClass}`}
-                        ref={ref}
+                        className={`h-5 w-5 rounded-full transform ${checkedClassInput}`}
                         {...props}
                     />
                 </div>
