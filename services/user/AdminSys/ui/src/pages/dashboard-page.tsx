5c804e0936f534cf8540e95af36e42c5470afc07
sparkplug0025 <sparkplug0025@gmail.com>
2023-01-15 11:53:34 -0500

    psibase dashboards

--- services/user/AdminSys/ui/src/pages/dashboard-page.tsx
+++ services/user/AdminSys/ui/src/pages/dashboard-page.tsx
@@ -1,3 +1,10 @@
 export const DashboardPage = () => {
-    return <div>Dashboard goes here</div>;
+    return (
+        <iframe
+            src="http://localhost:3000/d/xKzdPUh4k/psinode-dashboards?orgId=1&from=now-1h&to=now&refresh=5s&theme=light&kiosk=tv"
+            style={{ overflow: "hidden", height: "100%", width: "100%" }}
+            height="100%"
+            width="100%"
+        />
+    );
 };
