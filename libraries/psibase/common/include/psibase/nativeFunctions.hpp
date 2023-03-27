gofractally/psibase

commit e3419bca51661f1129a91869a8fc83265661a699

Convert event data storage format to fracpack struct. Breaking change to chain state.

libraries/psibase/common/include/psibase/nativeFunctions.hpp
@@ -229,19 +229,30 @@ namespace psibase
       return raw::putSequential(db, value.pos, value.remaining());
    }
 
+   template <typename Type, NotOptional V = void>
+   struct SequentialRecord
+   {
+      AccountNumber service;
+      Type          type;
+      V             value;
+      PSIO_REFLECT(SequentialRecord, service, type, value);
+   };
+
+   template <typename Type>
+   struct SequentialRecord<Type, void>
+   {
+      AccountNumber service;
+      Type          type;
+      PSIO_REFLECT(SequentialRecord, service, type);
+   };
+
    /// Add a sequentially-numbered record
    ///
    /// Returns the id.
    template <typename Type, NotOptional V>
    uint64_t putSequential(DbId db, AccountNumber service, Type type, const V& value)
    {
-      std::vector<char>     packed(psio::fracpack_size(service) + psio::fracpack_size(type) +
-                                   psio::fracpack_size(value));
-      psio::fast_buf_stream stream(packed.data(), packed.size());
-      psio::to_frac(service, stream);
-      psio::to_frac(type, stream);
-      psio::to_frac(value, stream);
-      return putSequentialRaw(db, packed);
+      return putSequentialRaw(db, psio::to_frac(SequentialRecord<Type, V>{service, type, value}));
    }
 
    /// Remove a key-value pair if it exists
@@ -358,46 +369,23 @@ namespace psibase
                                          AccountNumber*       service      = nullptr,
                                          Type*                type         = nullptr)
    {
-      // TODO: this record format is seriously problematic.
-      // fracpack in general is not able to determine the end of an object,
-      // so simply appending components is not reliable. The layout
-      // also doesn't match any struct (except when V is fixed size),
-      // which makes reading unnecessarily complicated
-      std::optional<V> result;
-      auto             v = getSequentialRaw(db, id);
+      auto v = getSequentialRaw(db, id);
       if (!v)
-         return result;
+         return {};
 
-      std::uint32_t pos = 0;
+      auto [c, t] = psio::from_frac<SequentialRecord<Type>>(*v);
 
-      auto fracunpack = [&](auto& value)
-      {
-         bool has_unknown = false;
-         bool known_end;
-         if (!psio::is_packable<std::remove_reference_t<decltype(value)>>::template unpack<true,
-                                                                                           false>(
-                 &value, has_unknown, known_end, v->data(), pos, v->size()))
-            check(false, "Invalid frac encoding");
-      };
-
-      AccountNumber c;
-      fracunpack(c);
       if (service)
          *service = c;
       if (matchService && *matchService != c)
-         return result;
+         return {};
 
-      Type t;
-      fracunpack(t);
       if (type)
          *type = t;
       if (matchType && *matchType != t)
-         return result;
+         return {};
 
-      result.emplace();
-      // TODO: validate (allow opt-in or opt-out)
-      fracunpack(*result);
-      return result;
+      return psio::view<SequentialRecord<Type, V>>{*v}.value().unpack();
    }
 
    /// Get the first key-value pair which is greater than or equal to `key`

libraries/psibase/common/include/psibase/serveGraphQL.hpp
@@ -657,56 +657,37 @@ namespace psibase
              },
              input_stream, output_stream, error, true);
 
-      std::uint32_t pos = 0;
-
-      auto fracunpack = [&](auto& value)
-      {
-         bool has_unknown = false;
-         bool known_end;
-         if (!psio::is_packable<std::remove_reference_t<decltype(value)>>::template unpack<true,
-                                                                                           false>(
-                 &value, has_unknown, known_end, v->data(), pos, v->size()))
-            check(false, "Invalid frac encoding");
-         check(known_end,
-               "The end of the object must be known in order to decode consecutive packed objects");
-      };
-
-      AccountNumber service;
-      fracunpack(service);
-      if (service != decoder.service)
+      SequentialRecord<MethodNumber> header = {};
+      if (!psio::from_frac(header, *v) || header.service != decoder.service)
          return gql_query(
              EventDecoderStatus{
                  .event_db                = (uint32_t)decoder.db,
                  .event_id                = decoder.eventId,
                  .event_found             = true,
-                 .event_service           = service,
+                 .event_service           = header.service,
                  .event_supported_service = false,
-                 .event_type              = {},
+                 .event_type              = header.type,
                  .event_unpack_ok         = false,
              },
              input_stream, output_stream, error, true);
 
-      MethodNumber type;
-      fracunpack(type);
-
       bool ok    = true;
       bool found = false;
       psio::reflect<Events>::get_by_name(
-          type.value,
+          header.type.value,
           [&](auto meta, auto member)
           {
              using MT = psio::MemberPtrType<decltype(member(std::declval<Events*>()))>;
              static_assert(MT::isFunction);
-             using TT = decltype(psio::tuple_remove_view(
+             using TT                                     = decltype(psio::tuple_remove_view(
                  std::declval<psio::TupleFromTypeList<typename MT::SimplifiedArgTypes>>()));
-             std::span<char> eventData{v->data() + pos, v->data() + v->size()};
-             if (psio::fracpack_validate<TT>(eventData))
+             SequentialRecord<MethodNumber, TT> eventData = {};
+             if (psio::from_frac(eventData, *v))
              {
-                found      = true;
-                auto value = psio::from_frac<TT>(psio::prevalidated{eventData});
-                ok = gql_query_decoder_value(decoder, type, value, input_stream, output_stream,
-                                             error,
-                                             {meta.param_names.begin(), meta.param_names.end()});
+                found = true;
+                ok    = gql_query_decoder_value(decoder, header.type, eventData.value, input_stream,
+                                                output_stream, error,
+                                                {meta.param_names.begin(), meta.param_names.end()});
              }
           });
 
@@ -716,9 +697,9 @@ namespace psibase
                  .event_db                = (uint32_t)decoder.db,
                  .event_id                = decoder.eventId,
                  .event_found             = true,
-                 .event_service           = service,
+                 .event_service           = header.service,
                  .event_supported_service = true,
-                 .event_type              = type,
+                 .event_type              = header.type,
                  .event_unpack_ok         = false,
              },
              input_stream, output_stream, error, true);
@@ -1032,34 +1013,16 @@ namespace psibase
             break;
          }
 
-         std::uint32_t pos = 0;
-
-         auto fracunpack = [&](auto& value)
-         {
-            bool has_unknown = false;
-            bool known_end;
-            if (!psio::is_packable<std::remove_reference_t<decltype(value)>>::template unpack<
-                    true, false>(&value, has_unknown, known_end, v->data(), pos, v->size()))
-               check(false, "Invalid frac encoding");
-            check(known_end,
-                  "The end of the object must be known in order to decode consecutive packed "
-                  "objects");
-         };
-
-         AccountNumber c;
-         fracunpack(c);
-         if (c != service)
+         SequentialRecord<MethodNumber> header;
+         if (!psio::from_frac(header, *v) && header.service != service)
          {
             result.pageInfo.hasNextPage = false;
             break;
          }
 
-         MethodNumber type;
-         fracunpack(type);
-
          bool found = false;
          psio::reflect<Events>::get_by_name(
-             type.value,
+             header.type.value,
              [&](auto meta, auto member)
              {
                 using MT = psio::MemberPtrType<decltype(member(std::declval<Events*>()))>;
@@ -1067,12 +1030,12 @@ namespace psibase
                 using TT = decltype(psio::tuple_remove_view(
                     std::declval<psio::TupleFromTypeList<typename MT::SimplifiedArgTypes>>()));
                 // TODO: EventDecoder validates and unpacks this again
-                std::span<const char> eventData{v->data() + pos, v->data() + v->size()};
-                if (psio::fracpack_validate<TT>(eventData))
+                SequentialRecord<MethodNumber, TT> eventData;
+                if (psio::from_frac(eventData, *v))
                 {
-                   auto value = psio::from_frac<TT>(psio::prevalidated{eventData});
                    get_event_field<0>(  //
-                       value, fieldName, {}, {meta.param_names.begin(), meta.param_names.end()},
+                       eventData.value, fieldName, {},
+                       {meta.param_names.begin(), meta.param_names.end()},
                        [&](auto _, const auto& field)
                        {
                           if constexpr (std::is_arithmetic_v<std::remove_cvref_t<decltype(field)>>)

libraries/psibase/common/include/psibase/Actor.hpp
@@ -150,25 +150,19 @@ namespace psibase
       template <uint32_t idx, uint64_t Name, auto MemberPtr>
       auto call(EventNumber n) const
       {
-         auto size         = raw::getSequential(event_log, n);
          using param_tuple = decltype(psio::tuple_remove_view(psio::args_as_tuple(MemberPtr)));
-         struct EventHeader
+         AccountNumber service;
+         MethodNumber  type;
+         MethodNumber  expected_type{Name};
+         auto result = psibase::getSequential<param_tuple>(event_log, n, &sender, &expected_type,
+                                                           &service, &type);
+         if (!result)
          {
-            AccountNumber sender;
-            MethodNumber  event_type;
-         };
-
-         EventHeader header;
-         raw::getResult(reinterpret_cast<char*>(&header), sizeof(header), 0);
-         psibase::check(header.event_type == MethodNumber{Name}, "unexpected event type");
-         psibase::check(header.sender == sender, "unexpected event sender");
-
-         std::vector<char> tmp(size);
-         raw::getResult(tmp.data(), tmp.size(), 0);
-
-         psio::shared_view_ptr<param_tuple> ptr(psio::size_tag{size - sizeof(header)});
-         memcpy(ptr.data(), tmp.data() + sizeof(header), ptr.size());
-         return ptr;
+            psibase::check(type == MethodNumber{Name}, "unexpected event type");
+            psibase::check(service == sender, "unexpected event sender");
+            psibase::check(false, "event not found");
+         }
+         return std::move(*result);
       }
    };
 

libraries/psibase/native/src/NativeFunctions.cpp
@@ -409,11 +409,13 @@ namespace psibase
              clearResult(*this);
              auto m = getDbWriteSequential(*this, db);
 
-             psio::input_stream v{value.data(), value.size()};
-             check(v.remaining() >= sizeof(AccountNumber::value),
-                   "value prefix must match service during write");
-             auto service = psio::from_bin<AccountNumber>(v);
-             check(service == code.codeNum, "value prefix must match service during write");
+             std::span<const char>     v{value.data(), value.size()};
+             std::tuple<AccountNumber> service;
+             if (!psio::from_frac(service, v) || std::get<0>(service) != code.codeNum)
+             {
+                check(false,
+                      "value of putSequential must have service account as its first member");
+             }
 
              auto&    dbStatus = transactionContext.blockContext.databaseStatus;
              uint64_t indexNumber;

services/psibase_tests/test_event.cpp
@@ -1,13 +1,18 @@
 #include "event-service.hpp"
 
 #include <psibase/DefaultTestChain.hpp>
-
-#include <psio/to_hex.hpp>
+#include <psibase/serveGraphQL.hpp>
 
 #include <catch2/catch.hpp>
 
 using namespace psibase;
 using namespace std::literals::string_literals;
+
+struct Query
+{
+   auto events() const { return EventQuery<EventService::Events>{EventService::service}; }
+};
+PSIO_REFLECT(Query, method(events))
 
 TEST_CASE("test events")
 {
@@ -18,8 +23,21 @@ TEST_CASE("test events")
                    .to<EventService>()
                    .foo("antidisestablishmentarianism", 42)
                    .returnVal();
-   auto result = EventService().events().history().e(evId);
-   auto [s, i] = result->unpack();
+   auto [s, i] = EventService().events().history().e(evId);
    CHECK(s == "antidisestablishmentarianism"s);
    CHECK(i == 42);
+
+   std::string query = "query { events { history(ids: [" + std::to_string(evId) + "]) { s i } } }";
+
+   HttpRequest request  = {.host        = "event-service.psibase.127.0.0.1.sslip.io",
+                           .rootHost    = "psibase.127.0.0.1.sslip.io",
+                           .method      = "POST",
+                           .target      = "/graphql",
+                           .contentType = "application/graphql",
+                           .body        = std::vector(query.begin(), query.end())};
+   auto        response = serveGraphQL(request, Query());
+   REQUIRE(!!response);
+   auto response_body = std::string_view(response->body.data(), response->body.size());
+   CHECK(response_body ==
+         R"""({"data": {"events":{"history":[{"s":"antidisestablishmentarianism","i":42}]}}})""");
 }

commit 4d26f76e91cf98a8c42ae8353b118717991fda8c

Add service tests to ctest cand fix the the bugs introduced in e3419bca51661f1129a91869a8fc83265661a699

libraries/psibase/common/include/psibase/nativeFunctions.hpp
@@ -232,17 +232,17 @@ namespace psibase
    template <typename Type, NotOptional V = void>
    struct SequentialRecord
    {
-      AccountNumber service;
-      Type          type;
-      V             value;
+      AccountNumber       service;
+      std::optional<Type> type;
+      std::optional<V>    value;
       PSIO_REFLECT(SequentialRecord, service, type, value);
    };
 
    template <typename Type>
    struct SequentialRecord<Type, void>
    {
-      AccountNumber service;
-      Type          type;
+      AccountNumber       service;
+      std::optional<Type> type;
       PSIO_REFLECT(SequentialRecord, service, type);
    };
 
@@ -380,9 +380,11 @@ namespace psibase
       if (matchService && *matchService != c)
          return {};
 
+      if (!t)
+         return {};
       if (type)
-         *type = t;
-      if (matchType && *matchType != t)
+         *type = *t;
+      if (matchType && *matchType != *t)
          return {};
 
       return psio::view<SequentialRecord<Type, V>>{*v}.value().unpack();

libraries/psibase/common/include/psibase/serveGraphQL.hpp
@@ -666,7 +666,7 @@ namespace psibase
                  .event_found             = true,
                  .event_service           = header.service,
                  .event_supported_service = false,
-                 .event_type              = header.type,
+                 .event_type              = *header.type,
                  .event_unpack_ok         = false,
              },
              input_stream, output_stream, error, true);
@@ -674,7 +674,7 @@ namespace psibase
       bool ok    = true;
       bool found = false;
       psio::reflect<Events>::get_by_name(
-          header.type.value,
+          header.type->value,
           [&](auto meta, auto member)
           {
              using MT = psio::MemberPtrType<decltype(member(std::declval<Events*>()))>;
@@ -685,9 +685,9 @@ namespace psibase
              if (psio::from_frac(eventData, *v))
              {
                 found = true;
-                ok    = gql_query_decoder_value(decoder, header.type, eventData.value, input_stream,
-                                                output_stream, error,
-                                                {meta.param_names.begin(), meta.param_names.end()});
+                ok = gql_query_decoder_value(decoder, *header.type, *eventData.value, input_stream,
+                                             output_stream, error,
+                                             {meta.param_names.begin(), meta.param_names.end()});
              }
           });
 
@@ -699,7 +699,7 @@ namespace psibase
                  .event_found             = true,
                  .event_service           = header.service,
                  .event_supported_service = true,
-                 .event_type              = header.type,
+                 .event_type              = *header.type,
                  .event_unpack_ok         = false,
              },
              input_stream, output_stream, error, true);
@@ -1022,7 +1022,7 @@ namespace psibase
 
          bool found = false;
          psio::reflect<Events>::get_by_name(
-             header.type.value,
+             header.type->value,
              [&](auto meta, auto member)
              {
                 using MT = psio::MemberPtrType<decltype(member(std::declval<Events*>()))>;
@@ -1034,7 +1034,7 @@ namespace psibase
                 if (psio::from_frac(eventData, *v))
                 {
                    get_event_field<0>(  //
-                       eventData.value, fieldName, {},
+                       *eventData.value, fieldName, {},
                        {meta.param_names.begin(), meta.param_names.end()},
                        [&](auto _, const auto& field)
                        {

libraries/psibase/tester/src/DefaultTestChain.cpp
@@ -47,11 +47,17 @@ DefaultTestChain::DefaultTestChain(
    setBlockProducers();
    registerSysRpc();
 
-   from(UserService::NftSys::service).to<UserService::NftSys>().init();
-   from(UserService::TokenSys::service).to<UserService::TokenSys>().init();
-   from(UserService::SymbolSys::service).to<UserService::SymbolSys>().init();
-   from(UserService::Invite::InviteSys::service).to<UserService::Invite::InviteSys>().init();
-   from(UserService::Fractal::FractalSys::service).to<UserService::Fractal::FractalSys>().init();
+   expect(from(UserService::NftSys::service).to<UserService::NftSys>().init().trace());
+   expect(from(UserService::TokenSys::service).to<UserService::TokenSys>().init().trace());
+   expect(from(UserService::SymbolSys::service).to<UserService::SymbolSys>().init().trace());
+   expect(from(UserService::Invite::InviteSys::service)
+              .to<UserService::Invite::InviteSys>()
+              .init()
+              .trace());
+   expect(from(UserService::Fractal::FractalSys::service)
+              .to<UserService::Fractal::FractalSys>()
+              .init()
+              .trace());
    from(UserService::Fractal::CoreFractalSys::service)
        .to<UserService::Fractal::CoreFractalSys>()
        .init();

services/user/CMakeLists.txt
@@ -20,7 +20,7 @@ function(add_user_service suffix name)
     set_target_properties(${name}${suffix} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${ROOT_BINARY_DIR})
 endfunction(add_user_service)
 
-function(add_user_test suffix name)
+function(add_user_test_impl suffix name)
     set(TARGET ${name}-test${suffix})
     add_executable(${TARGET} ${ARGN})
     target_include_directories(${TARGET} PUBLIC
@@ -41,18 +41,24 @@ function(add_user_test suffix name)
      ${CMAKE_CURRENT_LIST_DIR})
     target_link_libraries(${TARGET} services_system${suffix} psitestlib)
     set_target_properties(${TARGET} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${ROOT_BINARY_DIR})
-endfunction(add_user_test)
+endfunction()
 
 function(add_user_test_release name)
     if(BUILD_RELEASE_WASM)
-        add_user_test("" ${name} ${ARGN})
+        add_user_test_impl("" ${name} ${ARGN})
+        add_wasm_test(${name}-test)
     endif()
 endfunction()
 
 function(add_user_test_debug name)
     if(BUILD_DEBUG_WASM)
-        add_user_test("-debug" ${name} ${ARGN})
+        add_user_test_impl("-debug" ${name} ${ARGN})
     endif()
+endfunction()
+
+function(add_user_test name)
+    add_user_test_release(${name} ${ARGN})
+    add_user_test_debug(${name} ${ARGN})
 endfunction()
 
 function(add suffix)

generator/tools/templates/contract/test/CMakeLists.txt
@@ -1,3 +1,3 @@
-add_user_test_debug(__contract__ src/__contract__-test.cpp)
+add_user_test(__contract__ src/__contract__-test.cpp)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS on)

services/user/InviteSys/test/CMakeLists.txt
@@ -1,3 +1,3 @@
-add_user_test_debug(InviteSys src/InviteSys-test.cpp)
+add_user_test(InviteSys src/InviteSys-test.cpp)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS on)

services/user/NftSys/test/CMakeLists.txt
@@ -1,3 +1,3 @@
-add_user_test_debug(NftSys src/NftSys-test.cpp)
+add_user_test(NftSys src/NftSys-test.cpp)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS on)

services/user/SymbolSys/test/CMakeLists.txt
@@ -1,3 +1,3 @@
-add_user_test_debug(SymbolSys src/SymbolSys-test.cpp)
+add_user_test(SymbolSys src/SymbolSys-test.cpp)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS on)

services/user/TokenSys/test/CMakeLists.txt
@@ -1,3 +1,3 @@
-add_user_test_debug(TokenSys src/TokenSys-test.cpp)
+add_user_test(TokenSys src/TokenSys-test.cpp)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS on)