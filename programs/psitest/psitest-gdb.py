df23deccac33dad8f626b50569c4b85113c1b8cb
Steven Watanabe <steven@clarionos.com>
2023-04-25 15:10:10 -0600

    Add gdb command: set hide-native on|off

--- programs/psitest/psitest-gdb.py
+++ programs/psitest/psitest-gdb.py
@@ -1,5 +1,13 @@
 import gdb
 
+class HideNative(gdb.Parameter):
+    """Controls whether native frames and functions are visible"""
+    def __init__(self, name):
+        super (HideNative, self).__init__(name, gdb.COMMAND_BREAKPOINTS, gdb.PARAM_BOOLEAN)
+        self.value = True
+
+hide_native = HideNative("hide-native")
+
 def is_wasm_frame(frame):
     sal = frame.find_sal()
     if (not sal.is_valid()):
@@ -13,7 +21,10 @@ class WasmFilter:
         self.enabled = True
         gdb.frame_filters[self.name] = self
     def filter(self, frame_iter):
-        return (x for x in frame_iter if is_wasm_frame(x.inferior_frame()))
+        if hide_native.value:
+            return (x for x in frame_iter if is_wasm_frame(x.inferior_frame()))
+        else:
+            return frame_iter
 
 WasmFilter()
 
@@ -24,10 +35,12 @@ def is_wasm_address(pc):
     return sal.symtab is None or not sal.symtab.objfile.is_file
 
 def disable_native_breakpoints(breakpoint):
-    if breakpoint.visible:
+    if hide_native.value and breakpoint.visible:
         for loc in breakpoint.locations:
             if loc.enabled and not is_wasm_address(loc.address):
                 loc.enabled = False
 
 gdb.events.breakpoint_created.connect(disable_native_breakpoints)
 gdb.events.breakpoint_modified.connect(disable_native_breakpoints)
+
+gdb.set_parameter("breakpoint pending", True)
