e275491a5f525acc6af7152d26b388ab71e3a952
Steven Watanabe <steven@clarionos.com>
2023-06-13 08:43:35 -0600

    Use max_stack_bytes in psitest. Other limits are not needed because psitest doesn't need to be deterministic.

--- programs/psitest/main.cpp
+++ programs/psitest/main.cpp
@@ -32,7 +32,9 @@ void backtrace();
 
 struct vm_options
 {
-   static constexpr std::uint32_t max_call_depth = 1024;
+   static constexpr bool          enable_simd          = true;
+   static constexpr std::uint32_t max_func_local_bytes = 1024 * 1024;
+   static constexpr std::uint32_t max_stack_bytes      = 1024 * 1024;
 };
 
 template <>
