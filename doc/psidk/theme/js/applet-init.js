36b3c8f80b3002db802ffee003c4dd6925a36c8b
James-Mart <jmartx93@gmail.com>
2023-10-30 22:17:57 +0000

    improve warning message running book outside of psibase

--- doc/psidk/theme/js/applet-init.js
+++ doc/psidk/theme/js/applet-init.js
@@ -1,4 +1,8 @@
 (async () => {
+  try {
     const commonLib = await import('/common/common-lib.js');
     await commonLib.initializeApplet();
-  })();
+  } catch (err) {
+    console.warn("Cannot initialize applet. If you're accessing this book outside of a psibase network, ignore this message.")
+  }
+})();
