687392cb4ef4208819623e8a2b992332217afd39
John Williamson <johnjwilliamson@live.com>
2023-12-14 19:57:15 +0000

    add supervisor-sys hello world

--- rust/psibase/src/boot.rs
+++ rust/psibase/src/boot.rs
@@ -18,6 +18,86 @@ macro_rules! method {
     };
 }
 
+const ACCOUNTS: [AccountNumber; 31] = [
+    account_sys::SERVICE,
+    account!("alice"),
+    auth_ec_sys::SERVICE,
+    account!("auth-sys"),
+    account!("auth-any-sys"),
+    account!("auth-inv-sys"),
+    account!("bob"),
+    common_sys::SERVICE,
+    cpu_sys::SERVICE,
+    account!("doc-sys"),
+    account!("supervisor-sys"),
+    account!("explore-sys"),
+    account!("fractal-sys"),
+    account!("core-frac-sys"),
+    account!("invite-sys"),
+    nft_sys::SERVICE,
+    producer_sys::SERVICE,
+    proxy_sys::SERVICE,
+    psispace_sys::SERVICE,
+    account!("r-account-sys"),
+    account!("r-ath-ec-sys"),
+    account!("r-auth-sys"),
+    account!("r-prod-sys"),
+    account!("r-proxy-sys"),
+    account!("r-tok-sys"),
+    setcode_sys::SERVICE,
+    account!("symbol-sys"),
+    account!("token-sys"),
+    transaction_sys::SERVICE,
+    account!("verifyec-sys"),
+    account!("verify-sys"),
+];
+
+macro_rules! sgc {
+    ($acc:literal, $flags:expr, $wasm:literal) => {
+        SharedGenesisService {
+            service: account!($acc),
+            flags: $flags,
+            vmType: 0,
+            vmVersion: 0,
+            code: include_bytes!(concat!("../boot-image/contents/", $wasm)).into(),
+        }
+    };
+}
+
+macro_rules! store {
+    ($acc:literal, $dest:expr, $ty:expr, $src:expr) => {
+        store_sys(
+            account!($acc),
+            account!($acc),
+            $dest,
+            $ty,
+            include_bytes!(concat!("../boot-image/contents/", $src)),
+        )
+    };
+}
+
+macro_rules! store_common {
+    ($name:literal, $ty:expr) => {
+        store!(
+            "common-sys",
+            concat!("/common/", $name),
+            $ty,
+            concat!("CommonSys/common/", $name)
+        )
+    };
+}
+
+macro_rules! store_third_party {
+    ($name:literal, $ty:expr) => {
+        store!(
+            "common-sys",
+            concat!("/common/", $name),
+            $ty,
+            concat!("CommonSys/common/thirdParty/", $name)
+        )
+    };
+}
+
 fn set_producers_action(name: AccountNumber, key: Claim) -> Action {
     producer_sys::Wrapper::pack().setProducers(vec![ProducerConfigRow {
         producerName: name,
@@ -97,7 +177,177 @@ pub fn get_initial_actions<R: Read + Seek>(
             s.store_data(&mut actions)?;
         }
 
-        s.postinstall(&mut actions)?;
+        let mut reg_actions = vec![
+            reg_server(account_sys::SERVICE, account!("r-account-sys")),
+            reg_server(auth_ec_sys::SERVICE, account!("r-ath-ec-sys")),
+            reg_server(account!("auth-sys"), account!("r-auth-sys")),
+            reg_server(common_sys::SERVICE, common_sys::SERVICE),
+            reg_server(account!("explore-sys"), account!("explore-sys")),
+            reg_server(producer_sys::SERVICE, account!("r-prod-sys")),
+            reg_server(proxy_sys::SERVICE, account!("r-proxy-sys")),
+            reg_server(psispace_sys::SERVICE, psispace_sys::SERVICE),
+        ];
+
+        let mut common_sys_files = vec![
+            store!(
+                "common-sys",
+                "/ui/common.index.html",
+                html,
+                "CommonSys/ui/vanilla/common.index.html"
+            ),
+            store_common!("keyConversions.mjs", js),
+            store_common!("common-lib.js", js),
+            store_common!("rpc.mjs", js),
+            store_common!("SimpleUI.mjs", js),
+            store_common!("useGraphQLQuery.mjs", js),
+            store_common!("useLocalStorage.mjs", js),
+            store_common!("widgets.mjs", js),
+            store_common!("fonts/raleway.css", css),
+            store_common!("fonts/raleway-variable-italic.ttf", ttf),
+            store_common!("fonts/raleway-variable-normal.ttf", ttf),
+            store_common!("fonts/red-hat-mono.css", css),
+            store_common!("fonts/red-hat-mono-variable-normal.ttf", ttf),
+        ];
+
+        fill_dir(
+            &include_dir!("$CARGO_MANIFEST_DIR/boot-image/contents/CommonSys/ui/dist"),
+            &mut common_sys_files,
+            common_sys::SERVICE,
+            common_sys::SERVICE,
+        );
+
+        let mut common_sys_3rd_party_files = vec![
+            store_third_party!("htm.module.js", js),
+            store_third_party!("iframeResizer.contentWindow.js", js),
+            store_third_party!("iframeResizer.js", js),
+            store_third_party!("react-dom.development.js", js),
+            store_third_party!("react-dom.production.min.js", js),
+            store_third_party!("react-router-dom.min.js", js),
+            store_third_party!("react.development.js", js),
+            store_third_party!("react.production.min.js", js),
+            store_third_party!("semantic-ui-react.min.js", js),
+            store_third_party!("useLocalStorageState.js", js),
+        ];
+
+        let mut account_sys_files = vec![];
+        fill_dir(
+            &include_dir!("$CARGO_MANIFEST_DIR/boot-image/contents/AccountSys/ui/dist"),
+            &mut account_sys_files,
+            account!("r-account-sys"),
+            account!("r-account-sys"),
+        );
+
+        let mut auth_ec_sys_files = vec![
+            store!("r-ath-ec-sys", "/", html, "AuthEcSys/ui/index.html"),
+            store!("r-ath-ec-sys", "/index.js", js, "AuthEcSys/ui/index.js"),
+        ];
+
+        let mut auth_sys_files = vec![
+            store!("r-auth-sys", "/", html, "AuthSys/ui/index.html"),
+            store!("r-auth-sys", "/index.js", js, "AuthSys/ui/index.js"),
+        ];
+
+        let mut explore_sys_files = vec![];
+        fill_dir(
+            &include_dir!("$CARGO_MANIFEST_DIR/boot-image/contents/ExploreSys/ui/dist"),
+            &mut explore_sys_files,
+            account!("explore-sys"),
+            account!("explore-sys"),
+        );
+
+        let mut token_sys_files = vec![];
+        fill_dir(
+            &include_dir!("$CARGO_MANIFEST_DIR/boot-image/contents/TokenSys/ui/dist"),
+            &mut token_sys_files,
+            account!("r-tok-sys"),
+            account!("r-tok-sys"),
+        );
+
+        let mut psispace_sys_files = vec![];
+        fill_dir(
+            &include_dir!("$CARGO_MANIFEST_DIR/boot-image/contents/PsiSpaceSys/ui/dist"),
+            &mut psispace_sys_files,
+            psispace_sys::SERVICE,
+            psispace_sys::SERVICE,
+        );
+
+        let mut invite_sys_files = vec![];
+        fill_dir(
+            &include_dir!("$CARGO_MANIFEST_DIR/boot-image/contents/InviteSys/ui/dist"),
+            &mut invite_sys_files,
+            account!("invite-sys"),
+            account!("invite-sys"),
+        );
+
+        actions.append(&mut reg_actions);
+        actions.append(&mut common_sys_files);
+        actions.append(&mut common_sys_3rd_party_files);
+        actions.append(&mut account_sys_files);
+        actions.append(&mut auth_ec_sys_files);
+        actions.append(&mut auth_sys_files);
+        actions.append(&mut explore_sys_files);
+        actions.append(&mut token_sys_files);
+        actions.append(&mut psispace_sys_files);
+        actions.append(&mut invite_sys_files);
+    }
+
+    let mut supervisor_actions = vec![
+        new_account_action(account_sys::SERVICE, account!("supervisor-sys")), //
+    ];
+    fill_dir(
+        &include_dir!("$CARGO_MANIFEST_DIR/boot-image/contents/SupervisorSys"),
+        &mut supervisor_actions,
+        account!("supervisor-sys"),
+        psispace_sys::SERVICE,
+    );
+    actions.append(&mut supervisor_actions);
+
+    let mut doc_actions = vec![
+        new_account_action(account_sys::SERVICE, account!("doc-sys")), //
+    ];
+    fill_dir(
+        &include_dir!("$CARGO_MANIFEST_DIR/boot-image/contents/doc/html"),
+        &mut doc_actions,
+        account!("doc-sys"),
+        psispace_sys::SERVICE,
+    );
+    actions.append(&mut doc_actions);
+
+    if install_token_users {
+        #[allow(clippy::inconsistent_digit_grouping)]
+        let mut create_and_fund_example_users = vec![
+            new_account_action(account_sys::SERVICE, account!("alice")),
+            new_account_action(account_sys::SERVICE, account!("bob")),
+            Action {
+                sender: account!("symbol-sys"),
+                service: account!("token-sys"),
+                method: method!("setTokenConf"),
+                rawData: (1u32, method!("untradeable"), false).packed().into(),
+            },
+            Action {
+                sender: account!("symbol-sys"),
+                service: account!("token-sys"),
+                method: method!("mint"),
+                rawData: (1u32, (1_000_000_00000000_u64,), "memo").packed().into(),
+            },
+            Action {
+                sender: account!("symbol-sys"),
+                service: account!("token-sys"),
+                method: method!("credit"),
+                rawData: (1u32, account!("alice"), (1_000_00000000_u64,), "memo")
+                    .packed()
+                    .into(),
+            },
+            Action {
+                sender: account!("symbol-sys"),
+                service: account!("token-sys"),
+                method: method!("credit"),
+                rawData: (1u32, account!("bob"), (1_000_00000000_u64,), "memo")
+                    .packed()
+                    .into(),
+            },
+        ];
+        actions.append(&mut create_and_fund_example_users);
     }
 
     actions.push(set_producers_action(
