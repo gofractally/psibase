42bfa10b92bf9f3eee32741a62732fc87a17679b
John Williamson <johnjwilliamson@live.com>
2023-12-21 01:16:52 +0000

    up vite

--- services/user/SupervisorSys/ui/src/main.ts
+++ services/user/SupervisorSys/ui/src/main.ts
@@ -39,3 +39,16 @@ connection.promise.then((parent) => {
   // @ts-ignore
   parent.add(3, 1).then((total) => console.log(total));
 });
+
+// Create an iframe with a source of https://app.psibase.io/plugin.html
+// and append it to the DOM.
+//
+// For POC purposes we will include the plugin.html
+// It will request the WASM component from https://app.psibase.io/plugin.wasm (Which needs to be manually uploaded for now for the POC)
+// It also listens for window.postMessage through penpal, and it will transpile the wasm
+// Listening for window.postMessage it will WASM.transfer('alice', 'bob', 3, 'tokens man!');
+//
+
+// Await the compiled WASM
+// Just create a vanilla VITE and figure out how to transpile it in the vanilla world.
+// Function is called `call` no params, returns a string
