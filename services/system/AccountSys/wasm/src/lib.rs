4e859eafdf6639b0d09165e7cab6735692058b69
John Williamson <johnjwilliamson@live.com>
2024-01-02 04:22:36 +0000

    add apps

---
+++ services/system/AccountSys/wasm/src/lib.rs
@@ -0,0 +1,16 @@
+cargo_component_bindings::generate!();
+
+use bindings::Guest;
+
+struct Component;
+
+impl Guest for Component {
+    /// Say hello!
+    fn hello_world() -> String {
+        "Hello, World John!".to_string()
+    }
+
+    fn call() -> String {
+        "DerpCasey".to_string()
+    }
+}
