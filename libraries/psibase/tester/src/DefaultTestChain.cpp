c74c57574efac8c843346376216c4c78ac42b163
John Williamson <johnjwilliamson@live.com>
2024-01-04 21:53:46 +0000

    f

--- libraries/psibase/tester/src/DefaultTestChain.cpp
+++ libraries/psibase/tester/src/DefaultTestChain.cpp
@@ -273,3 +273,178 @@ AccountNumber DefaultTestChain::addService(const char* acc,
 {
    return addService(AccountNumber(acc), filename, show);
 }
+
+void DefaultTestChain::registerSysRpc()
+{
+   auto r = ProxySys::service;
+
+   // Register servers
+   std::vector<Action> a{
+       transactor<ProxySys>{AccountSys::service, r}.registerServer(RAccountSys::service),
+       transactor<ProxySys>{AuthEcSys::service, r}.registerServer(RAuthEcSys::service),
+       transactor<ProxySys>{CommonSys::service, r}.registerServer(CommonSys::service),
+       transactor<ProxySys>{ExploreSys::service, r}.registerServer(ExploreSys::service),
+       transactor<ProxySys>{ProxySys::service, r}.registerServer(RProxySys::service),
+       transactor<ProxySys>{PsiSpaceSys::service, r}.registerServer(PsiSpaceSys::service),
+   };
+
+   auto trace = pushTransaction(makeTransaction(std::move(a)));
+   check(psibase::show(false, trace) == "", "Failed to register system rpc services");
+
+   transactor<CommonSys>   rpcCommon(CommonSys::service, CommonSys::service);
+   transactor<RAccountSys> rpcAccount(RAccountSys::service, RAccountSys::service);
+   transactor<ExploreSys>  rpcExplore(ExploreSys::service, ExploreSys::service);
+   transactor<RAuthEcSys>  rpcAuthEc(RAuthEcSys::service, RAuthEcSys::service);
+   transactor<PsiSpaceSys> rpcPsiSpace(PsiSpaceSys::service, PsiSpaceSys::service);
+   transactor<RTokenSys>   rpcToken(RTokenSys::service, RTokenSys::service);
+
+   // Store UI files
+   std::string cdir      = "../services";
+   std::string comDir    = cdir + "/user/CommonSys";
+   std::string accDir    = cdir + "/system/AccountSys";
+   std::string expDir    = cdir + "/user/ExploreSys";
+   std::string psiSpDir  = cdir + "/user/PsiSpaceSys";
+   std::string tokDir    = cdir + "/user/TokenSys";
+   std::string authEcDir = cdir + "/system/AuthEcSys";
+   std::string thirdPty  = comDir + "/common/thirdParty/src";
+
+   const std::string html = "text/html";
+   const std::string js   = "text/javascript";
+   const std::string css  = "text/css";
+   const std::string ttf  = "font/ttf";
+   const std::string svg  = "image/svg+xml";
+   const std::string wasm = "application/wasm";
+
+   std::vector<Action> b{
+       // CommonSys Fancy UI
+       rpcCommon.storeSys("/index.html", html, readWholeFile(comDir + "/ui/dist/index.html")),
+       rpcCommon.storeSys("/index.js", js, readWholeFile(comDir + "/ui/dist/index.js")),
+       rpcCommon.storeSys("/style.css", css, readWholeFile(comDir + "/ui/dist/style.css")),
+       rpcCommon.storeSys("/ninebox.svg", svg, readWholeFile(comDir + "/ui/dist/ninebox.svg")),
+       rpcCommon.storeSys("/loader.svg", svg, readWholeFile(comDir + "/ui/dist/loader.svg")),
+       rpcCommon.storeSys("/psibase.svg", svg, readWholeFile(comDir + "/ui/dist/psibase.svg")),
+       rpcCommon.storeSys("/app-account-desktop.svg", svg,
+                          readWholeFile(comDir + "/ui/dist/app-account-desktop.svg")),
+       rpcCommon.storeSys("/app-account-mobile.svg", svg,
+                          readWholeFile(comDir + "/ui/dist/app-account-mobile.svg")),
+       rpcCommon.storeSys("/app-explore-desktop.svg", svg,
+                          readWholeFile(comDir + "/ui/dist/app-explore-desktop.svg")),
+       rpcCommon.storeSys("/app-explore-mobile.svg", svg,
+                          readWholeFile(comDir + "/ui/dist/app-explore-mobile.svg")),
+       rpcCommon.storeSys("/app-wallet-desktop.svg", svg,
+                          readWholeFile(comDir + "/ui/dist/app-wallet-desktop.svg")),
+       rpcCommon.storeSys("/app-wallet-mobile.svg", svg,
+                          readWholeFile(comDir + "/ui/dist/app-wallet-mobile.svg")),
+       rpcCommon.storeSys("/app-psispace-mobile.svg", svg,
+                          readWholeFile(comDir + "/ui/dist/app-psispace-mobile.svg")),
+       rpcCommon.storeSys("/app-psispace-mobile.svg", svg,
+                          readWholeFile(comDir + "/ui/dist/app-psispace-mobile.svg")),
+
+       // CommonSys Basic UI
+       //    rpcCommon.storeSys("/index.html", html, readWholeFile(comDir + "/ui/vanilla/index.html")),
+       //    rpcCommon.storeSys("/ui/index.js", js, readWholeFile(comDir + "/ui/vanilla/index.js")),
+
+       // CommonSys Other
+       rpcCommon.storeSys("/ui/common.index.html", html,
+                          readWholeFile(comDir + "/ui/vanilla/common.index.html")),
+
+       rpcCommon.storeSys("/common/rpc.mjs", js, readWholeFile(comDir + "/common/rpc.mjs")),
+       rpcCommon.storeSys("/common/useGraphQLQuery.mjs", js,
+                          readWholeFile(comDir + "/common/useGraphQLQuery.mjs")),
+       rpcCommon.storeSys("/common/useLocalStorage.mjs", js,
+                          readWholeFile(comDir + "/common/useLocalStorage.mjs")),
+       rpcCommon.storeSys("/common/SimpleUI.mjs", js,
+                          readWholeFile(comDir + "/common/SimpleUI.mjs")),
+       rpcCommon.storeSys("/common/keyConversions.mjs", js,
+                          readWholeFile(comDir + "/common/keyConversions.mjs")),
+       rpcCommon.storeSys("/common/widgets.mjs", js, readWholeFile(comDir + "/common/widgets.mjs")),
+       rpcCommon.storeSys("/common/fonts/raleway.css", css,
+                          readWholeFile(comDir + "/common/fonts/raleway.css")),
+       rpcCommon.storeSys("/common/fonts/raleway-variable-italic.ttf", ttf,
+                          readWholeFile(comDir + "/common/fonts/raleway-variable-italic.ttf")),
+       rpcCommon.storeSys("/common/fonts/raleway-variable-normal.ttf", ttf,
+                          readWholeFile(comDir + "/common/fonts/raleway-variable-normal.ttf")),
+       rpcCommon.storeSys("/common/fonts/red-hat-mono.css", css,
+                          readWholeFile(comDir + "/common/fonts/red-hat-mono.css")),
+       rpcCommon.storeSys("/common/fonts/red-hat-mono-variable-normal.ttf", ttf,
+                          readWholeFile(comDir + "/common/fonts/red-hat-mono-variable-normal.ttf")),
+
+       // CommonSys - 3rd party
+       rpcCommon.storeSys("/common/iframeResizer.js", js,
+                          readWholeFile(thirdPty + "/iframeResizer.js")),
+       rpcCommon.storeSys("/common/iframeResizer.contentWindow.js", js,
+                          readWholeFile(thirdPty + "/iframeResizer.contentWindow.js")),
+       rpcCommon.storeSys("/common/react.production.min.js", js,
+                          readWholeFile(thirdPty + "/react.production.min.js")),
+       rpcCommon.storeSys("/common/react-dom.production.min.js", js,
+                          readWholeFile(thirdPty + "/react-dom.production.min.js")),
+       rpcCommon.storeSys("/common/react-router-dom.min.js", js,
+                          readWholeFile(thirdPty + "/react-router-dom.min.js")),
+       rpcCommon.storeSys("/common/htm.module.js", js, readWholeFile(thirdPty + "/htm.module.js")),
+       rpcCommon.storeSys("/common/react.development.js", js,
+                          readWholeFile(thirdPty + "/react.development.js")),
+       rpcCommon.storeSys("/common/react-dom.development.js", js,
+                          readWholeFile(thirdPty + "/react-dom.development.js")),
+       rpcCommon.storeSys("/common/semantic-ui-react.min.js", js,
+                          readWholeFile(thirdPty + "/semantic-ui-react.min.js")),
+       rpcCommon.storeSys("/common/use-local-storage-state.mjs", js,
+                          readWholeFile(thirdPty + "/useLocalStorageState.js")),
+
+       // AccountSys Basic UI
+       //    rpcAccount.storeSys("/index.html", html, readWholeFile(accDir + "/ui/vanilla/index.html")),
+       //    rpcAccount.storeSys("/ui/index.js", js, readWholeFile(accDir + "/ui/vanilla/index.js")),
+
+       // AccountSys Fancy UI
+       rpcAccount.storeSys("/app-account.svg", svg,
+                           readWholeFile(accDir + "/ui/dist/app-account.svg")),
+       rpcAccount.storeSys("/index.html", html, readWholeFile(accDir + "/ui/dist/index.html")),
+
+       rpcAccount.storeSys("/yes.html", html, readWholeFile(accDir + "/ui/dist/yes.html")),
+       rpcAccount.storeSys("/account_sys.wasm", wasm, readWholeFile(accDir + "/ui/dist/account_sys.wasm")),
+
+       rpcAccount.storeSys("/index.js", js, readWholeFile(accDir + "/ui/dist/index.js")),
+       rpcAccount.storeSys("/lock-closed.svg", svg,
+                           readWholeFile(accDir + "/ui/dist/lock-closed.svg")),
+       rpcAccount.storeSys("/lock-open.svg", svg, readWholeFile(accDir + "/ui/dist/lock-open.svg")),
+       rpcAccount.storeSys("/logout.svg", svg, readWholeFile(accDir + "/ui/dist/logout.svg")),
+       rpcAccount.storeSys("/refresh.svg", svg, readWholeFile(accDir + "/ui/dist/refresh.svg")),
+       rpcAccount.storeSys("/style.css", css, readWholeFile(accDir + "/ui/dist/style.css")),
+
+       // AuthEcSys
+       rpcAuthEc.storeSys("/index.html", html, readWholeFile(authEcDir + "/ui/index.html")),
+       rpcAuthEc.storeSys("/ui/index.js", js, readWholeFile(authEcDir + "/ui/index.js")),
+
+       // ExploreSys
+       //    rpcExplore.storeSys("/ui/index.js", js, readWholeFile(expDir + "/ui/index.js"))
+
+       // PsiSpaceSys Fancy UI
+       rpcPsiSpace.storeSys("/index.html", html, readWholeFile(psiSpDir + "/ui/dist/index.html")),
+       rpcPsiSpace.storeSys("/index.js", js, readWholeFile(psiSpDir + "/ui/dist/index.js")),
+       rpcPsiSpace.storeSys("/loader.svg", svg, readWholeFile(psiSpDir + "/ui/dist/loader.svg")),
+       rpcPsiSpace.storeSys("/app-psispace-icon.svg", svg,
+                            readWholeFile(psiSpDir + "/ui/dist/app-psispace-icon.svg")),
+       rpcPsiSpace.storeSys("/style.css", css, readWholeFile(psiSpDir + "/ui/dist/style.css")),
+       rpcPsiSpace.storeSys(
+           "/default-profile/default-profile.html", html,
+           readWholeFile(psiSpDir + "/ui/dist/default-profile/default-profile.html")),
+       rpcPsiSpace.storeSys("/default-profile/index.js", js,
+                            readWholeFile(psiSpDir + "/ui/dist/default-profile/index.js")),
+       rpcPsiSpace.storeSys("/default-profile/loader.svg", svg,
+                            readWholeFile(psiSpDir + "/ui/dist/default-profile/loader.svg")),
+       rpcPsiSpace.storeSys("/default-profile/style.css", css,
+                            readWholeFile(psiSpDir + "/ui/dist/default-profile/style.css")),
+
+       // TokenSys
+       rpcToken.storeSys("/index.html", html, readWholeFile(tokDir + "/ui/dist/index.html")),
+       rpcToken.storeSys("/index.js", js, readWholeFile(tokDir + "/ui/dist/index.js")),
+       rpcToken.storeSys("/style.css", css, readWholeFile(tokDir + "/ui/dist/style.css")),
+       rpcToken.storeSys("/loader.svg", svg, readWholeFile(tokDir + "/ui/dist/loader.svg")),
+       rpcToken.storeSys("/app-wallet-icon.svg", svg,
+                         readWholeFile(tokDir + "/ui/dist/app-wallet-icon.svg")),
+       rpcToken.storeSys("/arrow-up-solid.svg", svg,
+                         readWholeFile(tokDir + "/ui/dist/arrow-up-solid.svg")),
+   };
+
+   trace = pushTransaction(makeTransaction(std::move(b)));
+   check(psibase::show(false, trace) == "", "Failed to add UI files to system rpc services");
+}
---
+++ services/system/AccountSys/wasm/target/release/deps/unicode_ident-2b2418fcebeb9167.d
@@ -0,0 +1,8 @@
+/root/psibase/services/system/AccountSys/wasm/target/release/deps/unicode_ident-2b2418fcebeb9167.rmeta: /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
+
+/root/psibase/services/system/AccountSys/wasm/target/release/deps/libunicode_ident-2b2418fcebeb9167.rlib: /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
+
+/root/psibase/services/system/AccountSys/wasm/target/release/deps/unicode_ident-2b2418fcebeb9167.d: /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
+
+/opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs:
+/opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs:
