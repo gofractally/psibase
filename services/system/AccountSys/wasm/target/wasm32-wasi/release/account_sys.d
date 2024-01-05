3ca0f19e82cac0f787bcc42489418a07520ffc2e
John Williamson <johnjwilliamson@live.com>
2024-01-05 04:52:18 +0000

    remove target

--- services/system/AccountSys/wasm/target/wasm32-wasi/release/account_sys.d
+++
@@ -1,1 +0,0 @@
-/root/psibase/services/system/AccountSys/wasm/target/wasm32-wasi/release/account_sys.wasm: /root/psibase/services/system/AccountSys/wasm/src/lib.rs /root/psibase/services/system/AccountSys/wasm/target/bindings/account-sys/bindings.rs
