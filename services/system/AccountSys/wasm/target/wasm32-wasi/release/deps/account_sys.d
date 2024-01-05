f60c90c6577dec75bc5e5276cee832b356f90b7c
John Williamson <johnjwilliamson@live.com>
2024-01-05 04:52:18 +0000

    remove target

--- services/system/AccountSys/wasm/target/wasm32-wasi/release/deps/account_sys.d
+++
@@ -1,6 +0,0 @@
-/root/psibase/services/system/AccountSys/wasm/target/wasm32-wasi/release/deps/account_sys.wasm: src/lib.rs /root/psibase/services/system/AccountSys/wasm/target/bindings/account-sys/bindings.rs
-
-/root/psibase/services/system/AccountSys/wasm/target/wasm32-wasi/release/deps/account_sys.d: src/lib.rs /root/psibase/services/system/AccountSys/wasm/target/bindings/account-sys/bindings.rs
-
-src/lib.rs:
-/root/psibase/services/system/AccountSys/wasm/target/bindings/account-sys/bindings.rs:
--- services/system/AccountSys/wasm/target/wasm32-wasi/release/deps/wit_bindgen-b5c215dfe55452b1.d
+++
@@ -1,7 +0,0 @@
-/root/psibase/services/system/AccountSys/wasm/target/wasm32-wasi/release/deps/wit_bindgen-b5c215dfe55452b1.rmeta: /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/wit-bindgen-0.16.0/src/lib.rs
-
-/root/psibase/services/system/AccountSys/wasm/target/wasm32-wasi/release/deps/libwit_bindgen-b5c215dfe55452b1.rlib: /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/wit-bindgen-0.16.0/src/lib.rs
-
-/root/psibase/services/system/AccountSys/wasm/target/wasm32-wasi/release/deps/wit_bindgen-b5c215dfe55452b1.d: /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/wit-bindgen-0.16.0/src/lib.rs
-
-/opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/wit-bindgen-0.16.0/src/lib.rs:
