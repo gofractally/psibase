a17aaa5c145d37d3fc35c4a41a92f0f2e7d61882
John Williamson <johnjwilliamson@live.com>
2024-01-06 05:56:23 +0000

    importables

--- services/system/AccountSys/wasm/src/lib.rs
+++ services/system/AccountSys/wasm/src/lib.rs
@@ -5,9 +5,27 @@ use bindings::Guest;
 struct Component;
 
 impl Guest for Component {
-    /// Say hello!
-    fn hello_world() -> String {
-        "Hello, World John!".to_string()
+    fn numbers(num1: u8, num2: u8, doubleup: bool) -> String {
+        let mutiplier: u8 = if doubleup { 2 } else { 1 };
+        format!(
+            "Hello, {} + {} equals {}",
+            num1,
+            num2,
+            (num1 + num2) * mutiplier
+        )
+    }
+
+    fn strings(word: String) -> String {
+        bindings::component::account_sys::imports::prnt("Calling prnt from Rust");
+        format!("The word you gave me was {}", word)
+    }
+
+    fn hire(user: User) -> String {
+        format!("{} was hired", user.name)
+    }
+
+    fn peoples(users: Vec<User>) -> String {
+        format!("I got users")
     }
 
     fn call() -> String {
