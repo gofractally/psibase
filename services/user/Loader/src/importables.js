8f381ea5172b832bb82d212082b986b179710f45
John Williamson <johnjwilliamson@live.com>
2024-01-09 09:42:32 +0000

    child imported calls

--- services/user/Loader/src/importables.js
+++ services/user/Loader/src/importables.js
@@ -1,7 +1,30 @@
+import { connectToParent } from "penpal";
+
 function derp(num) {
   return num * 2;
 }
 
-export function prnt(string) {
-  console.log(`from imported code: john ${derp(2)}`, string);
+export async function funccall(params) {
+  console.log(params, "are received params in importables...");
+  return "yesbuddeh funcCall";
+}
+
+export async function prnt(string) {
+  console.log(window, window.postMessage, "was the window post message");
+  console.log(`from imported code: ${derp(2)}`, string);
+
+  console.log(connectToParent, "is the connect to parent");
+
+  const xx = await connectToParent().promise;
+
+  console.log(xx, "received in rust..");
+
+  const toCall = {
+    service: "idunno",
+    method: "stilldontknow",
+    params: "yesbuddeh",
+  };
+  const res = await xx.importedCall(toCall);
+
+  console.log(res, "came back from the imported call...");
 }
