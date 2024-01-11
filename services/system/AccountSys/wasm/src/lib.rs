1e109a0d87eda56a594abb49b7c8d7de13eefaab
John Williamson <johnjwilliamson@live.com>
2024-01-11 08:02:54 +0000

    parent 45dff04cb7ff4399401c6526f193800f12ac8472 author John Williamson <johnjwilliamson@live.com> 1704960174 +0000 committer Mike Manfredi <mike@fractally.com> 1707363731 +0000
    append demo

--- services/system/AccountSys/wasm/src/lib.rs
+++ services/system/AccountSys/wasm/src/lib.rs
@@ -1,5 +1,9 @@
 cargo_component_bindings::generate!();
 
+use bindings::component::account_sys::imports;
+use bindings::component::account_sys::types::Funccallparams;
+use bindings::component::account_sys::types::User;
+
 use bindings::Guest;
 
 struct Component;
@@ -7,6 +11,20 @@ struct Component;
 impl Guest for Component {
     fn numbers(num1: u8, num2: u8, doubleup: bool) -> String {
         let mutiplier: u8 = if doubleup { 2 } else { 1 };
+        // let y = token_sys::transfer("whatever");
+
+        struct Par {
+            service: String,
+            method: String,
+            params: String,
+        }
+        let test_params = Funccallparams {
+            service: "test_service".to_string(),
+            method: "test_method".to_string(),
+            params: "test_params".to_string(),
+        };
+        let _x = imports::add_to_tx(&test_params);
+
         format!(
             "Hello, {} + {} equals {}",
             num1,
@@ -16,7 +34,13 @@ impl Guest for Component {
     }
 
     fn strings(word: String) -> String {
-        bindings::component::account_sys::imports::prnt("Calling prnt from Rust");
+        let test_params = Funccallparams {
+            service: "test_service".to_string(),
+            method: "test_method".to_string(),
+            params: "test_params".to_string(),
+        };
+        let x = imports::funccall(&test_params);
+        println!("The x, 'is a thing.. {:?}", x);
         format!("The word you gave me was {}", word)
     }
 
