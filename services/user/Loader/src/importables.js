3e01541d9aa0b20d9794d690d432b02b2958e844
John Williamson <johnjwilliamson@live.com>
2024-01-31 01:38:06 +0000

    remove log

--- services/user/Loader/src/importables.js
+++ services/user/Loader/src/importables.js
@@ -1,12 +1,10 @@
 import { connectToParent } from "penpal";
 
 export async function funccall(params) {
-  console.log(params, "are received params in importables...");
-  return "yesbuddeh funcCall";
+  return "funcCall";
 }
 
 export function getLoggedInUser() {
-  console.log("I WAS ASKED FOR ALICE");
   return "alice";
 }
 
