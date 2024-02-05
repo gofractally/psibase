e5f0735e0f78ae0247041651d64590f3a95d6b0d
Mike Manfredi <mike@fractally.com>
2024-02-05 23:04:29 +0000

    fixing submodule issue with wasmbuilder.app and adding SupervisorSys_js entry in build file

---
+++ services/system/AccountSys/wasm/target/release/deps/cargo_component_macro-4747a450b6802ce5.d
@@ -0,0 +1,7 @@
+/root/psibase/services/system/AccountSys/wasm/target/release/deps/libcargo_component_macro-4747a450b6802ce5.so: /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/cargo-component-macro-0.6.0/src/lib.rs
+
+/root/psibase/services/system/AccountSys/wasm/target/release/deps/cargo_component_macro-4747a450b6802ce5.d: /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/cargo-component-macro-0.6.0/src/lib.rs
+
+/opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/cargo-component-macro-0.6.0/src/lib.rs:
+
+# env-dep:CARGO_TARGET_DIR=/root/psibase/services/system/AccountSys/wasm/target
---
+++ services/system/AccountSys/wasm/target/wasm32-wasi/release/deps/cargo_component_bindings-32afe6144b08c48e.d
@@ -0,0 +1,7 @@
+/root/psibase/services/system/AccountSys/wasm/target/wasm32-wasi/release/deps/cargo_component_bindings-32afe6144b08c48e.rmeta: /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/cargo-component-bindings-0.6.0/src/lib.rs
+
+/root/psibase/services/system/AccountSys/wasm/target/wasm32-wasi/release/deps/libcargo_component_bindings-32afe6144b08c48e.rlib: /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/cargo-component-bindings-0.6.0/src/lib.rs
+
+/root/psibase/services/system/AccountSys/wasm/target/wasm32-wasi/release/deps/cargo_component_bindings-32afe6144b08c48e.d: /opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/cargo-component-bindings-0.6.0/src/lib.rs
+
+/opt/cargo/registry/src/index.crates.io-6f17d22bba15001f/cargo-component-bindings-0.6.0/src/lib.rs:
