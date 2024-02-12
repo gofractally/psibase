b19c1ee9abc1d9e6eb568c5cfa37664839e907e5
Mike Manfredi <mike@fractally.com>
2024-02-12 19:39:55 +0000

    PoC code removed from Account/TokenSys

--- services/user/DemoApp1/ui/src/App.tsx
+++ services/user/DemoApp1/ui/src/App.tsx
@@ -1,23 +1,30 @@
 import "./App.css";
-import { connect } from "@psibase/plugin";
+import React, { useState } from "react";
+
+import { Supervisor } from "@messaging";
+
+const supervisor = new Supervisor();
 
 function App() {
+  const [res, setRes] = useState("Empty");
+
   const run = async () => {
-    const res = await connect();
     console.info("DemoApp1 connected to Supervisor");
 
     console.log("calling demoapp2.callintoplugin");
-    const back = await res.functionCall({
+    const _res = await supervisor.functionCall({
       service: "demoapp2",
       method: "callintoplugin",
       params: [],
     });
-    console.log("demosapp2.callintoplugin() returned:", back);
+    console.log("demosapp2.callintoplugin() returned:", _res);
+    setRes(res as string);
   };
 
   return (
     <>
       <h1>Psibase Demo App 1</h1>
+      <h3>{res}</h3>
       <div className="card">
         <button onClick={() => run()}>Say Hello</button>
       </div>
--- services/user/DemoApp1/ui/vite.config.ts
+++ services/user/DemoApp1/ui/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+        "@messaging": path.resolve(
+            "../../../user/CommonSys/common/messaging"
+        ),
+    },
+},
 })
