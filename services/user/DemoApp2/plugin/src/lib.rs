bf72c164cd81ef91bd5a7cd3826fe8d51323441c
Brandon Fancher <brandonfancher@gmail.com>
2024-02-13 19:59:48 +0000

    DemoApp2 proper round trip testing.

--- services/user/DemoApp2/plugin/src/lib.rs
+++ services/user/DemoApp2/plugin/src/lib.rs
@@ -1,6 +1,6 @@
 mod bindings;
 
-// use bindings::component::demoapp2::imports;
+use bindings::component::demoapp2::imports;
 // use bindings::component::demoapp2::types::Funccallparams;
 
 use bindings::Guest;
@@ -9,6 +9,8 @@ struct Component;
 
 impl Guest for Component {
     fn callintoplugin() -> String {
-        "DerpCasey".to_string()
+        let logged_in_user = imports::get_logged_in_user();
+        println!("logged_in_user x, 'is a thing.. {:?}", logged_in_user);
+        return logged_in_user + " is logged in";
     }
 }
