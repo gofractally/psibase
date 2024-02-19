f154cee11d150e4902111bfe652ec3d6c7b8c913
John Williamson <johnjwilliamson@live.com>
2024-02-19 05:05:58 +0000

    tidy up

--- services/user/DemoApp1/wasmbuilder.app/src/app.tsx
+++ services/user/DemoApp1/wasmbuilder.app/src/app.tsx
@@ -4,7 +4,6 @@ import { $init, provider } from "./graph";
 const App = () => {
   const run = async () => {
     $init.then(async () => {
-      console.log("f");
       const Graph = new provider.Graph();
 
       if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
@@ -14,12 +13,9 @@ const App = () => {
       const url =
         "https://account-sys.psibase.127.0.0.1.sslip.io:8080/loader/plugin.wasm";
       const file = await fetch(url);
-      console.log(file, "was received");
       const arrayBuffer = await file.arrayBuffer();
-      console.log({ arrayBuffer });
       const bytes = new Uint8Array(arrayBuffer);
 
-      console.log(Graph, "is the graph...");
       const component = Graph.addComponent("hello", bytes);
 
       console.log({ array: bytes, component });
