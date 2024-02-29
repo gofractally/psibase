ac4d8f98034c005b8f2105b95b6b1f81b8caa25b
James-Mart <jmartx93@gmail.com>
2024-02-29 20:46:37 +0000

    update rust scripts

--- rust/scripts/publish.sh
+++ rust/scripts/publish.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Run with 
+#   `./scripts/publish.sh`
+# Can also dry-run with:
+#   `./scripts/publish.sh --dry-run`
+
 ###### 
 #   ~ Dependencies ~
 #
@@ -13,7 +18,7 @@
 # Dependencies above imply the following publish order:
 dirs=(psibase_names psibase_macros fracpack psibase cargo-psibase)
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")/../"
 
 # Ensure each crate builds
 for dir in "${dirs[@]}"; do
@@ -27,14 +32,6 @@ done
 for dir in "${dirs[@]}"; do
     cd "$dir"
     echo "Publishing $dir..."
-
-    # psibase crate needs `--allow-dirty` to be published because the boot-image directory
-    #   is copied into the project directory but is not added to git.
-    extra_flags=""
-    if [ "$dir" = "psibase" ]; then
-        extra_flags="--allow-dirty"
-    fi
-
-    cargo publish $extra_flags
+    cargo publish $1
     cd ..
 done
---
+++ rust/scripts/set-version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run with 
+#   `./scripts/set-version.sh x.y.z`
+# Can also dry-run with:
+#   `./scripts/set-version.sh x.y.z --dry-run`
+
+cargo set-version $1 --workspace
