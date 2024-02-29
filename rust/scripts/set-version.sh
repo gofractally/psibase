f7323e7d3f8f0e639936ac545bdee9b5747e87e8
James-Mart <jmartx93@gmail.com>
2024-02-29 20:51:09 +0000

    upgrade rust packages to version 0.3.0 for publishing

--- rust/scripts/set-version.sh
+++ rust/scripts/set-version.sh
@@ -3,6 +3,6 @@
 # Run with 
 #   `./scripts/set-version.sh x.y.z`
 # Can also dry-run with:
-#   `./scripts/set-version.sh x.y.z --dry-run`
+#   `./scripts/set-version.sh "x.y.z --dry-run"`
 
 cargo set-version $1 --workspace
