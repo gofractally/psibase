596d4f896ed91a5365eb30f1b63fc21b22532dba
Brandon Fancher <brandonfancher@gmail.com>
2024-02-29 21:36:59 +0000

    Remove broken InviteSys app.

--- services/user/InviteSys/ui/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Button } from './Button';
-export { Input, Form } from './Form';
-export { Text } from './Text';
-export { Icon } from './Icon'
-export { Heading } from './Heading'
--- services/user/InviteSys/ui/src/store/queries/isAccountTaken.ts
+++
@@ -1,9 +0,0 @@
-const wait = (ms: number = 500) =>
-  new Promise((resolve) => setTimeout(resolve, ms));
-
-export const isAccountAvailable = async (
-  accountName: string
-): Promise<boolean> => {
-  await wait(100);
-  return true;
-};
