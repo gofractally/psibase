d97e0cd89db9b8d30ff68560254ee571e6ba2485
James-Mart <jmartx93@gmail.com>
2024-03-03 22:48:21 +0000

    add accounts plugin

---
+++ services/system/AccountSys/plugin/src/lib.rs
@@ -0,0 +1,22 @@
+#[allow(warnings)]
+mod bindings;
+
+use bindings::exports::accounts::plugin::accounts::Guest;
+
+struct Component;
+
+impl Guest for Component {
+    fn get_logged_in_user() -> Result<String, String> { 
+        Ok("alice".to_string())
+    }
+
+    fn get_available_accounts() -> Result<Vec<String>, String> { 
+        Ok(vec!["alice".to_string(), "bob".to_string()])
+    }
+
+    fn add_auth_service(_: String) -> Result<(), String> { 
+        Err("Not yet implemented".to_string())
+    }
+}
+
+bindings::export!(Component with_types_in bindings);
