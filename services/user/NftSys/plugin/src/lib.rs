771803bf481917b2266292d4a87d6e81c8604de7
Mike Manfredi <mike@fractally.com>
2024-03-08 09:23:14 +0000

    compiling again with namespace ambiguity resolved and notes about what makes it ambiguous

--- services/user/NftSys/plugin/src/lib.rs
+++ services/user/NftSys/plugin/src/lib.rs
@@ -1,11 +1,20 @@
 #[allow(warnings)]
 mod bindings;
 
-use bindings::common::plugin;
+// NOTE: I critically avoided importing this as common::plugin
+// to avoid an ambiguous name "plugin" (with nft_sys::plugin).
+// This will be a massive source of confusion, given
+// every plugin import will have "plugin" in the name.
+// AND given we're mixing 1) services, 2) plugins, and 3) psibase,
+// we're going to have redundant types, which *must* be clearly referenced
+// to avoid confusion, e.g., NID...
+// AND at least so far in my example, "nft_sys" refers to BOTH
+// the service and the plugin...
+use bindings::common;
 use bindings::exports::nft_sys::plugin::nfts;
 // use nft_sys::*;
 use psibase::services::nft_sys::NID;
-use psibase::AccountNumber;
+// use psibase::AccountNumber;
 use serde::Serialize;
 
 struct Nfts;
@@ -17,7 +26,7 @@ impl nfts::Guest for Nfts {
         struct MintArgs {}
         let mint_args = MintArgs {};
 
-        let _ = plugin::server::add_action_to_transaction(
+        let _ = common::plugin::server::add_action_to_transaction(
             "nft-sys",
             "mint",
             serde_json::to_string(&mint_args)
@@ -35,7 +44,7 @@ impl nfts::Guest for Nfts {
         }
         let burn_args = BurnArgs { nft_id: nft_id };
 
-        let _ = plugin::server::add_action_to_transaction(
+        let _ = common::plugin::server::add_action_to_transaction(
             "nft-sys",
             "burn",
             serde_json::to_string(&burn_args)
@@ -45,8 +54,10 @@ impl nfts::Guest for Nfts {
         Ok(())
     }
 
+    // TODO: how can we make types from wit vs service super obviously different?
+    // The AccountNumber keyword was ambiguously coming from 2 places
     fn credit(_nft_id: NID, receiver: nfts::AccountNumber, _memo: String) -> Result<(), String> {
-        let _receiver = AccountNumber::new(receiver);
+        let _receiver = psibase::AccountNumber::new(receiver);
         Ok(())
     }
 
