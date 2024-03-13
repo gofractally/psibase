f8f113d4872e8277929ffb1fe0e756ef924663aa
Mike Manfredi <mike@fractally.com>
2024-03-13 15:37:33 +0000

    adding rest of plugin functions

--- services/user/NftSys/plugin/src/lib.rs
+++ services/user/NftSys/plugin/src/lib.rs
@@ -1,32 +1,36 @@
 #[allow(warnings)]
 mod bindings;
 
-// NOTE: I critically avoided importing this as common::plugin
-// to avoid an ambiguous name "plugin" (with nft_sys::plugin).
-// This will be a massive source of confusion, given
-// every plugin import will have "plugin" in the name.
-// AND given we're mixing 1) services, 2) plugins, and 3) psibase,
-// we're going to have redundant types, which *must* be clearly referenced
-// to avoid confusion, e.g., NID...
-// AND at least so far in my example, "nft_sys" refers to BOTH
-// the service and the plugin...
-use bindings::common;
-use bindings::exports::nft_sys::plugin::nfts;
-// use nft_sys::*;
-use psibase::services::nft_sys::NID;
-// use psibase::AccountNumber;
-use serde::Serialize;
+/* NOTE: I critically avoided importing this as common::plugin
+to avoid an ambiguous name "plugin" (with nft_sys::plugin).
+This will be a massive source of confusion, given
+every plugin import will have "plugin" in the name.
+AND given we're mixing 1) services, 2) plugins, and 3) psibase,
+we're going to have redundant types, which *must* be clearly referenced
+to avoid confusion, e.g., NID...
+AND at least so far in my example, "nft_sys" refers to BOTH
+the service and the plugin... */
+
+use std::str::FromStr;
+
+use bindings::common::plugin as CommonPlugin;
+use bindings::exports::nft_sys::plugin as NftSysPlugin;
+
+// This line is the locally-compiled service
+// but doesn't include type NID
+// use nft_sys as NftSysService;
+
+// This is the public crate, which includes type NID
+// but *obviously* isn't being recompiled locally
+// Will only happen with system services.
+use psibase::services::nft_sys as NftSysService;
 
 struct Nfts;
 
-impl nfts::Guest for Nfts {
+impl NftSysPlugin::nfts::Guest for Nfts {
     fn mint(_nft_label: String) -> Result<(), String> {
-        // Err("Not implemented".to_string())
-        #[derive(Serialize)]
-        struct MintArgs {}
-        let mint_args = MintArgs {};
-
-        let _ = common::plugin::server::add_action_to_transaction(
+        let mint_args = NftSysService::action_structs::mint {};
+        let _ = CommonPlugin::server::add_action_to_transaction(
             "nft-sys",
             "mint",
             serde_json::to_string(&mint_args)
@@ -37,14 +41,13 @@ impl nfts::Guest for Nfts {
         Ok(())
     }
 
-    fn burn(nft_id: NID) -> Result<(), String> {
-        #[derive(Serialize)]
-        struct BurnArgs {
-            nft_id: NID,
-        }
-        let burn_args = BurnArgs { nft_id: nft_id };
+    fn burn(nft_id: NftSysPlugin::types::Nid) -> Result<(), String> {
+        // NftSysService::
+        // NftSysService. ;
+        // NftSysService::action_structs::burn:: ;
+        let burn_args = NftSysService::action_structs::burn { nftId: nft_id };
 
-        let _ = common::plugin::server::add_action_to_transaction(
+        let _ = CommonPlugin::server::add_action_to_transaction(
             "nft-sys",
             "burn",
             serde_json::to_string(&burn_args)
@@ -56,18 +59,110 @@ impl nfts::Guest for Nfts {
 
     // TODO: how can we make types from wit vs service super obviously different?
     // The AccountNumber keyword was ambiguously coming from 2 places
-    fn credit(_nft_id: NID, receiver: nfts::AccountNumber, _memo: String) -> Result<(), String> {
-        let _receiver = psibase::AccountNumber::new(receiver);
+    fn credit(
+        nft_id: NftSysPlugin::types::Nid,
+        receiver: String,
+        memo: String,
+    ) -> Result<(), String> {
+        let rcvr = match psibase::AccountNumber::from_str(&receiver) {
+            Ok(acct) => match acct.value {
+                0 => return Err("{} is not a valid account.".to_string()),
+                _ => acct,
+            },
+            Err(e) => return Err(e.to_string()),
+        };
+        let credit_args = NftSysService::action_structs::credit {
+            nftId: nft_id,
+            receiver: rcvr,
+            memo,
+        };
+        // let bob = match serde_json::to_string(&credit_args) {
+        //     Ok(str) => str,
+        //     Err(e) => return Err(e.to_string()),
+        // };
+        // CommonPlugin::server::add_action_to_transaction("nft-sys", "credit", &bob);
+
+        CommonPlugin::server::add_action_to_transaction(
+            "nft-sys",
+            "credit",
+            &(match serde_json::to_string(&credit_args) {
+                Ok(str) => str,
+                Err(e) => return Err(e.to_string()),
+            }),
+        )
+
+        // CommonPlugin::server::add_action_to_transaction(
+        //     "nft-sys",
+        //     "credit",
+        //     &(match serde_json::to_string(&credit_args) {
+        //         Ok(str) => {
+        //             let bob = str.to_owned();
+        //             bob
+        //         }
+        //         Err(e) => return Err(e.to_string()),
+        //     }),
+        // )
+    }
+
+    fn uncredit(nft_id: NftSysPlugin::types::Nid, memo: String) -> Result<(), String> {
+        let uncredit_args = NftSysService::action_structs::uncredit {
+            nftId: nft_id,
+            memo,
+        };
+
+        CommonPlugin::server::add_action_to_transaction(
+            "nft-sys",
+            "uncredit",
+            &(match serde_json::to_string(&uncredit_args) {
+                Ok(str) => str,
+                Err(e) => return Err(e.to_string()),
+            }),
+        )
+    }
+
+    fn debit(nft_id: NftSysPlugin::types::Nid, memo: String) -> Result<(), String> {
+        let debit_args = NftSysService::action_structs::uncredit {
+            nftId: nft_id,
+            memo,
+        };
+
+        CommonPlugin::server::add_action_to_transaction(
+            "nft-sys",
+            "uncredit",
+            &(match serde_json::to_string(&debit_args) {
+                Ok(str) => str,
+                Err(e) => return Err(e.to_string()),
+            }),
+        )
+    }
+
+    fn get_nft(_nft_id: NftSysPlugin::types::Nid) -> Result<(), NftSysPlugin::types::NftRecord> {
         Ok(())
     }
 
-    fn uncredit(_nft_id: NID, _memo: String) -> Result<(), String> {
+    fn get_nft_holder(
+        _account: NftSysPlugin::types::AccountNumber,
+    ) -> Result<(), NftSysPlugin::types::NftHolderRecord> {
         Ok(())
     }
 
-    fn debit(_nft_id: NID, _memo: String) -> Result<(), String> {
+    fn get_cred_record(
+        _nft_id: NftSysPlugin::types::Nid,
+    ) -> Result<(), NftSysPlugin::types::CreditRecord> {
         Ok(())
     }
+
+    fn exists(_nft_id: NftSysPlugin::types::Nid) -> Result<(), bool> {
+        Ok(())
+    }
+
+    // fn getUserConf(account: AccountNumber, flag: NamedBit) -> bool {
+    //     unimplemented!()
+    // }
+
+    // fn setUserConf(flag: NamedBit, enable: bool) {
+    //     unimplemented!()
+    // }
 }
 
 bindings::export!(Nfts with_types_in bindings);
