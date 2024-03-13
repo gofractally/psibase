1e53f139187da921acc63fe82460e3164fb83647
Mike Manfredi <mike@fractally.com>
2024-03-13 16:11:38 +0000

    adding Ui file

--- services/user/NftSys/ui/src/App.tsx
+++ services/user/NftSys/ui/src/App.tsx
@@ -1,8 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { Supervisor } from "@psibase/common-lib/messaging";
 
 function App() {
     const [count, setCount] = useState(0);
+    const [res, setRes] = useState("Empty");
+
+    const init = async () => {
+        const supervisor = new Supervisor();
+        await supervisor.onLoaded();
+        supervisor.preLoadServices(["nft-sys"]);
+    };
+
+    useEffect(() => {
+        init();
+        console.info("nft-sys connected to Supervisor");
+    }, []);
+
+    const supervisor = new Supervisor();
+    const mintNft = async () => {
+        if (!supervisor.onLoaded()) return;
+
+        console.log("calling nft-sys.callintoplugin");
+        const res = await supervisor.functionCall({
+            service: "nft-sys",
+            method: "mint",
+            params: [],
+        });
+        setRes(res as string);
+    };
 
     return (
         <>
@@ -14,6 +40,8 @@ function App() {
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
+                <button onClick={mintNft}>mint</button>
+                <pre>{JSON.stringify(res, null, 2)}</pre>
             </div>
         </>
     );
