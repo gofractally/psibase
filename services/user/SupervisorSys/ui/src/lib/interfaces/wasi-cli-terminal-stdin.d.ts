10879004d4c0442971dac4a3d2f6ad6a05ba27c3
James-Mart <jmartx93@gmail.com>
2024-03-23 18:42:11 +0000

    remove unneeded files from supervisor

--- services/user/SupervisorSys/ui/src/lib/interfaces/wasi-cli-terminal-stdin.d.ts
+++
@@ -1,5 +0,0 @@
-export namespace WasiCliTerminalStdin {
-  export function getTerminalStdin(): TerminalInput | undefined;
-}
-import type { TerminalInput } from '../interfaces/wasi-cli-terminal-input.js';
-export { TerminalInput };
--- services/user/SupervisorSys/ui/src/lib/interfaces/wasi-filesystem-preopens.d.ts
+++
@@ -1,5 +0,0 @@
-export namespace WasiFilesystemPreopens {
-  export function getDirectories(): [Descriptor, string][];
-}
-import type { Descriptor } from '../interfaces/wasi-filesystem-types.js';
-export { Descriptor };
