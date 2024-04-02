fd2b9696481ac7ebaf5e7d8b3b168681bc7be583
John Williamson <johnjwilliamson@live.com>
2024-04-02 10:00:45 +0000

    remove use client

--- services/user/CommonSys/ui/src/components/ui/3d-pin.tsx
+++ services/user/CommonSys/ui/src/components/ui/3d-pin.tsx
@@ -1,7 +1,6 @@
-"use client";
-import React, { useState } from "react";
-import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
 
 export const PinContainer = ({
     children,
