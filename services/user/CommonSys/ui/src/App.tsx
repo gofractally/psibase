6fea760090863c240f8eff3b7cd01634a56dacc8
John Williamson <johnjwilliamson@live.com>
2024-04-02 09:53:30 +0000

    install shadcn

--- services/user/CommonSys/ui/src/App.tsx
+++ services/user/CommonSys/ui/src/App.tsx
@@ -1,19 +1,22 @@
-import { siblingUrl } from "@psibase/common-lib";
+import { Nav } from "@/components/nav";
+import { AppItem } from "@/components/app-item";
 
 const App = () => {
     return (
-        <div className="flex h-screen w-screen flex-col">
-            Under construction...
-            <ul>
-                <li>
-                    <a href={siblingUrl(null, "explore-sys", null, false)}>
-                        Explorer
-                    </a>
-                </li>
-                <li>
-                    <a href={siblingUrl(null, "doc-sys", null, false)}>Docs</a>
-                </li>
-            </ul>
+        <div className="mx-auto h-screen w-screen max-w-screen-lg">
+            <Nav />
+            <div className="flex flex-col sm:flex-row">
+                <AppItem
+                    name="Explorer"
+                    description="Explore recent transactions and chain history."
+                    service="explore"
+                />
+                <AppItem
+                    name="Doc"
+                    description="Review blockchain documentation."
+                    service="doc"
+                />
+            </div>
         </div>
     );
 };
