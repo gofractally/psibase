b228818919b0e5ad72a1ef0dbc1d21114fbff991
John Williamson <johnjwilliamson@live.com>
2024-04-22 06:17:38 +0000

    rename service import

--- dev/DemoApp1/plugin/src/lib.rs
+++ dev/DemoApp1/plugin/src/lib.rs
@@ -6,8 +6,8 @@ use bindings::exports::demoapp1::example::intf::Guest as Intf;
 use bindings::invite;
 use bindings::Guest as MainInterface;
 
-use example::action_structs as demoapp1_actions;
 use psibase::fracpack::Pack;
+use service::action_structs;
 
 struct Component;
 
@@ -25,7 +25,7 @@ impl Intf for Component {
     fn multiply(a: u32, b: u32) -> Result<String, CommonTypes::Error> {
         let res = server::add_action_to_transaction(
             "multiply",
-            &demoapp1_actions::multiply { a, b }.packed(),
+            &action_structs::multiply { a, b }.packed(),
         );
 
         Ok(format!("Mutliply res is {:?}", res))
