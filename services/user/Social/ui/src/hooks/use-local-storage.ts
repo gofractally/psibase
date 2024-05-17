be8d953131c4185c5cf6e4b583bf3adf5746a585
Brandon Fancher <brandonfancher@gmail.com>
2024-05-17 19:47:42 +0000

    Finish getting starting point working right.

---
+++ services/user/Social/ui/src/hooks/use-local-storage.ts
@@ -0,0 +1,57 @@
+/* `useLocalStorage`
+ *
+ * Features:
+ *  - JSON Serializing
+ *  - Also value will be updated everywhere, when value updated (via `storage` event)
+ */
+
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(
+    key: string,
+    defaultValue: T,
+): [T, (value: T) => void] {
+    const [value, setValue] = useState(defaultValue);
+
+    useEffect(() => {
+        const item = localStorage.getItem(key);
+        console.log("item", item);
+
+        if (item === undefined || item === null) {
+            console.log("setting");
+            localStorage.setItem(key, JSON.stringify(defaultValue));
+        }
+
+        console.log("setValue:", JSON.parse(item!));
+        setValue(item ? JSON.parse(item) : defaultValue);
+
+        function handler(e: StorageEvent) {
+            if (e.key !== key) return;
+
+            const lsi = localStorage.getItem(key);
+            setValue(JSON.parse(lsi ?? ""));
+        }
+
+        window.addEventListener("storage", handler);
+
+        return () => {
+            window.removeEventListener("storage", handler);
+        };
+    }, []);
+
+    const setValueWrap = (value: T) => {
+        console.log("setValueWrap:", value);
+        try {
+            setValue(value);
+
+            localStorage.setItem(key, JSON.stringify(value));
+            if (typeof window !== "undefined") {
+                window.dispatchEvent(new StorageEvent("storage", { key }));
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    return [value, setValueWrap];
+}
--- services/user/Social/ui/src/main.tsx
+++ services/user/Social/ui/src/main.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 import "./styles/globals.css";
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
