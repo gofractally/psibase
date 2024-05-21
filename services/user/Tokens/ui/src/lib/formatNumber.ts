713ee7a1b12b735011927a177ba4474cec1d9dd0
John Williamson <johnjwilliamson@live.com>
2024-05-21 06:00:47 +0000

    handle isNaN

--- services/user/Tokens/ui/src/lib/formatNumber.ts
+++ services/user/Tokens/ui/src/lib/formatNumber.ts
@@ -1,4 +1,4 @@
 export const formatNumber = (num: number, precision: number = 4) =>
   new Intl.NumberFormat(undefined, {
     maximumFractionDigits: precision,
-  }).format(num);
+  }).format(Number.isNaN(num) ? 0 : num);
