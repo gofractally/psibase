248e119a9129a894f4754b61edea9adf9a9495b0
John Williamson <johnjwilliamson@live.com>
2024-05-21 05:58:36 +0000

    remove lorem

--- services/user/Tokens/ui/src/components/modal-create-token.tsx
+++ services/user/Tokens/ui/src/components/modal-create-token.tsx
@@ -1,7 +1,6 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
@@ -26,12 +25,6 @@ export const ModalCreateToken = ({
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Create a token</DialogTitle>
-          <DialogDescription>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet,
-            beatae autem assumenda nulla, culpa dignissimos aperiam debitis
-            voluptates mollitia a velit. Ducimus impedit esse tempora architecto
-            voluptate sapiente ad quam!
-          </DialogDescription>
           {children}
         </DialogHeader>
       </DialogContent>
