ce5e1a20174e29c12b180fcd8fd9bdc5512fd2fa
John Williamson <johnjwilliamson@live.com>
2024-05-23 07:38:59 +0000

    adopt use of token_code

--- services/user/Tokens/plugin/src/errors.rs
+++ services/user/Tokens/plugin/src/errors.rs
@@ -3,13 +3,15 @@ use crate::bindings::common::plugin::types::{Error, PluginId};
 #[derive(PartialEq, Eq, Hash)]
 pub enum ErrorType {
     QueryError,
+    InvalidTokenCode,
+    TokenIdMismatch,
 }
 
 fn my_plugin_id() -> PluginId {
-    return PluginId {
+    PluginId {
         service: "tokens".to_string(),
         plugin: "plugin".to_string(),
-    };
+    }
 }
 
 impl ErrorType {
@@ -19,6 +21,16 @@ impl ErrorType {
                 code: self as u32,
                 producer: my_plugin_id(),
                 message: format!("Failed to post graphql query: {}", msg),
+            },
+            ErrorType::InvalidTokenCode => Error {
+                code: self as u32,
+                producer: my_plugin_id(),
+                message: format!("Invalid token code: {}", msg),
+            },
+            ErrorType::TokenIdMismatch => Error {
+                code: self as u32,
+                producer: my_plugin_id(),
+                message: format!("Token IDs do not match: {}", msg),
             },
         }
     }
--- services/user/Tokens/plugin/src/lib.rs
+++ services/user/Tokens/plugin/src/lib.rs
@@ -7,19 +7,50 @@ use bindings::exports::component::tokens::{intf::Guest as Intf, transfer::Guest 
 use psibase::services::tokens as Wrapper;
 use psibase::AccountNumber;
 use query::shared_balance::fetch_balances;
+use query::token_detail::fetch_token;
 
 mod errors;
-
+use errors::ErrorType;
 struct Component;
 
 use psibase::fracpack::Pack;
 
 mod query {
     pub mod shared_balance;
-    pub mod token;
+    pub mod token_detail;
 }
 
-use query::token::fetch_precision;
+enum TokenType {
+    id(u32),
+    symbol(String),
+}
+
+fn identify_token_type(token_code: String) -> Result<TokenType, CommonTypes::Error> {
+    use TokenType::{id, symbol};
+
+    let first_char = token_code
+        .chars()
+        .next()
+        .ok_or(ErrorType::InvalidTokenCode.err("token code is empty"))?;
+
+    Ok(if first_char.is_ascii_digit() {
+        id(token_code
+            .parse::<u32>()
+            .map_err(|_| ErrorType::InvalidTokenCode.err("failed to parse token_code to u32"))?)
+    } else {
+        symbol(token_code)
+    })
+}
+
+fn token_code_to_id(token_id: Wit::TokenCode) -> Result<u32, CommonTypes::Error> {
+    let parsed = identify_token_type(token_id)?;
+    match parsed {
+        TokenType::id(id) => Ok(id),
+        TokenType::symbol(_) => {
+            unimplemented!("Dunno how to lookup a token by symbol")
+        }
+    }
+}
 
 impl Intf for Component {
     fn create(
@@ -37,19 +68,19 @@ impl Intf for Component {
     }
 
     fn burn(
-        tokenId: Wit::Tid,
+        code: Wit::TokenCode,
         amount: Wit::Quantity,
         memo: String,
         account: Wit::AccountNumber,
     ) -> Result<(), CommonTypes::Error> {
-        let precision = fetch_precision(tokenId).expect("failed to fetch precision");
+        let token = fetch_token(token_code_to_id(code)?)?;
 
         if (account.len() as u8) == 0 {
             server::add_action_to_transaction(
                 "burn",
                 &Wrapper::action_structs::burn {
-                    tokenId,
-                    amount: Wrapper::Quantity::new(amount.as_str(), precision),
+                    tokenId: token.id,
+                    amount: Wrapper::Quantity::new(amount.as_str(), token.precision),
                 }
                 .packed(),
             )
@@ -57,8 +88,8 @@ impl Intf for Component {
             server::add_action_to_transaction(
                 "recall",
                 &Wrapper::action_structs::recall {
-                    tokenId,
-                    amount: Wrapper::Quantity::new(amount.as_str(), precision),
+                    tokenId: token.id,
+                    amount: Wrapper::Quantity::new(amount.as_str(), token.precision),
                     from: AccountNumber::from(account.as_str()),
                     memo,
                 }
@@ -68,17 +99,18 @@ impl Intf for Component {
     }
 
     fn mint(
-        tokenId: Wit::Tid,
+        code: Wit::TokenCode,
         amount: Wit::Quantity,
         memo: String,
     ) -> Result<(), CommonTypes::Error> {
-        let precision = fetch_precision(tokenId).expect("failed to fetch precision");
+        let token = fetch_token(token_code_to_id(code)?)?;
+
         server::add_action_to_transaction(
             "mint",
             &Wrapper::action_structs::mint {
-                amount: Wrapper::Quantity::new(amount.as_str(), precision),
+                amount: Wrapper::Quantity::new(amount.as_str(), token.precision),
                 memo,
-                tokenId,
+                tokenId: token.id,
             }
             .packed(),
         )
@@ -86,6 +118,18 @@ impl Intf for Component {
 }
 
 impl Transfer for Component {
+    fn token_owner(code: Wit::TokenCode) -> Result<Wit::TokenDetail, CommonTypes::Error> {
+        let token_id = token_code_to_id(code)?;
+        let res = fetch_token(token_id)?;
+
+        Ok(Wit::TokenDetail {
+            id: res.id,
+            owner: res.owner,
+            precision: res.precision,
+            symbol_id: res.symbol_id,
+        })
+    }
+
     fn balances() -> Result<Vec<Wit::Balance>, CommonTypes::Error> {
         Ok(fetch_balances()
             .unwrap()
@@ -100,17 +144,18 @@ impl Transfer for Component {
     }
 
     fn uncredit(
-        token: Wit::Tid,
+        code: Wit::TokenCode,
         debitor: Wit::AccountNumber,
         amount: Wit::Quantity,
         memo: String,
     ) -> Result<(), CommonTypes::Error> {
-        let precision = fetch_precision(token).expect("failed to fetch precision");
-        let quantity = Wrapper::Quantity::new(amount.as_str(), precision);
+        let fetched_token = fetch_token(token_code_to_id(code)?)?;
+        let quantity = Wrapper::Quantity::new(amount.as_str(), fetched_token.precision);
+
         server::add_action_to_transaction(
             "uncredit",
             &Wrapper::action_structs::uncredit {
-                tokenId: token,
+                tokenId: fetched_token.id,
                 memo,
                 maxAmount: quantity,
                 receiver: AccountNumber::from(debitor.as_str()),
@@ -118,23 +163,24 @@ impl Transfer for Component {
             .packed(),
         )
         .expect("failed to add action to tx");
+
         Ok(())
     }
 
     fn credit(
-        token: Wit::Tid,
+        code: Wit::TokenCode,
         receiver: Wit::AccountNumber,
         amount: Wit::Quantity,
         memo: String,
     ) -> Result<(), CommonTypes::Error> {
-        let precision = fetch_precision(token).expect("failed to fetch precision");
+        let fetched_token = fetch_token(token_code_to_id(code)?)?;
 
         server::add_action_to_transaction(
             "credit",
             &Wrapper::action_structs::credit {
-                amount: Wrapper::Quantity::new(amount.as_str(), precision),
+                amount: Wrapper::Quantity::new(amount.as_str(), fetched_token.precision),
                 receiver: AccountNumber::from(receiver.as_str()),
-                tokenId: token,
+                tokenId: fetched_token.id,
                 memo,
             }
             .packed(),
