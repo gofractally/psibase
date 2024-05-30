f865374756bc49ace9cad74ddbebeca910c19019
John Williamson <johnjwilliamson@live.com>
2024-05-30 06:18:49 +0000

    style nav-header

--- services/user/XAdmin/ui/src/components/nav-header.tsx
+++ services/user/XAdmin/ui/src/components/nav-header.tsx
@@ -3,31 +3,30 @@ interface NavHeaderProps {
     activeItem: string;
 }
 
-export const NavHeader = ({
-    menuItems,
-    activeItem: currentNav,
-}: NavHeaderProps) => {
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export const NavHeader = ({ menuItems, activeItem }: NavHeaderProps) => {
     return (
         <Header>
             <LogoGroup>
                 <img src="/psibase.svg" alt="Psibase Logo" />
             </LogoGroup>
             <LogoTitle />
-            <NavGroup>
-                {menuItems.map((item) => (
-                    <NavItem
-                        key={item}
-                        label={item}
-                        isActive={currentNav === item}
-                    />
-                ))}
-            </NavGroup>
+            <Tabs value={activeItem} className="w-[400px]">
+                <TabsList>
+                    {menuItems.map((item) => (
+                        <TabsTrigger asChild key={item} value={item}>
+                            <a href={`#${item}`}>{item}</a>
+                        </TabsTrigger>
+                    ))}
+                </TabsList>
+            </Tabs>
         </Header>
     );
 };
 
 const Header = ({ children }: { children: React.ReactNode }) => (
-    <header className="mx-auto flex max-w-7xl justify-between">
+    <header className="mx-auto my-4 flex max-w-7xl justify-between">
         {children}
     </header>
 );
@@ -36,30 +35,8 @@ const LogoGroup = ({ children }: { children: React.ReactNode }) => (
     <div className="mr-12 flex items-center">{children}</div>
 );
 
-const LogoTitle = () => <h1 className="ml-4 text-2xl">Admin Panel</h1>;
-
-const NavGroup = ({ children }: { children: React.ReactNode }) => (
-    <nav className="flex space-x-4">{children}</nav>
+const LogoTitle = () => (
+    <h1 className=" scroll-m-20 text-4xl font-extrabold tracking-tight">
+        Admin Panel
+    </h1>
 );
-
-interface NavItemProps {
-    label: string;
-    onClick?: () => void;
-    isActive?: boolean;
-}
-
-const NavItem = ({ label, isActive }: NavItemProps) => {
-    const classBase =
-        "rounded-md px-3 py-2 text-sm font-bold text-gray-500 no-underline ";
-    const className =
-        classBase +
-        (isActive ? "text-gray-900 bg-gray-200" : "hover:underline");
-
-    return isActive ? (
-        <div className={className}>{label}</div>
-    ) : (
-        <a href={`#${label}`} className={className}>
-            {label}
-        </a>
-    );
-};
