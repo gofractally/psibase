551e37f929f1580be332b13b42e3175ca7476bd9
Brandon Fancher <brandonfancher@gmail.com>
2024-06-03 21:15:01 +0000

    Style up mermaid block.

--- services/user/Social/ui/src/components/editor/mermaid-block.tsx
+++ services/user/Social/ui/src/components/editor/mermaid-block.tsx
@@ -1,8 +1,10 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useNodeViewContext } from "@prosemirror-adapter/react";
-import * as Tabs from "@radix-ui/react-tabs";
 import mermaid from "mermaid";
 import clsx from "clsx";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@shadcn/tabs";
+import { Button } from "@shadcn/button";
 
 export const MermaidDiagram = ({
     readOnly = false,
@@ -17,7 +19,7 @@ export const MermaidDiagram = ({
     const codeInput = useRef<HTMLTextAreaElement>(null);
 
     const defaultTab = readOnly ? "preview" : "source";
-    const [value, setValue] = useState("preview");
+    const [value, setValue] = useState(defaultTab);
     const rendering = useRef(false);
 
     const renderMermaid = useCallback(async () => {
@@ -44,62 +46,56 @@ export const MermaidDiagram = ({
         });
     }, [renderMermaid, value]);
 
+    if (readOnly) {
+        return (
+            <div
+                ref={codePanel}
+                className={clsx(
+                    "flex justify-center py-3",
+                    value !== "preview" ? "hidden" : "",
+                )}
+            />
+        );
+    }
+
     return (
-        <Tabs.Root
-            contentEditable={false}
-            className={selected ? "ring-2 ring-offset-2" : ""}
-            value={value}
-            onValueChange={(value) => {
-                setValue(value);
-            }}
-        >
-            <Tabs.List className="border-b border-gray-200 text-center text-gray-500 dark:border-gray-700 dark:text-gray-400">
-                <div className="-mb-px flex flex-wrap">
-                    <Tabs.Trigger
-                        value="preview"
+        <div className="rounded-lg border-2 border-dashed border-gray-300 p-2.5">
+            <Tabs
+                contentEditable={false}
+                className={selected ? "ring-2 ring-offset-2" : ""}
+                onValueChange={setValue}
+                value={value}
+            >
+                <TabsList>
+                    <TabsTrigger value="preview">Preview</TabsTrigger>
+                    <TabsTrigger value="source">Source</TabsTrigger>
+                </TabsList>
+                <TabsContent value="preview" forceMount>
+                    <div
+                        ref={codePanel}
                         className={clsx(
-                            "inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300",
-                            value === "preview" ? "text-nord9" : "",
+                            "flex justify-center py-3",
+                            value !== "preview" ? "hidden" : "",
                         )}
+                    />
+                </TabsContent>
+                <TabsContent value="source" className="relative">
+                    <textarea
+                        className="block h-48 w-full bg-slate-800 font-mono text-gray-50"
+                        ref={codeInput}
+                        defaultValue={code}
+                    />
+                    <Button
+                        className="absolute bottom-full right-0 mb-1.5"
+                        onClick={() => {
+                            setAttrs({ value: codeInput.current?.value || "" });
+                            setValue("preview");
+                        }}
                     >
-                        Preview
-                    </Tabs.Trigger>
-                    <Tabs.Trigger
-                        value="source"
-                        className={clsx(
-                            "inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300",
-                            value === "source" ? "text-nord9" : "",
-                        )}
-                    >
-                        Source
-                    </Tabs.Trigger>
-                </div>
-            </Tabs.List>
-            <Tabs.Content value="preview" forceMount>
-                <div
-                    ref={codePanel}
-                    className={clsx(
-                        "flex justify-center py-3",
-                        value !== "preview" ? "hidden" : "",
-                    )}
-                />
-            </Tabs.Content>
-            <Tabs.Content value="source" className="relative">
-                <textarea
-                    className="block h-48 w-full bg-slate-800 font-mono text-gray-50"
-                    ref={codeInput}
-                    defaultValue={code}
-                />
-                <button
-                    className="bg-nord8 dark:bg-nord9 absolute bottom-full right-0 mb-1 inline-flex items-center justify-center rounded border border-gray-600 px-6 py-2 text-base font-medium leading-6 text-gray-50 shadow-sm hover:bg-blue-200 focus:ring-2 focus:ring-offset-2"
-                    onClick={() => {
-                        setAttrs({ value: codeInput.current?.value || "" });
-                        setValue("preview");
-                    }}
-                >
-                    OK
-                </button>
-            </Tabs.Content>
-        </Tabs.Root>
+                        Save
+                    </Button>
+                </TabsContent>
+            </Tabs>
+        </div>
     );
 };
