ebf8e0afba765c7b0148d9fb660e3a770d20c0cd
Brandon Fancher <brandonfancher@gmail.com>
2024-06-05 15:58:45 +0000

    Editor selection plugin for control bar active button states.

--- services/user/Social/ui/src/components/markdown-editor.tsx
+++ services/user/Social/ui/src/components/markdown-editor.tsx
@@ -1,11 +1,13 @@
 import type { MilkdownPlugin } from "@milkdown/ctx";
 
 import { useMemo } from "react";
+import { atom, useSetAtom } from "jotai";
 import {
     defaultValueCtx,
     editorViewOptionsCtx,
     Editor as MilkdownEditor,
     rootCtx,
+    schemaCtx,
 } from "@milkdown/core";
 import { $view } from "@milkdown/utils";
 import { nord } from "@milkdown/theme-nord";
@@ -22,12 +24,24 @@ import { history } from "@milkdown/plugin-history";
 import { math, mathBlockSchema } from "@milkdown/plugin-math";
 import { diagram, diagramSchema } from "@milkdown/plugin-diagram";
 import { useNodeViewFactory } from "@prosemirror-adapter/react";
+import { Node, Schema } from "@milkdown/prose/model";
+
+import {
+    selectionCtx,
+    selectionListener,
+} from "@lib/editor/plugin-selection-listener";
 
 import { MathBlock, MermaidDiagram } from "./editor";
 
 import "@milkdown/theme-nord/style.css";
 import "katex/dist/katex.min.css";
 import "../styles/editor.css";
+
+export const editorSelectionAtom = atom<{
+    doc: Node;
+    selection: any;
+    schema: Schema<any, any>;
+} | null>(null);
 
 interface EditorProps {
     initialValue: string;
@@ -42,6 +56,7 @@ export const MarkdownEditor = ({
     readOnly = false,
 }: EditorProps) => {
     const nodeViewFactory = useNodeViewFactory();
+    const setSelection = useSetAtom(editorSelectionAtom);
 
     const mathPlugins: MilkdownPlugin[] = useMemo(() => {
         return [
@@ -67,7 +82,7 @@ export const MarkdownEditor = ({
         ].flat();
     }, [nodeViewFactory]);
 
-    const { get } = useEditor((root) =>
+    useEditor((root) =>
         MilkdownEditor.make()
             .config(nord)
             .config((ctx) => {
@@ -84,12 +99,20 @@ export const MarkdownEditor = ({
                 });
             })
             .config((ctx) => {
+                const slistener = ctx.get(selectionCtx);
+                slistener.selection((_, selection, doc) => {
+                    const schema = ctx.get(schemaCtx);
+                    setSelection({ doc, selection, schema });
+                });
+            })
+            .config((ctx) => {
                 ctx.update(editorViewOptionsCtx, (prev) => ({
                     ...prev,
                     editable: () => !readOnly,
                 }));
             })
             .use(listener)
+            .use(selectionListener)
             .use(commonmark)
             .use(gfm)
             .use(history)
