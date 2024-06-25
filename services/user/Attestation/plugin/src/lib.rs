64ff8d36e7376827ee8d6f599acd21ac91933d46
Mike Manfredi <mike@fractally.com>
2024-06-25 16:49:00 +0000

    everything at least compiles

--- services/user/Attestation/plugin/src/lib.rs
+++ services/user/Attestation/plugin/src/lib.rs
@@ -1,8 +1,9 @@
 #[allow(warnings)]
 mod bindings;
 
-use bindings::common::plugin::types as CommonTypes;
+use bindings::common::plugin::{server as CommonServer, types as CommonTypes};
 use bindings::exports::attestation::plugin::api::Guest;
+// use attestation::AttestationService;
 
 mod errors;
 use errors::ErrorType::*;
@@ -11,8 +12,24 @@ struct AttestationPlugin;
 
 impl Guest for AttestationPlugin {
     /// Say hello!
-    fn attest(identity_type: String, claim: String) -> Result<(), CommonTypes::Error> {
+    fn attest(
+        attestation_type: String,
+        // subject: String,
+        claim: String,
+    ) -> Result<(), CommonTypes::Error> {
         if true {
+            // attestation::action_structs::attest
+            // CommonServer::add_action_to_transaction(
+            //     "attest",
+            //     &AttestationService::action_structs::attest {
+            //     vc: Attestation {
+            //         attester: get_sender(),
+            //         subject,
+            //         claim,
+            //     },
+            //   }.packed(),
+            // );
+            // psibase::services::invite::action_structs::accept { inviteKey: todo!() };
             return Ok(());
         } else {
             return Err(NotYetImplemented.err("add attest fn"));
