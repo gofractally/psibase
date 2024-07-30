378f9b4911e971f53ed87fc7ceb55c07cd7aa533
Mike Manfredi <mike@fractally.com>
2024-07-30 04:04:15 +0000

    tests split out into a separate file

--- services/user/Identity/service/src/lib.rs
+++ services/user/Identity/service/src/lib.rs
@@ -1,7 +1,4 @@
-use psibase::services::http_server;
-use psibase::AccountNumber;
-
-// mod tests;
+mod tests;
 
 /// Identity service to log identity attestations, and provide a graphiql
 /// query interface.
@@ -313,73 +310,4 @@ mod service {
         let table = WebContentTable::new();
         store_content(path, contentType, content, &table).unwrap();
     }
-}
-
-fn test_attest(
-    chain: &psibase::Chain,
-    attester: AccountNumber,
-    subject: String,
-    conf: u8,
-) -> Result<(), psibase::Error> {
-    http_server::Wrapper::push_from(&chain, SERVICE).registerServer(SERVICE);
-    http_server::Wrapper::push_to(&chain, SERVICE).registerServer(SERVICE);
-    let result = crate::Wrapper::push_from(&chain, attester).attest(subject, conf);
-    assert_eq!(result.get()?, ());
-    println!("\n\nTrace:\n{}", result.trace);
-
-    chain.finish_block();
-    Ok(())
-}
-
-#[psibase::test_case(services("identity"))]
-pub fn test_attest_high_conf(chain: psibase::Chain) -> Result<(), psibase::Error> {
-    use serde_json::{json, Value};
-
-    test_attest(
-        &chain,
-        AccountNumber::from("alice"),
-        String::from("bob"),
-        95,
-    )?;
-
-    // TODO: handle 404 the following code causes
-    let reply: Value = chain.graphql(
-        SERVICE,
-        r#"query { allAttestationStats { subject, uniqueAttesters } }"#,
-        // r#"query { allAttestations { attester subject value } }"#,
-    )?;
-    println!("graphql reply: {}", reply);
-    assert_eq!(
-        reply,
-        json!({ "data": { "allAttestationStats": [{"subject": "bob", "uniqueAttesters": 1}] } })
-    );
-
-    Ok(())
-}
-
-#[psibase::test_case(services("identity"))]
-pub fn test_attest_low_conf(chain: psibase::Chain) -> Result<(), psibase::Error> {
-    // use psibase::HttpBody;
-    use serde_json::{json, Value};
-
-    test_attest(
-        &chain,
-        AccountNumber::from("alice"),
-        String::from("bob"),
-        75,
-    )?;
-
-    // TODO: handle 404 the following code causes
-    let reply: Value = chain.graphql(
-        SERVICE,
-        r#"query { allAttestationStats { subject, uniqueAttesters } }"#,
-        // r#"query { allAttestations { attester subject value } }"#,
-    )?;
-    println!("graphql reply: {}", reply);
-    assert_eq!(
-        reply,
-        json!({ "data": { "allAttestationStats": [{"subject": "bob", "uniqueAttesters": 1}] } })
-    );
-
-    Ok(())
 }
