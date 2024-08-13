932bd3caec10f71ab75a8b19318f27bc75e79ad0
Mike Manfredi <mike@fractally.com>
2024-08-13 16:56:18 +0000

    getAccount() addition made in Identity service branch

--- services/system/Accounts/plugin/src/lib.rs
+++ services/system/Accounts/plugin/src/lib.rs
@@ -1,16 +1,30 @@
 #[allow(warnings)]
 mod bindings;
 
-use bindings::host::common::{server as Server, types as CommonTypes};
+use bindings::accounts::plugin::types as AccountTypes;
 use bindings::exports::accounts::plugin::accounts::Guest as Accounts;
+use bindings::host::common::{server as Server, types as CommonTypes};
 use psibase::fracpack::Pack;
 use psibase::services::accounts as AccountsService;
 use psibase::AccountNumber;
+
+use serde::Deserialize;
 
 mod errors;
 use errors::ErrorType::*;
 
 struct AccountsPlugin;
+
+#[derive(Deserialize, Debug)]
+struct ResponseRoot {
+    data: Data,
+}
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Debug)]
+struct Data {
+    getAccount: Option<AccountsService::Account>,
+}
 
 impl Accounts for AccountsPlugin {
     fn login() -> Result<(), CommonTypes::Error> {
@@ -27,6 +41,39 @@ impl Accounts for AccountsPlugin {
 
     fn add_auth_service(_: String) -> Result<(), CommonTypes::Error> {
         Err(NotYetImplemented.err("add_auth_service"))
+    }
+
+    fn get_account(name: String) -> Result<Option<AccountTypes::Account>, CommonTypes::Error> {
+        let query = format!(
+            "query {{ getAccount(account: \"{}\") {{ accountNum, authService, resourceBalance }} }}",
+            AccountNumber::from(name.as_str())
+        );
+
+        let account_result = Server::post_graphql_get_json(&query)
+            .map_err(|e| QueryError.err(&e.message))
+            .and_then(|result| {
+                serde_json::from_str(&result).map_err(|e| QueryError.err(&e.to_string()))
+            });
+
+        let account = account_result.and_then(|response_root: ResponseRoot| {
+            response_root
+                .data
+                .getAccount
+                .ok_or_else(|| InvalidAccountNumber.err(&name))
+        })?;
+
+        if account.accountNum.value != AccountNumber::from_exact(&name).unwrap().value {
+            return Err(InvalidAccountNumber.err(&name));
+        } else {
+            return Ok(Some(AccountTypes::Account {
+                account_num: account.accountNum.to_string(),
+                auth_service: account.authService.to_string(),
+                resource_balance: Some(match account.resourceBalance {
+                    Some(val) => val.value,
+                    None => 0,
+                }),
+            }));
+        }
     }
 
     fn set_auth_service(service_name: String) -> Result<(), CommonTypes::Error> {
