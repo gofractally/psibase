e74ebd19cbca17496e43a746539b2e65a08f6b88
Mike Manfredi <mike@fractally.com>
2024-08-19 22:14:14 +0000

    init creation of plugina nd service

---
+++ services/user/Branding/plugin/src/lib.rs
@@ -0,0 +1,15 @@
+#[allow(warnings)]
+mod bindings;
+
+use bindings::Guest;
+
+struct Component;
+
+impl Guest for Component {
+    /// Say hello!
+    fn hello_world() -> String {
+        "Hello, World!".to_string()
+    }
+}
+
+bindings::export!(Component with_types_in bindings);
