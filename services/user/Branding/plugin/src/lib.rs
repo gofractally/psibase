d1e3223d7927298e94d204d9b51f27f7a3b8677c
Mike Manfredi <mike@fractally.com>
2024-08-20 17:55:24 +0000

    initial plugin & service

--- services/user/Branding/plugin/src/lib.rs
+++ services/user/Branding/plugin/src/lib.rs
@@ -1,15 +1,52 @@
 #[allow(warnings)]
 mod bindings;
 
-use bindings::Guest;
+use bindings::exports::branding::plugin::api::Guest as Api;
+use bindings::exports::branding::plugin::queries::Guest as Queries;
+use bindings::host::common::server as CommonServer;
+use bindings::host::common::types::Error;
 
-struct Component;
+mod errors;
+use errors::ErrorType;
 
-impl Guest for Component {
-    /// Say hello!
-    fn hello_world() -> String {
-        "Hello, World!".to_string()
+struct BrandingPlugin;
+
+impl Api for BrandingPlugin {
+    fn set_chain_name(name: String) {
+        // store name in singleton
+        println!("set_chain_name({})", name);
+    }
+    fn set_logo(logo: Vec<u8>) {
+        // push logo to sites in root so others can reach it by url
+        println!("set_chain_logo({:#?})", logo);
     }
 }
 
-bindings::export!(Component with_types_in bindings);
+#[derive(serde::Deserialize)]
+struct ChainName {
+    name: String,
+}
+#[derive(serde::Deserialize)]
+#[allow(non_snake_case)]
+struct ChainNameData {
+    chainName: ChainName,
+}
+#[derive(serde::Deserialize)]
+struct ChainNameResponse {
+    data: ChainNameData,
+}
+
+impl Queries for BrandingPlugin {
+    fn get_chain_name() -> Result<String, Error> {
+        // return String::from("chainName");
+        let graphql_str = format!("query {{ chainName {{ name }} }}");
+        let summary_val = serde_json::from_str::<ChainNameResponse>(
+            &CommonServer::post_graphql_get_json(&graphql_str)?,
+        )
+        .map_err(|err| ErrorType::QueryResponseParseError.err(err.to_string().as_str()))?;
+
+        Ok(summary_val.data.chainName.name)
+    }
+}
+
+bindings::export!(BrandingPlugin with_types_in bindings);
