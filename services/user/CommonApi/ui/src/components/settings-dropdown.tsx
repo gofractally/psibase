903a5fbccd7a2524a210306e52bc1e644b84bc2f
John Williamson <johnjwilliamson@live.com>
2024-08-21 05:49:53 +0000

    add modal + react query

--- services/user/CommonApi/ui/src/components/settings-dropdown.tsx
+++ services/user/CommonApi/ui/src/components/settings-dropdown.tsx
@@ -22,87 +22,192 @@ import {
     PlusCircle,
     User,
     UserPlus,
+    Copy,
+    RefreshCcw,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./mode-toggle";
+import { useEffect, useState } from "react";
 
-export const SettingsDropdown = () => (
-    <>
-        <ModeToggle />
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { useMutation } from "@tanstack/react-query";
+import { cn } from "@/lib/utils";
+import { toast } from "sonner";
+import { siblingUrl } from "@psibase/common-lib";
 
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="ghost">
-                    <Settings className="h-4 w-4" />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56">
-                <DropdownMenuLabel>Settings</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                    <DropdownMenuItem disabled>
-                        <User className="mr-2 h-4 w-4" />
-                        <span>Profile</span>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem disabled>
-                        <CreditCard className="mr-2 h-4 w-4" />
-                        <span>Billing</span>
-                    </DropdownMenuItem>
-                </DropdownMenuGroup>
-                {false && (
+const wait = (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const generateInvite = async (): Promise<string> => {
+    await wait(2000);
+
+    console.log({ siblingUrl: siblingUrl() });
+    return `${siblingUrl(undefined, undefined, undefined, false)}/derp/fwe`;
+};
+
+export const SettingsDropdown = () => {
+    const {
+        data: inviteLink,
+        isPending,
+        mutate,
+    } = useMutation({
+        mutationFn: () => {
+            return generateInvite();
+        },
+    });
+
+    useEffect(() => {
+        mutate();
+    }, []);
+
+    const onCopyClick = async () => {
+        if ("clipboard" in navigator) {
+            await navigator.clipboard.writeText("test");
+            toast("Copied to clipboard.");
+        } else {
+            toast("Copying failed, not in secure context?");
+        }
+    };
+
+    return (
+        <Dialog>
+            <DialogContent className="sm:max-w-md">
+                <DialogHeader>
+                    <DialogTitle>Share link</DialogTitle>
+                    <DialogDescription>
+                        Anyone who has this link will be able to create an
+                        account.
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="flex items-center space-x-2">
+                    <div className="grid flex-1 gap-2">
+                        <Label htmlFor="link" className="sr-only">
+                            Link
+                        </Label>
+                        <Input
+                            id="link"
+                            className={cn({ italic: isPending })}
+                            value={inviteLink || "Loading"}
+                            readOnly
+                        />
+                    </div>
+                    <Button
+                        type="submit"
+                        size="sm"
+                        className="px-3"
+                        onClick={() => onCopyClick()}
+                    >
+                        <span className="sr-only">Copy</span>
+                        <Copy className="h-4 w-4" />
+                    </Button>
+                    <Button
+                        type="submit"
+                        size="sm"
+                        variant="outline"
+                        className="px-3"
+                        onClick={() => mutate()}
+                    >
+                        <span className="sr-only">Refresh</span>
+                        <RefreshCcw className="h-4 w-4" />
+                    </Button>
+                </div>
+                <DialogFooter className="sm:justify-start">
+                    <DialogClose asChild>
+                        <Button type="button" variant="secondary">
+                            Close
+                        </Button>
+                    </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+
+            <ModeToggle />
+
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost">
+                        <Settings className="h-4 w-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel>Settings</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
                     <DropdownMenuGroup>
-                        <DropdownMenuSub>
-                            <DropdownMenuSubTrigger>
-                                <UserPlus className="mr-2 h-4 w-4" />
-                                <span>Invite users</span>
-                            </DropdownMenuSubTrigger>
-                            <DropdownMenuPortal>
-                                <DropdownMenuSubContent>
-                                    <DropdownMenuItem>
-                                        <Mail className="mr-2 h-4 w-4" />
-                                        <span>Email</span>
-                                    </DropdownMenuItem>
-                                    <DropdownMenuItem>
-                                        <MessageSquare className="mr-2 h-4 w-4" />
-                                        <span>Message</span>
-                                    </DropdownMenuItem>
-                                    <DropdownMenuSeparator />
-                                    <DropdownMenuItem>
-                                        <PlusCircle className="mr-2 h-4 w-4" />
-                                        <span>More...</span>
-                                    </DropdownMenuItem>
-                                </DropdownMenuSubContent>
-                            </DropdownMenuPortal>
-                        </DropdownMenuSub>
+                        <DialogTrigger asChild>
+                            <DropdownMenuItem>
+                                <User className="mr-2 h-4 w-4" />
+                                <span>Create invite</span>
+                            </DropdownMenuItem>
+                        </DialogTrigger>
+                        <DropdownMenuItem disabled>
+                            <CreditCard className="mr-2 h-4 w-4" />
+                            <span>Billing</span>
+                        </DropdownMenuItem>
                     </DropdownMenuGroup>
-                )}
-                <DropdownMenuSeparator />
-                <a
-                    href="https://github.com/gofractally/psibase"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <DropdownMenuItem>
-                        <Github className="mr-2 h-4 w-4" />
-                        <span>GitHub</span>
+                    {false && (
+                        <DropdownMenuGroup>
+                            <DropdownMenuSub>
+                                <DropdownMenuSubTrigger>
+                                    <UserPlus className="mr-2 h-4 w-4" />
+                                    <span>Invite users</span>
+                                </DropdownMenuSubTrigger>
+                                <DropdownMenuPortal>
+                                    <DropdownMenuSubContent>
+                                        <DropdownMenuItem>
+                                            <Mail className="mr-2 h-4 w-4" />
+                                            <span>Email</span>
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            <MessageSquare className="mr-2 h-4 w-4" />
+                                            <span>Message</span>
+                                        </DropdownMenuItem>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem>
+                                            <PlusCircle className="mr-2 h-4 w-4" />
+                                            <span>More...</span>
+                                        </DropdownMenuItem>
+                                    </DropdownMenuSubContent>
+                                </DropdownMenuPortal>
+                            </DropdownMenuSub>
+                        </DropdownMenuGroup>
+                    )}
+                    <DropdownMenuSeparator />
+                    <a
+                        href="https://github.com/gofractally/psibase"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <DropdownMenuItem>
+                            <Github className="mr-2 h-4 w-4" />
+                            <span>GitHub</span>
+                        </DropdownMenuItem>
+                    </a>
+                    <a
+                        href="https://t.me/psibase_developers"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <DropdownMenuItem>
+                            <LifeBuoy className="mr-2 h-4 w-4" />
+                            <span>Support</span>
+                        </DropdownMenuItem>
+                    </a>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem disabled>
+                        <LogOut className="mr-2 h-4 w-4" />
+                        <span>Log out</span>
                     </DropdownMenuItem>
-                </a>
-                <a
-                    href="https://t.me/psibase_developers"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <DropdownMenuItem>
-                        <LifeBuoy className="mr-2 h-4 w-4" />
-                        <span>Support</span>
-                    </DropdownMenuItem>
-                </a>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem disabled>
-                    <LogOut className="mr-2 h-4 w-4" />
-                    <span>Log out</span>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    </>
-);
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </Dialog>
+    );
+};
--- services/user/CommonApi/ui/src/main.tsx
+++ services/user/CommonApi/ui/src/main.tsx
@@ -1,11 +1,19 @@
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@/components/theme-provider";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { Toaster } from "@/components/ui/sonner";
 import App from "./App";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <Toaster />
+        </QueryClientProvider>
     </ThemeProvider>
 );
