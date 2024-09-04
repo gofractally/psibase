511848be021ca82fbe96c39f04c93c42f7f46d31
John Williamson <johnjwilliamson@live.com>
2024-09-04 18:51:11 +0000

    remove stale pages

--- services/user/Homepage/ui/src/router.tsx
+++ services/user/Homepage/ui/src/router.tsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import { Invite } from "./pages/Invite";
 import { Root } from "./layout";
-import { InviteResponse } from "./pages/InviteResponse";
 
 export default createBrowserRouter([
     {
@@ -12,14 +10,6 @@ export default createBrowserRouter([
             {
                 path: "/",
                 element: <Dashboard />,
-            },
-            {
-                path: "invite",
-                element: <Invite />,
-            },
-            {
-                path: "invite-response",
-                element: <InviteResponse />,
             },
         ],
     },
