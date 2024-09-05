a1815db9732fc0e8972f925fb6161e0ccb8e6952
James-Mart <jmartx93@gmail.com>
2024-09-05 16:03:34 +0000

    add some concepts to blob compare to increase readability

--- libraries/psibase/common/include/psibase/blob.hpp
+++ libraries/psibase/common/include/psibase/blob.hpp
@@ -4,16 +4,29 @@
 #include <algorithm>
 #include <type_traits>
 
+template <typename T>
+concept IsByteType =
+    std::is_same_v<std::decay_t<T>, char> || std::is_same_v<std::decay_t<T>, unsigned char>;
+
+template <typename T>
+concept IsContainerLike = requires(T t) {
+   {
+      t.data()
+   } -> std::convertible_to<const void*>;
+   {
+      t.size()
+   } -> std::convertible_to<std::size_t>;
+};
+
 namespace psibase
 {
    // Compare vector, string, string_view, shared_string (unsigned)
    template <typename A, typename B>
+      requires IsContainerLike<A> && IsContainerLike<B> &&
+               IsByteType<decltype(*std::declval<A>().data())> &&
+               IsByteType<decltype(*std::declval<B>().data())>
    int compare_blob(const A& a, const B& b)
    {
-      static_assert(std::is_same_v<std::decay_t<decltype(*a.data())>, char> ||
-                    std::is_same_v<std::decay_t<decltype(*a.data())>, unsigned char>);
-      static_assert(std::is_same_v<std::decay_t<decltype(*b.data())>, char> ||
-                    std::is_same_v<std::decay_t<decltype(*b.data())>, unsigned char>);
       auto r = memcmp(a.data(), b.data(), std::min(a.size(), b.size()));
       if (r)
          return r;
