988247d4e95ffc2b5156c616f2f5ff0ed7513dca
Mike Manfredi <mike@fractally.com>
2024-09-06 20:09:07 +0000

    began rearranging code

---
+++ rust/psibase_macros/src/lib.rs
@@ -0,0 +1,18 @@
+pub use macro_derive::service_macro;
+
+// use macro_core::service_macro_impl;
+// use quote::quote;
+
+// #[test]
+// fn test1() {
+//     let before = quote! {
+//         mod service {
+//             #[action]
+//             fn action1() {
+//                 println!("only line");
+//             }
+//         }
+//     };
+//     let after = service_macro_impl(quote!(), before);
+//     // assert_tokens_eq(&expected, &after);
+// }
