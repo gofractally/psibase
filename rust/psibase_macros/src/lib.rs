c3a8d9089b797a43519fa7691f397c40fd0f3500
Mike Manfredi <mike@fractally.com>
2024-09-09 17:47:08 +0000

    everything compiling; now can focus on syn and test cases

--- rust/psibase_macros/src/lib.rs
+++ rust/psibase_macros/src/lib.rs
@@ -2,18 +2,20 @@
 // pub use macro_derive::service_macro;
 
 use macro_core::service_macro::service_macro_impl;
-// use quote::quote;
+use syn::parse_macro_input;
 
 #[test]
 fn test1() {
-    //     let before = quote! {
-    //         mod service {
-    //             #[action]
-    //             fn action1() {
-    //                 println!("only line");
-    //             }
-    //         }
-    //     };
-    //     let after = service_macro_impl(quote!(), before);
+    use quote::quote;
+
+    let before = quote! {
+        mod service {
+            #[action]
+            fn action1() {
+                println!("only line");
+            }
+        }
+    };
+    let after = service_macro_impl(quote!(), before);
     //     // assert_tokens_eq(&expected, &after);
 }
