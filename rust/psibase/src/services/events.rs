36cd2cbba7ee153028dc1a20cfa6c24efe9a1e62
James-Mart <jmartx93@gmail.com>
2024-09-11 19:58:43 +0000

    partial fix of Events rust wrapper. Still needs the 'setSchema' action

--- rust/psibase/src/services/events.rs
+++ rust/psibase/src/services/events.rs
@@ -1,15 +1,15 @@
 #[crate::service(name = "events", dispatch = false, psibase_mod = "crate")]
 #[allow(non_snake_case, unused_variables)]
 mod service {
-    fn setSchema(schema: &crate::Schema) {
+    use crate::Schema;
+
+    // #[action]
+    fn setSchema(schema: Schema) {
         unimplemented!()
     }
-    fn addIndex(
-        db_id: crate::DbId,
-        service: crate::AccountNumber,
-        event: crate::MethodNumber,
-        column: u8,
-    ) {
+
+    #[action]
+    fn addIndex(db_id: u32, service: crate::AccountNumber, event: crate::MethodNumber, column: u8) {
         unimplemented!()
     }
 }
