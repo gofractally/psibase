5302f6414ba9fdcf78e8b2e80490ef72c4c756e3
John Williamson <johnjwilliamson@live.com>
2024-09-18 07:07:53 +0000

    add accounts ui

---
+++ services/system/Accounts/ui/src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
---
+++ services/system/Accounts/ui/src/router.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+
+export default createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
