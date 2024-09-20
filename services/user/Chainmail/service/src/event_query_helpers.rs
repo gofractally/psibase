c43cf0a9beb156b361cba2ee3a53d87d5de509ac
Mike Manfredi <mike@fractally.com>
2024-09-20 19:23:06 +0000

    code restructured to separate out table defs and compiling; before REST handler rework for msg retrieval by id

---
+++ services/user/Chainmail/service/src/event_query_helpers.rs
@@ -0,0 +1,114 @@
+use std::collections::HashMap;
+
+use crate::helpers::validate_user;
+
+use psibase::services::r_events::Wrapper as REventsSvc;
+
+use psibase::{HttpReply, HttpRequest};
+
+fn make_query(req: &HttpRequest, sql: String) -> HttpRequest {
+    return HttpRequest {
+        host: req.host.clone(),
+        rootHost: req.rootHost.clone(),
+        method: String::from("POST"),
+        target: String::from("/sql"),
+        contentType: String::from("application/sql"),
+        body: sql.into(),
+    };
+}
+
+fn parse_query(query: &str) -> HashMap<String, String> {
+    let mut params: HashMap<String, String> = HashMap::new();
+
+    let itr = query.split("&");
+    for p in itr {
+        let kv = p.split_once("=").unwrap();
+        params.insert(kv.0.to_string(), kv.1.trim_start_matches('=').to_string());
+    }
+    params
+}
+
+fn get_where_clause_from_sender_receiver_params(params: HashMap<String, String>) -> Option<String> {
+    let mut s_clause = String::new();
+    let s_opt = params.get("sender");
+    if let Some(s) = s_opt {
+        if !validate_user(s) {
+            return None;
+        }
+        s_clause = format!("sender = '{}'", s);
+    }
+
+    let mut r_clause = String::new();
+    let r_opt = params.get(&String::from("receiver"));
+    if let Some(r) = r_opt {
+        if !validate_user(r) {
+            return None;
+        }
+        r_clause = format!("receiver = '{}'", r);
+    }
+
+    if s_opt.is_none() && r_opt.is_none() {
+        return None;
+    }
+
+    let mut where_clause: String = String::from("WHERE ");
+    if s_opt.is_some() {
+        where_clause += s_clause.as_str();
+    }
+    if s_opt.is_some() && r_opt.is_some() {
+        where_clause += " AND ";
+    }
+    if r_opt.is_some() {
+        where_clause += r_clause.as_str();
+    }
+
+    Some(where_clause)
+}
+
+pub fn serve_rest_api(request: &HttpRequest) -> Option<HttpReply> {
+    if request.method == "GET" {
+        if !request.target.starts_with("/messages") {
+            return None;
+        }
+
+        let query_start = request.target.find('?');
+        if query_start.is_none() {
+            return None;
+        }
+        let query_start = query_start.unwrap();
+
+        let query = request.target.split_at(query_start + 1).1;
+        let params = parse_query(query);
+
+        let where_clause: String;
+        let mq: HttpRequest;
+        // handle id param requests (for specific message)
+        if params.contains_key("id") {
+            where_clause = format!(
+                "WHERE CONCAT(sent.receiver, sent.rowid) = {}",
+                params.get("id")?
+            );
+
+            mq = make_query(
+                request,
+                format!(
+                    "SELECT *
+                    FROM \"history.chainmail.sent\" {} ORDER BY ROWID",
+                    where_clause
+                ),
+            );
+        // handle receiver or sender param requests
+        } else {
+            where_clause = get_where_clause_from_sender_receiver_params(params)?;
+
+            mq = make_query(
+                request,
+                format!("SELECT *
+                    FROM \"history.chainmail.sent\" AS sent
+                    LEFT JOIN \"history.chainmail.archive\" AS archive ON CONCAT(sent.receiver, sent.rowid) = archive.event_id {} ORDER BY ROWID", where_clause),
+            );
+        }
+        return REventsSvc::call().serveSys(mq);
+    }
+    return None;
+}
--- services/user/Chainmail/service/src/lib.rs
+++ services/user/Chainmail/service/src/lib.rs
@@ -1,143 +1,19 @@
-use std::collections::HashMap;
-
-use psibase::services::accounts::Wrapper as AccountsSvc;
-use psibase::services::r_events::Wrapper as REventsSvc;
-use psibase::{AccountNumber, HttpReply, HttpRequest};
-
-fn validate_user(user: &str) -> bool {
-    let acc = AccountNumber::from(user);
-    if acc.to_string() != user {
-        return false;
-    }
-
-    AccountsSvc::call().exists(acc)
-}
-
-fn make_query(req: &HttpRequest, sql: String) -> HttpRequest {
-    return HttpRequest {
-        host: req.host.clone(),
-        rootHost: req.rootHost.clone(),
-        method: String::from("POST"),
-        target: String::from("/sql"),
-        contentType: String::from("application/sql"),
-        body: sql.into(),
-    };
-}
-
-fn parse_query(query: &str) -> HashMap<String, String> {
-    let mut params: HashMap<String, String> = HashMap::new();
-
-    let itr = query.split("&");
-    for p in itr {
-        let kv = p.split_once("=").unwrap();
-        params.insert(kv.0.to_string(), kv.1.trim_start_matches('=').to_string());
-    }
-    params
-}
-
-fn get_where_clause_from_sender_receiver_params(params: HashMap<String, String>) -> Option<String> {
-    let mut s_clause = String::new();
-    let s_opt = params.get("sender");
-    if let Some(s) = s_opt {
-        if !validate_user(s) {
-            return None;
-        }
-        s_clause = format!("sender = '{}'", s);
-    }
-
-    let mut r_clause = String::new();
-    let r_opt = params.get(&String::from("receiver"));
-    if let Some(r) = r_opt {
-        if !validate_user(r) {
-            return None;
-        }
-        r_clause = format!("receiver = '{}'", r);
-    }
-
-    if s_opt.is_none() && r_opt.is_none() {
-        return None;
-    }
-
-    let mut where_clause: String = String::from("WHERE ");
-    if s_opt.is_some() {
-        where_clause += s_clause.as_str();
-    }
-    if s_opt.is_some() && r_opt.is_some() {
-        where_clause += " AND ";
-    }
-    if r_opt.is_some() {
-        where_clause += r_clause.as_str();
-    }
-
-    Some(where_clause)
-}
-fn serve_rest_api(request: &HttpRequest) -> Option<HttpReply> {
-    if request.method == "GET" {
-        if !request.target.starts_with("/messages") {
-            return None;
-        }
-
-        let query_start = request.target.find('?');
-        if query_start.is_none() {
-            return None;
-        }
-        let query_start = query_start.unwrap();
-
-        let query = request.target.split_at(query_start + 1).1;
-        let params = crate::parse_query(query);
-
-        let where_clause: String;
-        let mq: HttpRequest;
-        // handle id param requests (for specific message)
-        if params.contains_key("id") {
-            where_clause = format!(
-                "WHERE CONCAT(sent.receiver, sent.rowid) = {}",
-                params.get("id")?
-            );
-
-            mq = make_query(
-                request,
-                format!(
-                    "SELECT *
-                    FROM \"history.chainmail.sent\" {} ORDER BY ROWID",
-                    where_clause
-                ),
-            );
-        // handle receiver or sender param requests
-        } else {
-            where_clause = get_where_clause_from_sender_receiver_params(params)?;
-
-            mq = make_query(
-                request,
-                format!("SELECT *
-                    FROM \"history.chainmail.sent\" AS sent
-                    LEFT JOIN \"history.chainmail.archive\" AS archive ON CONCAT(sent.receiver, sent.rowid) = archive.event_id {} ORDER BY ROWID", where_clause),
-            );
-        }
-        return REventsSvc::call().serveSys(mq);
-    }
-    return None;
-}
+mod event_query_helpers;
+mod helpers;
+mod tables;
 
 #[psibase::service]
 mod service {
-    use crate::serve_rest_api;
+    use crate::event_query_helpers::serve_rest_api;
+    use crate::tables::SavedMessage;
     use psibase::services::accounts::Wrapper as AccountsSvc;
     use psibase::{
         anyhow, check, get_sender, get_service, serve_content, serve_simple_ui, store_content,
-        AccountNumber, Fracpack, HexBytes, HttpReply, HttpRequest, Table, WebContentRow,
+        AccountNumber, HexBytes, HttpReply, HttpRequest, Table, WebContentRow,
     };
-    use serde::{Deserialize, Serialize};
 
-    #[table(name = "SavedMessagesTable")]
-    #[derive(Debug, Fracpack, Serialize, Deserialize)]
-    struct SavedMessage {
-        #[primary_key]
-        event_id: u64,
-        sender: AccountNumber,
-        subject: String,
-        body: String,
-    }
+    #[table(name = "SavedMessagesTable", record = "SavedMessage")]
+    struct SavedMessagesTable;
 
     #[table(record = "WebContentRow", index = 1)]
     struct WebContentTable;
