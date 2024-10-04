abc800aaa58d4e303dd83caa70408e2667c494de
James-Mart <jmartx93@gmail.com>
2024-10-04 17:15:53 +0000

    reverse order that the http servers are checked

--- services/system/HttpServer/src/HttpServer.cpp
+++ services/system/HttpServer/src/HttpServer.cpp
@@ -220,19 +220,27 @@ namespace SystemService
       // TODO: use a view
       auto [sock, req] = psio::from_frac<std::tuple<std::int32_t, HttpRequest>>(act.rawData);
 
-      // First we check the `sites` server
-      auto server    = "sites"_a;
-      currentRequest = {.socket = sock, .owner = server};
-      auto result    = iface(server).serveSys(req, std::optional{sock});
-
-      // If not found, we check the registered server
-      if (!result && currentRequest)
+      // First we check the registered server
+      auto service    = getTargetService(req);
+      auto registered = getServer(service);
+      std::optional<HttpReply> result;
+      psibase::AccountNumber server;
+      if (!registered)
       {
-         auto service    = getTargetService(req);
-         auto registered = getServer(service);
-         if (registered)
+         // Check sites
+         server    = "sites"_a;
+         currentRequest = {.socket = sock, .owner = server};
+         result         = iface(server).serveSys(req, std::optional{sock});
+      } else {
+         // Check registered server, then sites if nothing found
+         server    = registered->server;
+         currentRequest = {.socket = sock, .owner = server};
+         result    = iface(server).serveSys(req, std::optional{sock});
+
+         // If not found, we check the `sites` server
+         if (!result && currentRequest)
          {
-            server         = registered->server;
+            server    = "sites"_a;
             currentRequest = {.socket = sock, .owner = server};
             result         = iface(server).serveSys(req, std::optional{sock});
          }
