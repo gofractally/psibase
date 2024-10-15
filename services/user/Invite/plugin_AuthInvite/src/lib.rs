4828d9cdbf033730a3934a04ea35d0fa429d1c0c
James-Mart <jmartx93@gmail.com>
2024-10-15 15:41:13 +0000

    finish initial auth-invite plugin

--- services/user/Invite/plugin_AuthInvite/src/lib.rs
+++ services/user/Invite/plugin_AuthInvite/src/lib.rs
@@ -9,6 +9,7 @@ use bindings::auth_sig::plugin::keyvault as KeyVault;
 use bindings::clientdata::plugin::keyvalue as Keyvalue;
 use bindings::host::common::{client as Client, types::Error};
 use bindings::invite::plugin::advanced::deserialize;
+use bindings::transact::plugin::auth as Transact;
 
 // Exported interfaces/types
 use bindings::accounts::smart_auth::types::{Action, Claim, Proof};
@@ -35,26 +36,23 @@ struct Tokens {
     tokens: Vec<InviteToken>,
 }
 
+// Database keys
+const KEY_PUB: &str = "pub_key";
+const KEY_PRV: &str = "prv_key";
+
 impl Intf for AuthInvite {
     fn notify(token: InviteToken) -> Result<(), Error> {
-        let mut tokens: Tokens = Keyvalue::get("tokens")
-            .map(|a| <Tokens>::unpacked(&a).expect("[finish_tx] Failed to unpack"))
-            .unwrap_or(Tokens { tokens: vec![] });
+        let inv_keys = deserialize(&token).map_err(|_| DecodeInviteError.err("notify"))?;
 
-        if tokens.tokens.contains(&token) {
-            return Ok(());
-        }
+        Keyvalue::set(KEY_PUB, &inv_keys.pub_key)?;
+        Keyvalue::set(KEY_PRV, &inv_keys.priv_key)?;
 
-        tokens.tokens.push(token);
-        Keyvalue::set("tokens", &tokens.packed())?;
+        // This will notify transact that auth-invite has a claim/proof for this transaction.
+        Transact::notify();
 
         Ok(())
     }
 }
-
-// Database keys
-const KEY_INVITE_TOKENS: &str = "tokens";
-const KEY_ACTIVE_PK: &str = "active_claim";
 
 impl SmartAuth for AuthInvite {
     fn get_claims(account_name: String, actions: Vec<Action>) -> Result<Vec<Claim>, Error> {
@@ -62,68 +60,58 @@ impl SmartAuth for AuthInvite {
             return Err(Unauthorized.err("get_claims"));
         }
 
-        let current_user = Client::get_logged_in_user()?;
-        if current_user.is_some() {
-            return Err(Unauthorized.err("get_claims: use auth of the logged-in user"));
-        }
-
         let payer_account = psibase::services::invite::PAYER_ACCOUNT.to_string();
-        if account_name != payer_account {
-            return Err(Unauthorized.err(&format!(
-                "get_claims: can only provide claims for {}",
+        if account_name == payer_account {
+            // If the invite is itself paying for this transaction, we restrict the tx
+            // to only a single action: acceptCreate or reject
+            let e = Err(Unauthorized.err(&format!(
+                "{} can only authorize acceptCreate or reject action",
                 payer_account
             )));
+            if actions.len() != 1 {
+                return e;
+            }
+
+            let action = actions.first().unwrap();
+            if action.method != InviteActions::acceptCreate::ACTION_NAME
+                && action.method != InviteActions::reject::ACTION_NAME
+            {
+                return e;
+            }
+        } else {
+            // If an existing account is authorizing this transaction but wants to respond
+            // to an invite, we allow any number of actions, but there must be an invite action
+            // among them (accept, acceptCreate, reject) in order for the consumption of an invite
+            // token to occur
+            let has_invite_action = actions.iter().any(|action| {
+                action.method == InviteActions::acceptCreate::ACTION_NAME
+                    || action.method == InviteActions::reject::ACTION_NAME
+                    || action.method == InviteActions::accept::ACTION_NAME
+            });
+
+            if !has_invite_action {
+                return Err(Unauthorized.err("get_claims: must have an invite action"));
+            }
         }
 
-        if actions.len() != 1 {
-            return Err(Unauthorized.err("get_claims: must have exactly one action"));
-        }
-
-        let action = actions.first().unwrap();
-        if action.method != InviteActions::acceptCreate::ACTION_NAME
-            && action.method != InviteActions::reject::ACTION_NAME
-        {
-            return Err(Unauthorized.err("get_claims: action must be acceptCreate or reject"));
-        }
-
-        let tokens = Keyvalue::get(KEY_INVITE_TOKENS)
-            .map(|a| <Tokens>::unpacked(&a).expect("get_claims: Failed to unpack invite tokens"));
-
-        if tokens.is_none() || tokens.as_ref().unwrap().tokens.is_empty() {
-            return Err(MissingInviteToken.err("get_claims"));
-        }
-
-        // Consume an available token
-        let mut tokens = tokens.unwrap().tokens;
-        let token = tokens.pop().unwrap();
-        Keyvalue::set(KEY_INVITE_TOKENS, &Tokens { tokens }.packed())?;
-
-        let inv_keys = deserialize(&token).map_err(|_| DecodeInviteError.err("get_claims"))?;
-
-        Keyvalue::set(KEY_ACTIVE_PK, &inv_keys.priv_key)?;
-
+        let pub_key = Keyvalue::get(KEY_PUB).ok_or_else(|| MissingInviteToken.err("get_claims"))?;
         Ok(vec![Claim {
             verify_service: psibase::services::auth_invite::VERIFY_SERVICE.to_string(),
-            raw_data: inv_keys.pub_key,
+            raw_data: pub_key,
         }])
     }
 
-    fn get_proofs(account_name: String, message: Vec<u8>) -> Result<Vec<Proof>, Error> {
+    fn get_proofs(_account_name: String, message: Vec<u8>) -> Result<Vec<Proof>, Error> {
         if !from_transact() {
             return Err(Unauthorized.err("get_proofs"));
         }
 
-        let payer_account = psibase::services::invite::PAYER_ACCOUNT.to_string();
-        if account_name != payer_account {
-            return Err(Unauthorized.err(&format!(
-                "get_proofs: can only provide proofs for {}",
-                payer_account
-            )));
-        }
+        let prv_key = Keyvalue::get(KEY_PRV).ok_or_else(|| MissingInviteToken.err("get_proofs"))?;
+        let signature = KeyVault::sign(&message, &prv_key)?;
 
-        let pk = Keyvalue::get(KEY_ACTIVE_PK).expect("get_proofs: Failed to get active pk");
-
-        let signature = KeyVault::sign(&message, &pk)?;
+        // Free the local storage space
+        Keyvalue::delete(KEY_PUB);
+        Keyvalue::delete(KEY_PRV);
 
         Ok(vec![Proof { signature }])
     }
