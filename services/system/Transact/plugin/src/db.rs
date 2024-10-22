eb59fa3de88847ab2dd5eafd66aa8f56faeb2f1d
James-Mart <jmartx93@gmail.com>
2024-10-22 14:26:40 +0000

    transact plugin db interface cleanup

--- services/system/Transact/plugin/src/db.rs
+++ services/system/Transact/plugin/src/db.rs
@@ -2,39 +2,38 @@ use crate::bindings::accounts::smart_auth::smart_auth::Action as PartialAction;
 use crate::bindings::clientdata::plugin::keyvalue as Keyvalue;
 use psibase::fracpack::{Pack, Unpack};
 
-const KEY_AUTH_PLUGINS: &str = "auth_plugins";
 pub struct AuthPlugins;
-
 impl AuthPlugins {
+    const KEY_AUTH_PLUGINS: &'static str = "auth_plugins";
     pub fn push(auth_plugin: String) {
         let mut auth_plugins = Self::get();
         auth_plugins.push(auth_plugin);
 
-        Keyvalue::set(KEY_AUTH_PLUGINS, &auth_plugins.packed())
+        Keyvalue::set(Self::KEY_AUTH_PLUGINS, &auth_plugins.packed())
             .expect("Failed to set auth plugins");
     }
 
     pub fn get() -> Vec<String> {
-        Keyvalue::get(KEY_AUTH_PLUGINS)
+        Keyvalue::get(Self::KEY_AUTH_PLUGINS)
             .map(|a| <Vec<String>>::unpacked(&a).expect("Failed to unpack auth plugins"))
             .unwrap_or(vec![])
     }
 
     pub fn has_plugins() -> bool {
-        Keyvalue::get(KEY_AUTH_PLUGINS).is_some()
+        Keyvalue::get(Self::KEY_AUTH_PLUGINS).is_some()
     }
 
     pub fn clear() {
-        Keyvalue::delete(KEY_AUTH_PLUGINS);
+        Keyvalue::delete(Self::KEY_AUTH_PLUGINS);
     }
 }
 
-const KEY_ACTIONS: &str = "actions";
 pub struct CurrentActions;
-
 impl CurrentActions {
+    const KEY_ACTIONS: &'static str = "actions";
+
     pub fn push(service: String, method_name: String, packed_args: Vec<u8>) {
-        let mut actions = Keyvalue::get(KEY_ACTIONS)
+        let mut actions = Keyvalue::get(Self::KEY_ACTIONS)
             .map(|a| <Vec<PartialAction>>::unpacked(&a).expect("Failed to unpack actions"))
             .unwrap_or(vec![]);
 
@@ -44,20 +43,20 @@ impl CurrentActions {
             raw_data: packed_args,
         });
 
-        Keyvalue::set(KEY_ACTIONS, &actions.packed()).expect("Failed to set actions");
+        Keyvalue::set(Self::KEY_ACTIONS, &actions.packed()).expect("Failed to set actions");
     }
 
     pub fn get() -> Vec<PartialAction> {
-        Keyvalue::get(KEY_ACTIONS)
+        Keyvalue::get(Self::KEY_ACTIONS)
             .map(|a| <Vec<PartialAction>>::unpacked(&a).expect("Failed to unpack actions"))
             .unwrap_or(vec![])
     }
 
     pub fn has_actions() -> bool {
-        Keyvalue::get(KEY_ACTIONS).is_some()
+        Keyvalue::get(Self::KEY_ACTIONS).is_some()
     }
 
     pub fn clear() {
-        Keyvalue::delete(KEY_ACTIONS);
+        Keyvalue::delete(Self::KEY_ACTIONS);
     }
 }
