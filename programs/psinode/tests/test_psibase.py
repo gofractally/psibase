211ba66d7b4dc1ed79f4829218689d5ced8191e2
James-Mart <jmartx93@gmail.com>
2024-10-31 21:19:27 +0000

    fix test_psibase

--- programs/psinode/tests/test_psibase.py
+++ programs/psinode/tests/test_psibase.py
@@ -94,7 +94,7 @@ class TestPsibase(unittest.TestCase):
     @testutil.psinode_test
     def test_upgrade(self, cluster):
         a = cluster.complete(*testutil.generate_names(1))[0]
-        a.boot(packages=['Minimal', 'Explorer', 'Sites'])
+        a.boot(packages=['Minimal', 'Explorer', 'Sites', 'Brotli'])
 
         foo10 = TestPackage('foo', '1.0.0').depends('Sites').service('foo', data={'file1.txt': 'original', 'file2.txt': 'deleted'})
         foo11 = TestPackage('foo', '1.1.0').depends('Sites').service('foo', data={'file1.txt': 'updated', 'file3.txt': 'added'})
