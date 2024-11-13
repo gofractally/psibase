56ebeec3d7ebab8c3f466a8f9c263a6185da03d8
Steven Watanabe <steven@clarionos.com>
2024-11-13 13:18:52 -0700

    Remove old time parsing and formatting code

--- libraries/psibase/common/include/psibase/time.hpp
+++ libraries/psibase/common/include/psibase/time.hpp
@@ -6,21 +6,8 @@
 
 namespace psibase
 {
-   std::string        microseconds_to_str(uint64_t microseconds);
-   [[nodiscard]] bool string_to_utc_seconds(uint32_t&    result,
-                                            const char*& s,
-                                            const char*  end,
-                                            bool         eat_fractional,
-                                            bool         require_end);
-   [[nodiscard]] bool stringToUtcMicroseconds(uint64_t&    result,
-                                              const char*& s,
-                                              const char*  end,
-                                              bool         require_end);
-
-   using Seconds      = std::chrono::duration<std::int64_t>;
-   using MicroSec     = std::chrono::duration<std::int64_t, std::micro>;
-   using TimePointSec = std::chrono::time_point<std::chrono::system_clock, Seconds>;
-
+   using Seconds       = std::chrono::duration<std::int64_t>;
+   using MicroSec      = std::chrono::duration<std::int64_t, std::micro>;
+   using TimePointSec  = std::chrono::time_point<std::chrono::system_clock, Seconds>;
    using TimePointUSec = std::chrono::time_point<std::chrono::system_clock, MicroSec>;
-
 }  // namespace psibase
