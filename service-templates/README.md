25a26a742d65f4dbbca9bf99c6df478a196419aa
Mike Manfredi <mike@fractally.com>
2024-12-04 15:39:19 +0000

    first commit

---
+++ service-templates/README.md
@@ -0,0 +1,10 @@
+This directory contains service (with plugins and UIs) templates that can be instantiated with `cargo generate`.
+
+## Usage
+
+Instanstiate a Rust service:
+`cargo generate --git fractally/psibase/templates` rust
+
+# Resources used to build this project / Guiding docs
+
+[Advice from actual usage](https://thoughtbot.com/blog/cargo-generate-lessons)
---
+++ service-templates/rust/service/src/lib.rs
@@ -0,0 +1,34 @@
+#[psibase::service(name = "{{project-name}}")]
+#[allow(non_snake_case)]
+pub mod service {
+    use psibase::{check, Fracpack, Table, ToSchema};
+    use serde::{Deserialize, Serialize};
+
+    #[table(name = "InitTable", index = 0)]
+    #[derive(Serialize, Deserialize, ToSchema, Fracpack, Debug)]
+    pub struct InitRow {}
+    impl InitRow {
+        #[primary_key]
+        fn pk(&self) {}
+    }
+
+    #[action]
+    fn init() {
+        let table = InitTable::new();
+        table.put(&InitRow {}).unwrap();
+    }
+
+    #[pre_action(exclude(init))]
+    fn check_init() {
+        let table = InitTable::new();
+        check(
+            table.get_index_pk().get(&()).is_some(),
+            "service not inited",
+        );
+    }
+
+    #[action]
+    fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+}
