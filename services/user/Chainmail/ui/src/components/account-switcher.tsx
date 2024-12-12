b48993ec262054afc3cbcc0245a006c51ffed6d8
Brandon Fancher <brandonfancher@gmail.com>
2024-12-12 16:36:34 +0000

    Introduce and style account selection menu.

--- services/user/Chainmail/ui/src/components/account-switcher.tsx
+++ services/user/Chainmail/ui/src/components/account-switcher.tsx
@@ -1,5 +1,16 @@
 import { useIncomingMessages, useUser } from "@hooks";
 import { cn } from "@lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "@shadcn/avatar";
+import { Button } from "@shadcn/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@shadcn/dropdown-menu";
 import {
     Select,
     SelectContent,
@@ -7,46 +18,138 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@shadcn/select";
+import {
+    BadgeCheck,
+    Bell,
+    ChevronsUpDown,
+    CreditCard,
+    LogOut,
+    Sparkles,
+} from "lucide-react";
 
 interface AccountSwitcherProps {
     isCollapsed: boolean;
 }
 
+const user = {
+    avatar: undefined,
+    name: "alice",
+};
+
 export function AccountSwitcher({ isCollapsed }: AccountSwitcherProps) {
     const { setSelectedMessageId } = useIncomingMessages();
-    const { availableAccounts, user, setUser } = useUser();
+    // const { availableAccounts, user, setUser } = useUser();
 
     return (
-        <Select
-            defaultValue={user}
-            onValueChange={(value) => {
-                setSelectedMessageId("");
-                setUser(value);
-            }}
-        >
-            <SelectTrigger
-                className={cn(
-                    "flex items-center gap-2 [&>span]:line-clamp-1 [&>span]:flex [&>span]:w-full [&>span]:items-center [&>span]:gap-1 [&>span]:truncate [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0",
-                    isCollapsed &&
-                        "flex h-9 w-9 shrink-0 items-center justify-center p-0 [&>span]:w-auto [&>svg]:hidden",
-                )}
-                aria-label="Select account"
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button
+                    size="lg"
+                    variant="ghost"
+                    className={cn("w-full", !isCollapsed && "pl-1 pr-2")}
+                >
+                    <Avatar className="h-8 w-8 rounded-lg">
+                        <AvatarImage src={user.avatar} alt={user.name} />
+                        <AvatarFallback className="rounded-lg">
+                            {user.name[0]}
+                        </AvatarFallback>
+                    </Avatar>
+                    {isCollapsed ? null : (
+                        <>
+                            <div className="ml-2 grid flex-1 text-left text-sm leading-tight">
+                                <span className="truncate font-semibold">
+                                    {user.name}
+                                </span>
+                            </div>
+                            <ChevronsUpDown className="ml-auto size-4" />
+                        </>
+                    )}
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+                className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                // side={isMobile ? "bottom" : "right"}
+                side="right"
+                align="end"
+                sideOffset={4}
             >
-                <SelectValue placeholder="Select an account">
-                    <span className={cn("ml-2", isCollapsed && "hidden")}>
-                        {user}
-                    </span>
-                </SelectValue>
-            </SelectTrigger>
-            <SelectContent>
-                {availableAccounts.map((account) => (
-                    <SelectItem key={account} value={account}>
-                        <div className="flex items-center gap-3 [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0 [&_svg]:text-foreground">
-                            {account}
+                <DropdownMenuLabel className="p-0 font-normal">
+                    <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                        <Avatar className="h-8 w-8 rounded-lg">
+                            <AvatarImage src={user.avatar} alt={user.name} />
+                            <AvatarFallback className="rounded-lg">
+                                {user.name[0]}
+                            </AvatarFallback>
+                        </Avatar>
+                        <div className="grid flex-1 text-left text-sm leading-tight">
+                            <span className="truncate font-semibold">
+                                {user.name}
+                            </span>
                         </div>
-                    </SelectItem>
-                ))}
-            </SelectContent>
-        </Select>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        <Sparkles className="mr-2 h-4 w-4" />
+                        Upgrade to Pro
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        <BadgeCheck className="mr-2 h-4 w-4" />
+                        Account
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <CreditCard className="mr-2 h-4 w-4" />
+                        Billing
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Bell className="mr-2 h-4 w-4" />
+                        Notifications
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                    <LogOut className="mr-2 h-4 w-4" />
+                    Log out
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     );
+
+    // return (
+    //     <Select
+    //         defaultValue={user}
+    //         onValueChange={(value) => {
+    //             setSelectedMessageId("");
+    //             setUser(value);
+    //         }}
+    //     >
+    //         <SelectTrigger
+    //             className={cn(
+    //                 "flex items-center gap-2 [&>span]:line-clamp-1 [&>span]:flex [&>span]:w-full [&>span]:items-center [&>span]:gap-1 [&>span]:truncate [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0",
+    //                 isCollapsed &&
+    //                     "flex h-9 w-9 shrink-0 items-center justify-center p-0 [&>span]:w-auto [&>svg]:hidden",
+    //             )}
+    //             aria-label="Select account"
+    //         >
+    //             <SelectValue placeholder="Select an account">
+    //                 <span className={cn("ml-2", isCollapsed && "hidden")}>
+    //                     {user}
+    //                 </span>
+    //             </SelectValue>
+    //         </SelectTrigger>
+    //         <SelectContent>
+    //             {availableAccounts.map((account) => (
+    //                 <SelectItem key={account} value={account}>
+    //                     <div className="flex items-center gap-3 [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0 [&_svg]:text-foreground">
+    //                         {account}
+    //                     </div>
+    //                 </SelectItem>
+    //             ))}
+    //         </SelectContent>
+    //     </Select>
+    // );
 }
