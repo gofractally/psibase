5a07d0ff67816f1e95a4b5d8f1bc3fce98f72c0a
Mike Manfredi <mike@fractally.com>
2024-12-17 20:07:24 +0000

    don't run anything if there is no ui directory

--- services/user/Branding/build.rs
+++ services/user/Branding/build.rs
@@ -1,9 +1,14 @@
 use std::fs;
+use std::io::Path;
 use std::io::Write;
 
 use std::process::Command;
 
 fn main() {
+    if !Path::new("./ui").exists() {
+        // no UI to build
+        return;
+    }
     // Touch a marker file to ensure that cargo reruns this build script no matter what
     // unless it may decide in appropriately that it doesn't need to run the script.
     fs::File::create("./ui_build")
