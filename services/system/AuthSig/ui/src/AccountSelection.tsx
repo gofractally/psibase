2a51ae1df0b2c85a706d0f8ed0646841dfd64d83
John Williamson <johnjwilliamson@live.com>
2024-12-19 22:56:30 +0000

    get account

--- services/system/AuthSig/ui/src/AccountSelection.tsx
+++ services/system/AuthSig/ui/src/AccountSelection.tsx
@@ -3,23 +3,28 @@ import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
-import axios from "axios";
-import { siblingUrl } from "@psibase/common-lib";
+// import axios from "axios";
+// import { siblingUrl } from "@psibase/common-lib";
 import { useLoggedInUser } from "./hooks/useLoggedInUser";
 import { useCreateConnectionToken } from "./hooks/useCreateConnectionToken";
+import { graphql } from "./lib/graphql";
 
 dayjs.extend(relativeTime);
+
+// http://auth-sig.psibase.127.0.0.1.sslip.io:8080/graphql
 
 // const wait = (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const useAccountLookup = (accountName: string | undefined | null) =>
   useQuery({
     queryKey: ["account", accountName],
-    queryFn: async () => {
-      const url = siblingUrl();
-      console.log(url, "is the url");
-      return axios.post(siblingUrl());
-    },
+    queryFn: async () =>
+      graphql(`{
+      	account(name: "${accountName}") {
+      		pubkey
+      		account
+        }
+      }`),
     enabled: !!accountName,
   });
 
@@ -41,7 +46,9 @@ export const AccountSelection = () => {
       >
         Login
       </button>
-      <div>current user is {currentUser}</div>
+      <div>
+        current user is <span className="text-blue-500">{currentUser}</span>
+      </div>
       {JSON.stringify(data)}
     </div>
   );
