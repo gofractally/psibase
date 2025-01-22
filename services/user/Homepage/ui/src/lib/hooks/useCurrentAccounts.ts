0ffd0f0785766d7f87823b54d7a0c3c16c59c842
John Williamson <johnjwilliamson@live.com>
2025-01-22 06:02:00 +0000

    restore useCurrentAccounts hook

---
+++ services/user/Homepage/ui/src/lib/hooks/useCurrentAccounts.ts
@@ -0,0 +1,25 @@
+import { supervisor } from "@/main";
+import { useQuery } from "@tanstack/react-query";
+import { z } from "zod";
+
+export const useCurrentAccounts = (enabled: boolean) =>
+    useQuery({
+        queryKey: ["currentAccounts"],
+        enabled,
+        initialData: [],
+        queryFn: async () => {
+            const res = z
+                .string()
+                .array()
+                .parse(
+                    await supervisor.functionCall({
+                        method: "getConnectedAccounts",
+                        params: [],
+                        service: "accounts",
+                        intf: "activeApp",
+                    })
+                );
+
+            return res;
+        },
+    });
