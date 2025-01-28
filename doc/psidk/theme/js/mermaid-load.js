5e9bd574af10c841fe5ceb9f40b4c4af744e3e45
James-Mart <jmartx93@gmail.com>
2025-01-28 17:07:17 +0000

    fix outdated index.hbs, use locally bundled mermaid js, and add support for tab controls

--- doc/psidk/theme/js/mermaid-load.js
+++ doc/psidk/theme/js/mermaid-load.js
@@ -1,9 +1,4 @@
-const mermaidScript = document.createElement('script');
-mermaidScript.src = 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.min.js';
-mermaidScript.onload = () => {
-  mermaid.initialize({ 
-    startOnLoad: true,
-    theme: 'dark',
-  });
-};
-document.head.appendChild(mermaidScript);
+mermaid.initialize({ 
+  startOnLoad: true,
+  theme: 'dark',
+});
