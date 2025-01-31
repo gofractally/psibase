20548695fc237e5984fba05f7692eaa80e4709df
James-Mart <jmartx93@gmail.com>
2025-01-31 21:59:12 +0000

    use RAII in workshop propose latch

--- services/user/Workshop/plugin/src/lib.rs
+++ services/user/Workshop/plugin/src/lib.rs
@@ -16,6 +16,22 @@ use staged_tx::plugin::proposer::set_propose_latch;
 mod errors;
 use errors::ErrorType;
 
+struct ProposeLatch;
+
+impl ProposeLatch {
+    fn new(app: &str) -> Self {
+        set_propose_latch(app).unwrap();
+        Self
+    }
+}
+
+impl Drop for ProposeLatch {
+    fn drop(&mut self) {
+        // TODO - once I merge with main, uncomment this
+        //set_propose_latch(None).unwrap();
+    }
+}
+
 struct WorkshopPlugin;
 
 impl Admin for WorkshopPlugin {
@@ -49,32 +65,35 @@ impl Admin for WorkshopPlugin {
 
 impl App for WorkshopPlugin {
     fn upload(app: String, file: File, compression_quality: u8) -> Result<(), Error> {
-        set_propose_latch(&app)?;
+        let _latch = ProposeLatch::new(&app);
+
         sites::plugin::api::upload(&file, compression_quality)
     }
 
     fn upload_tree(app: String, files: Vec<File>, compression_quality: u8) -> Result<u16, Error> {
-        set_propose_latch(&app)?;
+        let _latch = ProposeLatch::new(&app);
+
         sites::plugin::api::upload_tree(&files, compression_quality)
     }
 
     fn enable_spa(app: String, enable: bool) -> Result<(), Error> {
-        set_propose_latch(&app)?;
+        let _latch = ProposeLatch::new(&app);
+
         sites::plugin::api::enable_spa(enable)
     }
 
     fn set_csp(app: String, path: String, csp: String) -> Result<(), Error> {
-        set_propose_latch(&app)?;
+        let _latch = ProposeLatch::new(&app);
         sites::plugin::api::set_csp(&path, &csp)
     }
 
     fn set_cache_mode(app: String, enable: bool) -> Result<(), Error> {
-        set_propose_latch(&app)?;
+        let _latch = ProposeLatch::new(&app);
         sites::plugin::api::set_cache_mode(enable)
     }
 
     fn set_service_code(app: String, code: Vec<u8>) -> Result<(), Error> {
-        set_propose_latch(&app)?;
+        let _latch = ProposeLatch::new(&app);
         setcode::plugin::api::set_service_code(&code);
         Ok(())
     }
@@ -82,17 +101,17 @@ impl App for WorkshopPlugin {
 
 impl Mail for WorkshopPlugin {
     fn send(app: String, receiver: String, subject: String, body: String) -> Result<(), Error> {
-        set_propose_latch(&app)?;
+        let _latch = ProposeLatch::new(&app);
         chainmail::plugin::api::send(&receiver, &subject, &body)
     }
 
     fn archive(app: String, msg_id: u64) -> Result<(), Error> {
-        set_propose_latch(&app)?;
+        let _latch = ProposeLatch::new(&app);
         chainmail::plugin::api::archive(msg_id)
     }
 
     fn save(app: String, msg_id: u64) -> Result<(), Error> {
-        set_propose_latch(&app)?;
+        let _latch = ProposeLatch::new(&app);
         chainmail::plugin::api::save(msg_id)
     }
 }
@@ -103,17 +122,17 @@ impl Registry for WorkshopPlugin {
     }
 
     fn set_app_metadata(app: String, metadata: AppMetadata) -> Result<(), Error> {
-        set_propose_latch(&app)?;
+        let _latch = ProposeLatch::new(&app);
         registry::plugin::developer::set_app_metadata(&metadata)
     }
 
     fn publish_app(app: String) -> Result<(), Error> {
-        set_propose_latch(&app)?;
+        let _latch = ProposeLatch::new(&app);
         registry::plugin::developer::publish_app(&app)
     }
 
     fn unpublish_app(app: String) -> Result<(), Error> {
-        set_propose_latch(&app)?;
+        let _latch = ProposeLatch::new(&app);
         registry::plugin::developer::unpublish_app(&app)
     }
 }
