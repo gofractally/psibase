1bec8fd7f4a1459e8ac40909f58fe06ba289e8f9
Steven Watanabe <steven@clarionos.com>
2025-02-07 14:06:46 -0700

    Add some utilities for parsing requests

--- libraries/psibase/common/src/Rpc.cpp
+++ libraries/psibase/common/src/Rpc.cpp
@@ -1,5 +1,7 @@
 #include <psibase/Rpc.hpp>
 
+#include <ranges>
+
 namespace
 {
    template <typename Iter>
@@ -40,6 +42,10 @@ namespace
 
 namespace psibase
 {
+   bool HttpHeader::matches(std::string_view h) const
+   {
+      return std::ranges::equal(name, h, {}, ::tolower, ::tolower);
+   }
 
    std::pair<std::string, std::string> HttpRequest::readQueryItem(std::string_view& query)
    {
@@ -63,5 +69,59 @@ namespace psibase
    std::string HttpRequest::path() const
    {
       return decode_pct(std::string_view(target).substr(0, target.find('?')));
+   }
+
+   std::optional<std::string_view> HttpRequest::getCookie(std::string_view name) const
+   {
+      for (const auto& header : headers)
+      {
+         if (std::ranges::equal(header.name, std::string_view{"cookie"}, {}, ::tolower))
+         {
+            for (auto kvrange : header.value | std::views::split(';'))
+            {
+               std::string_view kv(kvrange.begin(), kvrange.end());
+               auto             pos = kv.find('=');
+               check(pos != std::string_view::npos, "Invalid cookie");
+               auto key   = kv.substr(kv.find_first_not_of(" \t"), pos);
+               auto value = kv.substr(pos + 1);
+               if (key == name)
+                  return value;
+            }
+         }
+      }
+      return {};
+   }
+
+   void HttpRequest::removeCookie(std::string_view name)
+   {
+      for (auto& header : headers)
+      {
+         if (std::ranges::equal(header.name, std::string_view{"cookie"}, {}, ::tolower))
+         {
+            auto out   = header.value.begin();
+            bool first = true;
+            for (auto kvrange : header.value | std::views::split(';'))
+            {
+               std::string_view kv(kvrange.begin(), kvrange.end());
+               auto             pos     = kv.find('=');
+               bool             matched = false;
+               if (pos != std::string_view::npos)
+               {
+                  auto key   = kv.substr(kv.find_first_not_of(" \t"), pos);
+                  auto value = kv.substr(pos + 1);
+                  matched    = key == name;
+               }
+               if (!matched)
+               {
+                  if (first)
+                     first = false;
+                  else
+                     *out++ = ';';
+                  std::ranges::copy(kv, out);
+               }
+            }
+            header.value.resize(out - header.value.begin());
+         }
+      }
    }
 }  // namespace psibase
