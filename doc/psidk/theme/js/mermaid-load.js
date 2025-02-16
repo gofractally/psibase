a10c661e6302f6018afcc853bc5d00187392b16d
James-Mart <jmartx93@gmail.com>
2025-02-16 18:06:59 +0000

    go back to dynamically loaded mermaid to reduce size

--- doc/psidk/theme/js/mermaid-load.js
+++ doc/psidk/theme/js/mermaid-load.js
@@ -1,4 +1,10 @@
-mermaid.initialize({ 
-  startOnLoad: true,
-  theme: 'dark',
-});
+const mermaidScript = document.createElement("script");
+mermaidScript.src =
+  "https://cdn.jsdelivr.net/npm/mermaid@11.4.1/dist/mermaid.min.js";
+mermaidScript.onload = () => {
+  mermaid.initialize({
+    startOnLoad: true,
+    theme: "dark",
+  });
+};
+document.head.appendChild(mermaidScript);
