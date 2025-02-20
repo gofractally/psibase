a23bc182ee3adbaecedc462151a4ef1738e0f90b
James-Mart <jmartx93@gmail.com>
2025-02-20 17:18:21 +0000

    add auth-delegate query service

--- services/system/AuthDelegate/include/services/system/AuthDelegate.hpp
+++ services/system/AuthDelegate/include/services/system/AuthDelegate.hpp
@@ -16,8 +16,12 @@ namespace SystemService
    {
       psibase::AccountNumber account;
       psibase::AccountNumber owner;
+
+      auto secondary() const { return std::tie(owner, account); }
    };
    PSIO_REFLECT(AuthDelegateRecord, account, owner)
+   using AuthDelegateTable = psibase::
+       Table<AuthDelegateRecord, &AuthDelegateRecord::account, &AuthDelegateRecord::secondary>;
 
    /// The `auth-delegate` service is an auth service that can be used to authenticate actions for accounts.
    ///
@@ -27,8 +31,7 @@ namespace SystemService
    {
      public:
       static constexpr auto service = psibase::AccountNumber("auth-delegate");
-      using AuthDelegateTable = psibase::Table<AuthDelegateRecord, &AuthDelegateRecord::account>;
-      using Tables            = psibase::ServiceTables<AuthDelegateTable>;
+      using Tables                  = psibase::ServiceTables<AuthDelegateTable>;
 
       /// This is an implementation of the standard auth service interface defined in [SystemService::AuthInterface]
       ///
---
+++ services/system/AuthDelegate/include/services/system/RAuthDelegate.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <psibase/Rpc.hpp>
+#include <psibase/Service.hpp>
+#include <psibase/serviceEntry.hpp>
+
+namespace SystemService
+{
+   class RAuthDelegate : public psibase::Service
+   {
+     public:
+      static constexpr auto service = psibase::AccountNumber("r-auth-dlg");
+
+      auto serveSys(psibase::HttpRequest request) -> std::optional<psibase::HttpReply>;
+   };
+   PSIO_REFLECT(RAuthDelegate,  //
+                method(serveSys, request))
+}  // namespace SystemService
