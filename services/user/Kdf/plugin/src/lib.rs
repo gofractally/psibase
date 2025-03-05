eed2911e575d0e8802be57057f9c3bb1f4f65fb9
James-Mart <jmartx93@gmail.com>
2025-03-05 20:22:10 +0000

    improve kdf plugin interface

--- services/user/Kdf/plugin/src/lib.rs
+++ services/user/Kdf/plugin/src/lib.rs
@@ -11,10 +11,10 @@ const AES_KEY_SIZE: usize = 32; // 256 bits
 
 struct KdfPlugin;
 impl Api for KdfPlugin {
-    fn derive_key(type_: Keytype, seed: u64, salt: String) -> Vec<u8> {
+    fn derive_key(type_: Keytype, seed: Vec<u8>, salt: String) -> Vec<u8> {
         match type_ {
             Keytype::Aes => {
-                let seed_bytes = seed.to_le_bytes();
+                let seed_bytes = seed.as_slice();
                 let mut key = [0u8; AES_KEY_SIZE];
                 // Use a fixed salt for simplicity (in a real-world scenario, you might want to use a unique salt)
                 let salt = salt.as_bytes();
