0f40ea7daa4cc6bc6f3178978e806ca2aa087b4c
Mike Manfredi <mike@fractally.com>
2025-03-19 04:34:54 +0000

    more PR cleanup

--- services/user/Permissions/plugin/src/errors.rs
+++ services/user/Permissions/plugin/src/errors.rs
@@ -4,7 +4,6 @@ plugin_error! {
     pub ErrorType
     QueryResponseParseError(msg: String) => "Query response parsing error: {msg}",
     PermissionsDialogAsyncNotYetAvailable() => "Permissions dialog will fail until WASM async is implemented. Respond to dialog and repeat action to continue.",
-    RequestRedirect(id: String) = 999999999 => "{{ \"id\": \"{id}\" }}",
     InvalidRequester() => "Disallowed third-party requester while saving permission",
     SerderError(msg: String) => "(de)serialization error: {msg}"
 }
