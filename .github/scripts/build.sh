49239952127df09676d52771cb01c57ee28c889d
Mike Manfredi <mike@fractally.com>
2025-04-07 17:01:03 +0000

    add full path to cargo-psibase for building and update hashes for builder images

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -65,4 +65,4 @@ ${DOCKER} bash -c "cd build && cpack -G TGZ -D CPACK_PACKAGE_FILE_NAME=psidk-ubu
 echo =====
 ${DOCKER} bash -c "cd build && mv book psidk-book && tar czf ../psidk-book.tar.gz psidk-book"
 echo =====
-${DOCKER} bash -c "cargo generate -p ./package-templates/ --destination ./services/user/ --init -v --name Buildtest --silent basic-01 && cd services/user/Buildtest && cargo-psibase package"
+${DOCKER} bash -c "cargo generate -p ./package-templates/ --destination ./services/user/ --init -v --name Buildtest --silent basic-01 && cd services/user/Buildtest && /root/psibase/build/psidk/bin/psibase package"
