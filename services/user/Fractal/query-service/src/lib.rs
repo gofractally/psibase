50869e80c9a88e9e9d2b2f2a1db36b023d9f20f8
Mike Manfredi <GreenBusDriver@gmail.com>
2025-04-11 18:50:12 +0000

    queries now compiling

--- services/user/Fractal/query-service/src/lib.rs
+++ services/user/Fractal/query-service/src/lib.rs
@@ -2,34 +2,40 @@
 #[allow(non_snake_case)]
 mod service {
     use async_graphql::{connection::Connection, *};
+    use fractal::tables::tables::{Fractal, FractalTable, Member, MemberTable};
     use psibase::*;
-    use serde::Deserialize;
-    use fractal::tables::tables::{Member, MemberTable, Fractal, FractalTable};
 
     struct Query;
 
     #[Object]
     impl Query {
-        async fn get_members(&self,
+        async fn get_members(
+            &self,
             fractal: AccountNumber,
-            // titles: Option<u32>,
             first: Option<i32>,
             last: Option<i32>,
             before: Option<String>,
             after: Option<String>,
-        ) -> async_graphql::Result<Connection<u64, Member>> {
-            TableQuery::subindex::<AccountNumber>(MemberTable::new().get_index_pk().get(&fractal).unwrap_or_default())
-            .first(first)
-            .last(last)
-            .before(before)
-            .after(after)
-            .query()
-            .await
+        ) -> async_graphql::Result<Connection<RawKey, Member>> {
+            TableQuery::subindex::<AccountNumber>(MemberTable::new().get_index_pk(), &fractal)
+                .first(first)
+                .last(last)
+                .before(before)
+                .after(after)
+                .query()
+                .await
         }
 
-        async fn get_member(&self, fractal: AccountNumber, member: String) -> Option<Member> {
+        async fn get_member(
+            &self,
+            fractal: AccountNumber,
+            member: AccountNumber,
+        ) -> Option<Member> {
             let table = MemberTable::new();
-            let member = table.get_index_pk().get(&(fractal, member)).unwrap_or_default();
+            let member = table
+                .get_index_pk()
+                .get(&(fractal, member))
+                .unwrap_or_default();
             Some(member)
         }
 
@@ -39,9 +45,14 @@ mod service {
             Some(fractal)
         }
 
-        async fn get_council(&self, fractal: AccountNumber) -> Vec<Member> {
-            let table = MemberTable::new();
-            TableQuery::subindex::<(AccountNumber, psibase::TimePointSec, u32)>(MemberTable::new().get_index_pk().get(&(fractal)).unwrap_or_default())
+        async fn get_council(
+            &self,
+            fractal: AccountNumber,
+        ) -> async_graphql::Result<Connection<RawKey, Member>> {
+            TableQuery::subindex::<(AccountNumber, psibase::TimePointSec, u32)>(
+                MemberTable::new().get_index_pk(),
+                &fractal,
+            )
             .query()
             .await
         }
@@ -50,14 +61,13 @@ mod service {
     #[action]
     #[allow(non_snake_case)]
     fn serveSys(request: HttpRequest) -> Option<HttpReply> {
-            // Services graphql queries
+        // Services graphql queries
         None.or_else(|| serve_graphql(&request, Query))
-
             // Serves a GraphiQL UI interface at the /graphiql endpoint
             .or_else(|| serve_graphiql(&request))
 
-            // TODO: Is this needed? wasn't compiling and isn't in the package-template
-            // // Serves a full service schema at the /schema endpoint
-            // .or_else(|| serve_schema::<fractal::Wrapper>(&request))
+        // TODO: Is this needed? wasn't compiling and isn't in the package-template
+        // // Serves a full service schema at the /schema endpoint
+        // .or_else(|| serve_schema::<fractal::Wrapper>(&request))
     }
 }
