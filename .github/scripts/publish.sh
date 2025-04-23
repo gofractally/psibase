0a0947c0fceb5a999d18591c9b880dcf0737804c
Steven Watanabe <steven@clarionos.com>
2025-04-23 08:24:35 -0600

    Fix quoting

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -5,4 +5,4 @@ ACCOUNT=$( echo "$PUBLISH_LOGIN" | jq -r '.["account"]' )
 KEY=/tmp/publish-key
 echo "$PUBLISH_LOGIN" | jq -r '.["key"]' > $KEY
 
-rust/release/psibase publish -a \"$URL\" -S \"$ACCOUNT\" -s $KEY share/psibase/packages/*.psi
+rust/release/psibase publish -a "$URL" -S "$ACCOUNT" -s "$KEY" share/psibase/packages/*.psi
