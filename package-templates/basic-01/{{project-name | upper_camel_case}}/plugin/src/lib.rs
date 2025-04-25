4eee21505264839758e71f0b1028f70991e9e56d
Mike Manfredi <mike@fractally.com>
2025-04-25 17:14:49 +0000

    not working yet; ran into wasm issue

--- package-templates/basic-01/{{project-name | upper_camel_case}}/plugin/src/lib.rs
+++ package-templates/basic-01/{{project-name | upper_camel_case}}/plugin/src/lib.rs
@@ -1,12 +1,16 @@
 #[allow(warnings)]
 mod bindings;
+mod perms;
 
+use bindings::clientdata::plugin::keyvalue as Keyvalue;
+use bindings::exports::{{project-name | snake_case}}::plugin::admin::Guest as Admin;
 use bindings::exports::{{project-name | snake_case}}::plugin::api::Guest as Api;
 use bindings::exports::{{project-name | snake_case}}::plugin::queries::Guest as Queries;
 use bindings::host::common::server as CommonServer;
 use bindings::host::common::types::Error;
 use bindings::transact::plugin::intf::add_action_to_transaction;
 
+use perms::verify_auth_method;
 use psibase::fracpack::Pack;
 
 mod errors;
@@ -16,8 +20,22 @@ struct {{project-name | upper_camel_case}}Plugin;
 
 impl Api for {{project-name | upper_camel_case}}Plugin {
     fn set_example_thing(thing: String) {
-        let packed_example_thing_args = {{project-name | snake_case}}::action_structs::setExampleThing { thing }.packed();
-        add_action_to_transaction("setExampleThing", &packed_example_thing_args).unwrap();
+        verify_auth_method("setExampleThing");
+
+        // let packed_example_thing_args =
+        //     addperms::action_structs::setExampleThing { thing }.packed();
+        // add_action_to_transaction("setExampleThing", &packed_example_thing_args).unwrap();
+    }
+}
+
+impl Admin for {{project-name | upper_camel_case}}Plugin {
+    fn save_perm(caller: String, method: String) {
+        let any_value = "1".as_bytes();
+        Keyvalue::set(&format!("{caller}->{method}"), any_value).unwrap();
+    }
+
+    fn del_perm(caller: String, method: String) {
+        Keyvalue::delete(&format!("{caller}->{method}"));
     }
 }
 
