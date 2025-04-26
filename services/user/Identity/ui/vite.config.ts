856b33cfd26a704796c0ec0570306852f91b53f5
Mike Manfredi <mike@fractally.com>
2025-04-26 22:58:16 +0000

    confirmed a remaining change

--- services/user/Identity/ui/vite.config.ts
+++ services/user/Identity/ui/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createSkipUnchangedBuildPlugin } from '../../../vite.shared'
+import { createSharedViteConfig } from '../../../vite.shared'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), createSkipUnchangedBuildPlugin(__dirname)],
+  ...createSharedViteConfig({
+    projectDir: __dirname,
+    framework: 'react',
+    additionalPlugins: [react()]
+  }),
   server: {
     // NOTE: this or 127.0.0.1 will work; localhost won't because localhost is resolved to the IPv6 address of ::1
     host: "psibase.127.0.0.1.sslip.io",
