285b14ace74568c6530fc9f139a748229dc63318
Brandon Fancher <brandonfancher@gmail.com>
2025-05-06 18:33:02 +0000

    Adjust import order.

--- services/user/FractalUI/ui/src/router.tsx
+++ services/user/FractalUI/ui/src/router.tsx
@@ -1,10 +1,11 @@
+import { createBrowserRouter, redirect } from "react-router-dom";
+
 import { Loader } from "@/pages/Loader";
 import { ActiveAndUpcoming } from "@/pages/evaluations/active-and-upcoming";
 import { Completed } from "@/pages/evaluations/completed";
 import { Proposed } from "@/pages/evaluations/proposed";
 import { AllMembers } from "@/pages/membership/all-members";
 import { MyMembership } from "@/pages/membership/my-membership";
-import { createBrowserRouter, redirect } from "react-router-dom";
 
 import { AppExists } from "@/components/app-exists";
 import { Layout } from "@/components/layout";
