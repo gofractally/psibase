3ed97af3e3ef34531f2542d6dae11f9080dcf3fb
John Williamson <johnjwilliamson@live.com>
2025-05-09 03:40:19 +0000

    remove irrevant hooks and components

--- services/user/Fractals/ui/src/hooks/useCurrentThreadId.ts
+++
@@ -1,7 +0,0 @@
-import { useParams } from "react-router-dom";
-import { z } from "zod";
-
-export const useCurrentThreadId = (): string | undefined => {
-  const { threadId } = useParams();
-  return z.string().parse(threadId);
-};
