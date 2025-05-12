d80ef4a413e118322a7437fbc2d61d3cdc51e9a8
Steven Watanabe <steven@clarionos.com>
2025-05-12 15:56:35 -0600

    Add script to generate the login credentials and the setup transaction

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -7,7 +7,7 @@ cd build
 URL=$( echo "$PUBLISH_LOGIN" | jq -r '.["url"]' )
 ACCOUNT=$( echo "$PUBLISH_LOGIN" | jq -r '.["account"]' )
 KEY=/tmp/publish-key
-echo "$PUBLISH_LOGIN" | jq -r '.["key"]' > $KEY
+echo "$PUBLISH_LOGIN" | jq -j '.["key"]' > $KEY
 
 set -x
 rust/release/psibase publish -a "$URL" -S "$ACCOUNT" -s "$KEY" share/psibase/packages/*.psi
