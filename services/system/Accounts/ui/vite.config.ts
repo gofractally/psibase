d6190a9329d48ea095065dadde9ca09f29c3ccb3
James-Mart <jmartx93@gmail.com>
2025-05-15 19:20:39 +0000

    Revert "Merge pull request #1187 from gofractally/mm/yarnv1-wkspc-minimal"
    This reverts commit 2b49fecd6edb49fe8b081033a8b6b2cc57771671, reversing
    changes made to 286efdb2331ec6426e5a494fa0269db90f26af15.

--- services/system/Accounts/ui/vite.config.ts
+++ services/system/Accounts/ui/vite.config.ts
@@ -1,67 +1,97 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import type { IncomingMessage } from "http";
+import alias from "@rollup/plugin-alias";
+// import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
+const psibase = (appletContract: string, isServing?: boolean) => {
+  const buildAliases = [
+    {
+      find: "@",
+      replacement: path.resolve(__dirname, "./src"),
+    },
+  ];
+
+  if (isServing) {
+    buildAliases.push({
+      // @ts-expect-error fjweiojfiew
+      find: /^@psibase\/common-lib.*$/,
+      replacement: path.resolve(
+        "../../../user/CommonApi/common/packages/common-lib/src"
+      ),
+    });
+  }
+
+  return [
     {
       name: "psibase",
-      config: () => ({
-        build: {
-          assetsDir: "",
-          cssCodeSplit: false,
-          rollupOptions: {
-            external: ["/common/rootdomain.mjs", "/common/common-lib.js"],
-            makeAbsoluteExternalsRelative: false,
-            output: {
-              entryFileNames: "index.js",
-              assetFileNames: "[name][extname]",
-            },
-          },
-        },
-        server: {
-          host: "psibase.127.0.0.1.sslip.io",
-          port: 8081,
-          proxy: {
-            "/": {
-              target: "http://psibase.127.0.0.1.sslip.io:8079/",
-              bypass: (req: IncomingMessage) => {
-                const host = req.headers.host || "";
-                const subdomain = host.split(".")[0];
-                if (
-                  subdomain === "psibase" &&
-                  req.method !== "POST" &&
-                  req.headers.accept !== "application/json" &&
-                  !req.url?.startsWith("/common") &&
-                  !req.url?.startsWith("/api")
-                ) {
-                  return req.url;
-                }
+      config: () => {
+        return {
+          build: {
+            assetsDir: "",
+            cssCodeSplit: false,
+            rollupOptions: {
+              external: ["/common/rootdomain.mjs", "/common/common-lib.js"],
+              makeAbsoluteExternalsRelative: false,
+              output: {
+                entryFileNames: "index.js",
+                assetFileNames: "[name][extname]",
               },
             },
           },
-        },
-        resolve: {
-          alias: [
-            {
-              find: "@",
-              replacement: path.resolve(__dirname, "./src"),
+          server: {
+            host: "psibase.127.0.0.1.sslip.io",
+            port: 8081,
+            proxy: {
+              "/": {
+                target: "http://psibase.127.0.0.1.sslip.io:8079/",
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                bypass: (req: any) => {
+                  const host = req.headers.host || "";
+                  const subdomain = host.split(".")[0];
+                  if (
+                    subdomain === appletContract &&
+                    req.method !== "POST" &&
+                    req.headers.accept !== "application/json" &&
+                    !req.url.startsWith("/common") &&
+                    !req.url.startsWith("/api")
+                  ) {
+                    return req.url;
+                  }
+                },
+              },
             },
-          ],
-        },
-      }),
+          },
+          resolve: {
+            alias: buildAliases,
+          },
+        };
+      },
     },
+    alias({
+      entries: [
+        {
+          find: /^@psibase\/common-lib.*$/,
+          replacement: "/common/common-lib.js",
+        },
+      ],
+    }),
+  ];
+};
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
+  plugins: [
+    react(),
+    // svgr({ exportAsDefault: true }),
+    psibase("psibase", command === "serve"),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@psibase/common-lib": "/common/common-lib.js",
     },
   },
   build: {
     minify: false,
   },
-});
+}));
