b2b555bab62f045061b29649956f6106559c776f
John Williamson <johnjwilliamson@live.com>
2025-05-16 17:56:39 +0000

    update to fix build

--- services/user/Fractals/plugin/src/lib.rs
+++ services/user/Fractals/plugin/src/lib.rs
@@ -50,7 +50,7 @@ impl Api for FractallyPlugin {
             None => Ok(None),
             Some(rank_numbers) => {
                 let users: Vec<AccountNumber> =
-                    get_group_users(&"fractals", evaluation_id, group_number)?
+                    get_group_users("fractals", evaluation_id, group_number)?
                         .iter()
                         .map(|account| AccountNumber::from_str(account).unwrap())
                         .collect();
@@ -66,16 +66,16 @@ impl Api for FractallyPlugin {
     }
 
     fn attest(evaluation_id: u32, group_number: u32) -> Result<(), Error> {
-        attest(&"fractals", evaluation_id, group_number)
+        attest("fractals", evaluation_id, group_number)
     }
 
     fn get_group_users(evaluation_id: u32, group_number: u32) -> Result<Vec<String>, Error> {
-        get_group_users(&"fractals", evaluation_id, group_number)
+        get_group_users("fractals", evaluation_id, group_number)
     }
 
     fn propose(evaluation_id: u32, group_number: u32, proposal: Vec<String>) -> Result<(), Error> {
         let all_users: Vec<AccountNumber> =
-            get_group_users(&"fractals", evaluation_id, group_number)?
+            get_group_users("fractals", evaluation_id, group_number)?
                 .iter()
                 .map(|account| AccountNumber::from_str(account).unwrap())
                 .collect();
@@ -90,7 +90,7 @@ impl Api for FractallyPlugin {
             .map(|num| num.to_string())
             .collect();
 
-        propose(&"fractals", evaluation_id, group_number, &res)
+        propose("fractals", evaluation_id, group_number, &res)
     }
 
     fn set_schedule(
