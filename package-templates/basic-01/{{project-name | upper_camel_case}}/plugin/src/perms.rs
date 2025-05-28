c1df244bcbb4fa02b1d8144b110661a82438854b
Mike Manfredi <mike@fractally.com>
2025-05-28 17:24:15 +0000

    adding missing use

--- package-templates/basic-01/{{project-name | upper_camel_case}}/plugin/src/perms.rs
+++ package-templates/basic-01/{{project-name | upper_camel_case}}/plugin/src/perms.rs
@@ -2,6 +2,7 @@ use crate::bindings::accounts::plugin::api::get_current_user;
 use crate::bindings::clientdata::plugin::keyvalue as Keyvalue;
 use crate::bindings::host::common::client::{self, prompt_user};
 use crate::bindings::host::common::types::Error;
+use crate::errors::ErrorType;
 
 pub fn verify_auth_method(method: &str) -> Result<(), Error> {
     let caller = client::get_sender_app().app.unwrap();
