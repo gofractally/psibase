d4fdf2cad4fe0f203e30263cf557c6633324d2d7
Brandon Fancher <brandonfancher@gmail.com>
2025-06-05 19:05:22 +0000

    Fix eslint-plugin-react-hooks

--- services/user/Branding/ui/eslint.config.mjs
+++ services/user/Branding/ui/eslint.config.mjs
@@ -1,8 +1,8 @@
 import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import * as reactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
     { ignores: ["dist", "node_modules"] },
@@ -13,9 +13,7 @@ export default tseslint.config(
             ecmaVersion: 2020,
             globals: globals.browser,
         },
-        plugins: {
-            "react-hooks": reactHooks,
-        },
     },
+    reactHooks.configs["recommended-latest"],
     eslintConfigPrettier,
 );
