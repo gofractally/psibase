679899802c3e97bbcf3741d766d4f018b2f23629
John Williamson <johnjwilliamson@live.com>
2025-06-08 01:35:43 +0000

    add docs

--- services/user/Fractals/service/src/lib.rs
+++ services/user/Fractals/service/src/lib.rs
@@ -9,6 +9,12 @@ pub mod service {
 
     use psibase::*;
 
+    /// Creates a new account and fractal.
+    ///
+    /// # Arguments
+    /// * `fractal_account` - The account number for the new fractal.
+    /// * `name` - The name of the fractal.
+    /// * `mission` - The mission statement of the fractal.
     #[action]
     fn create_fractal(fractal_account: AccountNumber, name: String, mission: String) {
         let sender = get_sender();
@@ -20,6 +26,11 @@ pub mod service {
         Wrapper::emit().history().created_fractal(fractal_account);
     }
 
+    /// Starts an evaluation for the specified fractal and evaluation type.
+    ///
+    /// # Arguments
+    /// * `fractal` - The account number of the fractal.
+    /// * `evaluation_type` - The type of evaluation to start.
     #[action]
     fn start_eval(fractal: AccountNumber, evaluation_type: u8) {
         let evaluation = EvaluationInstance::get_assert(fractal, evaluation_type.into());
@@ -29,6 +40,12 @@ pub mod service {
         psibase::services::evaluations::Wrapper::call().start(evaluation.evaluation_id);
     }
 
+    /// Allows a user to join a fractal and immediately become a citizen.
+    ///
+    /// Cannot be called by a fractal.
+    ///
+    /// # Arguments
+    /// * `fractal` - The account number of the fractal to join.
     #[action]
     fn join(fractal: AccountNumber) {
         let sender = get_sender();
@@ -45,6 +62,15 @@ pub mod service {
         Wrapper::emit().history().joined_fractal(fractal, sender);
     }
 
+    /// Sets the schedule for an evaluation type within a fractal.
+    ///
+    /// # Arguments
+    /// * `evaluation_type` - The type of evaluation.
+    /// * `registration` - Unix seconds timestamp for the start of the registration phase.
+    /// * `deliberation` - Unix seconds timestamp for the start of the deliberation phase.
+    /// * `submission` - Unix seconds timestamp for the start of the submission phase.
+    /// * `finish_by` - Unix seconds timestamp for the start of the evaluation completion phase.
+    /// * `interval_seconds` - Interval in seconds for recurring evaluations.
     #[action]
     fn set_schedule(
         evaluation_type: u8,
@@ -71,6 +97,10 @@ pub mod service {
         );
     }
 
+    /// Called when an evaluation in a fractal is finalized.
+    ///
+    /// # Arguments
+    /// * `evaluation_id` - The ID of the evaluation.
     #[action]
     fn on_eval_fin(evaluation_id: u32) {
         check_is_eval();
@@ -86,11 +116,16 @@ pub mod service {
         evaluation.schedule_next_evaluation();
     }
 
+    /// Called when a user registers for an evaluation in a fractal.
+    ///
+    /// # Arguments
+    /// * `evaluation_id` - The ID of the evaluation.
+    /// * `account` - The account number of the registering user.
     #[action]
     fn on_ev_reg(evaluation_id: u32, account: AccountNumber) {
         check_is_eval();
         let evaluation = EvaluationInstance::get_by_evaluation_id(evaluation_id);
-        let member = psibase::check_some(
+        let member = check_some(
             Member::get(evaluation.fractal, account),
             "account is not a member of fractal",
         );
@@ -102,9 +137,21 @@ pub mod service {
         );
     }
 
+    /// Called when a user unregisters from an evaluation in a fractal.
+    ///
+    /// # Arguments
+    /// * `evaluation_id` - The ID of the evaluation.
+    /// * `account` - The account number of the unregistering user.
     #[action]
     fn on_ev_unreg(_evaluation_id: u32, _account: AccountNumber) {}
 
+    /// Called when a user submits an attestation for decrypted proposals in a fractal evaluation.
+    ///
+    /// # Arguments
+    /// * `evaluation_id` - The ID of the evaluation.
+    /// * `group_number` - The group number within the evaluation.
+    /// * `user` - The account number of the user submitting the attestation.
+    /// * `attestation` - The attestation data.
     #[action]
     fn on_attest(
         evaluation_id: u32,
@@ -123,6 +170,12 @@ pub mod service {
         check(is_valid_attestion, "invalid attestation");
     }
 
+    /// Called when a group finalizes its result in a fractal evaluation.
+    ///
+    /// # Arguments
+    /// * `evaluation_id` - The ID of the evaluation.
+    /// * `group_number` - The group number within the evaluation.
+    /// * `group_result` - The result data of the group.
     #[action]
     fn on_grp_fin(evaluation_id: u32, group_number: u32, group_result: Vec<u8>) {
         check_is_eval();
