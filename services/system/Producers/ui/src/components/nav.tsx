361dad00b46f9be2af993c5781f614f743aaa3bb
Brandon Fancher <brandonfancher@gmail.com>
2025-06-27 18:31:20 +0000

    Hoist producers ui

--- services/system/Producers/ui/src/components/nav.tsx
+++ services/system/Producers/ui/src/components/nav.tsx
@@ -1,4 +1,4 @@
-import { HoverBorderGradient } from "../components/ui/hover-border-gradient";
+import { HoverBorderGradient } from "@shared/components/hover-border-gradient";
 import { useBranding } from "../hooks/useBranding";
 import { siblingUrl } from "@psibase/common-lib";
 import { SettingsDropdown } from "./settings-dropdown";
