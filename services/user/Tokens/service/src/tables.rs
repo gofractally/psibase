079fe6a635e38ea68b2bfe0f0ebc354c264c23b3
John Williamson <johnjwilliamson@live.com>
2025-07-02 05:41:33 +0000

    drop the 2nd secondary key

--- services/user/Tokens/service/src/tables.rs
+++ services/user/Tokens/service/src/tables.rs
@@ -6,7 +6,7 @@ pub mod tables {
     use crate::service::TID;
     use async_graphql::{ComplexObject, SimpleObject};
     use psibase::services::nft::{Wrapper as Nfts, NID};
-    use psibase::services::tokens::{Quantity, Precision, Decimal};
+    use psibase::services::tokens::{Decimal, Precision, Quantity};
     use psibase::{check, check_none, check_some, get_sender, AccountNumber};
     use psibase::{Fracpack, Table, ToSchema};
     use serde::{Deserialize, Serialize};
@@ -321,16 +321,11 @@ pub mod tables {
 
     impl SharedBalance {
         #[primary_key]
-        fn pk(&self) -> (TID, AccountNumber, AccountNumber) {
-            (self.token_id, self.creditor, self.debitor)
-        }
-
-        #[secondary_key(1)]
         fn by_creditor(&self) -> (AccountNumber, AccountNumber, TID) {
             (self.creditor, self.debitor, self.token_id)
         }
 
-        #[secondary_key(2)]
+        #[secondary_key(1)]
         fn by_debitor(&self) -> (AccountNumber, AccountNumber, TID) {
             (self.debitor, self.creditor, self.token_id)
         }
@@ -352,7 +347,7 @@ pub mod tables {
         pub fn get_or_new(creditor: AccountNumber, debitor: AccountNumber, token_id: TID) -> Self {
             SharedBalanceTable::new()
                 .get_index_pk()
-                .get(&(token_id, creditor, debitor))
+                .get(&(creditor, debitor, token_id))
                 .unwrap_or(Self::new(creditor, debitor, token_id))
         }
 
@@ -422,7 +417,7 @@ pub mod tables {
         }
 
         fn delete(&self) {
-            SharedBalanceTable::new().erase(&(self.pk()));
+            SharedBalanceTable::new().erase(&(self.by_creditor()));
         }
 
         fn save(&mut self) {
