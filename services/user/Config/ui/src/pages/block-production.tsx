30b59de4412346ee21d842fc088ffc6da184e30d
John Williamson <johnjwilliamson@live.com>
2025-07-06 07:16:35 +0000

    use form

--- services/user/Config/ui/src/pages/block-production.tsx
+++ services/user/Config/ui/src/pages/block-production.tsx
@@ -1,7 +1,9 @@
+import { ProducersForm } from "@/components/forms/producers-form";
+
 export const BlockProduction = () => {
     return (
         <div className="mx-auto h-screen w-screen max-w-screen-lg">
-            Block production
+            <ProducersForm />
         </div>
     );
 };
