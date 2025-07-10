8729e66914f14b15a4d5d77f5d330bf7cc5f9169
Brandon Fancher <brandonfancher@gmail.com>
2025-07-10 17:24:15 +0000

    Run formatter on Identity ui

--- services/user/Identity/ui/src/App.tsx
+++ services/user/Identity/ui/src/App.tsx
@@ -1,256 +1,270 @@
-import { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
+import { useEffect, useRef, useState } from "react";
 
 import "./App.css";
-import {
-  addAllLinks,
-  addNodes,
-  applyCouncilDistance,
-  applyLinkColors,
-  handleSimulationTick,
-} from "./graph-drawing";
-import { initSimulation } from "./simulations";
-import rawData from "./members-fixture";
 import { Sidebar } from "./Sidebar";
-import { convertCyclicToAcyclicGraph } from "./graph-data";
 import { SvgCanvas } from "./SvgCanvas";
 import {
-  AttestationGraph,
-  AttestationGraphLink,
-  AttestationGraphNode,
-  UiOptions,
-} from "./types";
-import {
-  SELECTOR,
-  dataSetName,
-  defaultUiOptions,
-  height,
-  nodeRadius,
-  reenergizingForce,
-  viewBoxScalar,
-  viewBoxWidth,
-  width,
+    SELECTOR,
+    dataSetName,
+    defaultUiOptions,
+    height,
+    nodeRadius,
+    reenergizingForce,
+    viewBoxScalar,
+    viewBoxWidth,
+    width,
 } from "./constants";
+import { convertCyclicToAcyclicGraph } from "./graph-data";
+import {
+    addAllLinks,
+    addNodes,
+    applyCouncilDistance,
+    applyLinkColors,
+    handleSimulationTick,
+} from "./graph-drawing";
+import rawData from "./members-fixture";
+import { initSimulation } from "./simulations";
+import {
+    AttestationGraph,
+    AttestationGraphLink,
+    AttestationGraphNode,
+    UiOptions,
+} from "./types";
 
 function App() {
-  const opt1 = true; // false => Links; true => no Links
-  const opt2 = !opt1;
+    const opt1 = true; // false => Links; true => no Links
+    const opt2 = !opt1;
 
-  const [rerenderSidebar, setRerenderSidebar] = useState<boolean>(false);
-  const simulation = useRef<
-    d3.Simulation<AttestationGraphNode, AttestationGraphLink>
-  >(d3.forceSimulation<AttestationGraphNode, AttestationGraphLink>());
-  const uiOptions = useRef<UiOptions>(defaultUiOptions);
+    const [rerenderSidebar, setRerenderSidebar] = useState<boolean>(false);
+    const simulation = useRef<
+        d3.Simulation<AttestationGraphNode, AttestationGraphLink>
+    >(d3.forceSimulation<AttestationGraphNode, AttestationGraphLink>());
+    const uiOptions = useRef<UiOptions>(defaultUiOptions);
 
-  const deployNewGraph = () => {
-    const graphCopy = JSON.parse(JSON.stringify(rawData[dataSetName]));
-    const graph =
-      uiOptions.current.centeredNodeIdx >= 0
-        ? convertCyclicToAcyclicGraph(graphCopy, uiOptions.current)
-        : graphCopy;
+    const deployNewGraph = () => {
+        const graphCopy = JSON.parse(JSON.stringify(rawData[dataSetName]));
+        const graph =
+            uiOptions.current.centeredNodeIdx >= 0
+                ? convertCyclicToAcyclicGraph(graphCopy, uiOptions.current)
+                : graphCopy;
 
-    // ATTEMPT to apply the link force only to links currently shown in graph; didn't work
-    // Option 1: just recreate the simulation with the new graph
-    if (opt1) kickOffSimulation(graph);
-    // Option 2 (didn't work): Try to reset the link force to only apply to current graph's attestations (rather than all attestations)
-    // simulation.current.force(
-    //   "link",
-    //   d3.forceLink(graph.attestations).distance(120).strength(1)
-    // );
+        // ATTEMPT to apply the link force only to links currently shown in graph; didn't work
+        // Option 1: just recreate the simulation with the new graph
+        if (opt1) kickOffSimulation(graph);
+        // Option 2 (didn't work): Try to reset the link force to only apply to current graph's attestations (rather than all attestations)
+        // simulation.current.force(
+        //   "link",
+        //   d3.forceLink(graph.attestations).distance(120).strength(1)
+        // );
 
-    // 5: add/remove/update links to/in DOM and style them
-    addAllLinks(graph);
-    applyLinkColors(
-      uiOptions.current,
-      SELECTOR.LINK,
-      uiOptions.current.checkAttestations
-    );
-    applyLinkColors(
-      uiOptions.current,
-      SELECTOR.BACK_LINK,
-      uiOptions.current.checkBackAttestations
-    );
-    applyLinkColors(
-      uiOptions.current,
-      SELECTOR.ACCOUNT_CREATION_LINK,
-      uiOptions.current.checkAccountCreations
-    );
+        // 5: add/remove/update links to/in DOM and style them
+        addAllLinks(graph);
+        applyLinkColors(
+            uiOptions.current,
+            SELECTOR.LINK,
+            uiOptions.current.checkAttestations,
+        );
+        applyLinkColors(
+            uiOptions.current,
+            SELECTOR.BACK_LINK,
+            uiOptions.current.checkBackAttestations,
+        );
+        applyLinkColors(
+            uiOptions.current,
+            SELECTOR.ACCOUNT_CREATION_LINK,
+            uiOptions.current.checkAccountCreations,
+        );
 
-    d3.selectAll<SVGGElement, AttestationGraphNode>(".node").attr(
-      "class",
-      (n) =>
-        "node" +
-        (n.council ? " council" : "") +
-        (n.id === uiOptions.current.centeredNodeIdx ? " centered" : "")
-    );
-
-    simulation.current.alpha(reenergizingForce);
-    simulation.current.restart();
-  };
-
-  const centerThisNode = () => {
-    const centeredNode = d3
-      .selectAll<SVGGElement, AttestationGraphNode>(".node")
-      .data()
-      .find((n) => n.id === uiOptions.current.centeredNodeIdx);
-    if (centeredNode) {
-      centeredNode.x = 0;
-      centeredNode.y = 0;
-    }
-  };
-
-  const kickOffSimulation = (graph: AttestationGraph) => {
-    // 3: Define and kick off the simulation
-    simulation.current = initSimulation(graph, width, height, centerThisNode);
-
-    simulation.current
-      .on("tick", handleSimulationTick(simulation.current, nodeRadius))
-      .alphaDecay(0.002); // just added alpha decay to delay end of execution
-    // 4: add/remove/update nodes to/in DOM and style them
-    addNodes(graph.nodes, nodeRadius);
-
-    // deployNewGraph();
-
-    d3.select("#canvas")
-      .selectAll<SVGCircleElement, AttestationGraphNode>("circle")
-      .on("click", (_, node) => {
-        if (!!node && node?.id !== uiOptions.current.centeredNodeIdx) {
-          uiOptions.current.centeredNodeIdx = node.id;
-          if (uiOptions.current.centeredNodeIdx === -1)
-            uiOptions.current.graphMode = "fullGraph";
-          else uiOptions.current.graphMode = "subGraph";
-          setRerenderSidebar((prev) => !prev);
-
-          d3.select("#canvas")
-            .selectAll<SVGGElement, AttestationGraphNode>(".node")
-            .attr(
-              "class",
-              (n) =>
+        d3.selectAll<SVGGElement, AttestationGraphNode>(".node").attr(
+            "class",
+            (n) =>
                 "node" +
                 (n.council ? " council" : "") +
-                (n.id === uiOptions.current.centeredNodeIdx ? " centered" : "")
-            );
-        }
+                (n.id === uiOptions.current.centeredNodeIdx ? " centered" : ""),
+        );
 
-        // To Ensure a decent layout of a fresh graph
-        // Option 1: regenerate the simulation from scratch
-        // kickOffSimulation();
-        // Option 2: don't regenerate the simulation; just redraw it (layout isn't as good)
-        deployNewGraph();
-      });
-  };
-
-  useEffect(() => {
-    if (opt2) {
-      const graphCopy = JSON.parse(JSON.stringify(rawData[dataSetName]));
-      kickOffSimulation(graphCopy);
-    }
-    deployNewGraph();
-  }, []);
-
-  const handleLinkColorChange =
-    (selector: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
-      const selectedColor = e!.target.value;
-      uiOptions.current.colors[selector] = selectedColor;
-      setRerenderSidebar((prev) => !prev);
-      const checked =
-        (selector === SELECTOR.LINK && uiOptions.current.checkAttestations) ||
-        (selector === SELECTOR.BACK_LINK &&
-          uiOptions.current.checkBackAttestations) ||
-        (selector === SELECTOR.ACCOUNT_CREATION_LINK &&
-          uiOptions.current.checkAccountCreations);
-      applyLinkColors(uiOptions.current, selector, checked);
+        simulation.current.alpha(reenergizingForce);
+        simulation.current.restart();
     };
 
-  const handleGraphDepthChange = (newDepthStr: number) => {
-    uiOptions.current.dagDepth = newDepthStr;
-    setRerenderSidebar((prev) => !prev);
-    deployNewGraph();
-  };
+    const centerThisNode = () => {
+        const centeredNode = d3
+            .selectAll<SVGGElement, AttestationGraphNode>(".node")
+            .data()
+            .find((n) => n.id === uiOptions.current.centeredNodeIdx);
+        if (centeredNode) {
+            centeredNode.x = 0;
+            centeredNode.y = 0;
+        }
+    };
 
-  const handleCouncilDistanceChange = (newDistance: number) => {
-    uiOptions.current.councilDistance = newDistance;
-    setRerenderSidebar((prev) => !prev);
-    applyCouncilDistance(
-      JSON.parse(JSON.stringify(rawData[dataSetName])),
-      uiOptions.current
+    const kickOffSimulation = (graph: AttestationGraph) => {
+        // 3: Define and kick off the simulation
+        simulation.current = initSimulation(
+            graph,
+            width,
+            height,
+            centerThisNode,
+        );
+
+        simulation.current
+            .on("tick", handleSimulationTick(simulation.current, nodeRadius))
+            .alphaDecay(0.002); // just added alpha decay to delay end of execution
+        // 4: add/remove/update nodes to/in DOM and style them
+        addNodes(graph.nodes, nodeRadius);
+
+        // deployNewGraph();
+
+        d3.select("#canvas")
+            .selectAll<SVGCircleElement, AttestationGraphNode>("circle")
+            .on("click", (_, node) => {
+                if (!!node && node?.id !== uiOptions.current.centeredNodeIdx) {
+                    uiOptions.current.centeredNodeIdx = node.id;
+                    if (uiOptions.current.centeredNodeIdx === -1)
+                        uiOptions.current.graphMode = "fullGraph";
+                    else uiOptions.current.graphMode = "subGraph";
+                    setRerenderSidebar((prev) => !prev);
+
+                    d3.select("#canvas")
+                        .selectAll<SVGGElement, AttestationGraphNode>(".node")
+                        .attr(
+                            "class",
+                            (n) =>
+                                "node" +
+                                (n.council ? " council" : "") +
+                                (n.id === uiOptions.current.centeredNodeIdx
+                                    ? " centered"
+                                    : ""),
+                        );
+                }
+
+                // To Ensure a decent layout of a fresh graph
+                // Option 1: regenerate the simulation from scratch
+                // kickOffSimulation();
+                // Option 2: don't regenerate the simulation; just redraw it (layout isn't as good)
+                deployNewGraph();
+            });
+    };
+
+    useEffect(() => {
+        if (opt2) {
+            const graphCopy = JSON.parse(JSON.stringify(rawData[dataSetName]));
+            kickOffSimulation(graphCopy);
+        }
+        deployNewGraph();
+    }, []);
+
+    const handleLinkColorChange =
+        (selector: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+            const selectedColor = e!.target.value;
+            uiOptions.current.colors[selector] = selectedColor;
+            setRerenderSidebar((prev) => !prev);
+            const checked =
+                (selector === SELECTOR.LINK &&
+                    uiOptions.current.checkAttestations) ||
+                (selector === SELECTOR.BACK_LINK &&
+                    uiOptions.current.checkBackAttestations) ||
+                (selector === SELECTOR.ACCOUNT_CREATION_LINK &&
+                    uiOptions.current.checkAccountCreations);
+            applyLinkColors(uiOptions.current, selector, checked);
+        };
+
+    const handleGraphDepthChange = (newDepthStr: number) => {
+        uiOptions.current.dagDepth = newDepthStr;
+        setRerenderSidebar((prev) => !prev);
+        deployNewGraph();
+    };
+
+    const handleCouncilDistanceChange = (newDistance: number) => {
+        uiOptions.current.councilDistance = newDistance;
+        setRerenderSidebar((prev) => !prev);
+        applyCouncilDistance(
+            JSON.parse(JSON.stringify(rawData[dataSetName])),
+            uiOptions.current,
+        );
+    };
+
+    const handleCheckAttestations = (e: React.MouseEvent<HTMLInputElement>) => {
+        const checked = e.currentTarget.checked;
+        uiOptions.current.checkAttestations = checked;
+        setRerenderSidebar((prev) => !prev);
+        applyLinkColors(uiOptions.current, SELECTOR.LINK, checked);
+    };
+
+    const handleCheckBackAttestations = (
+        e: React.MouseEvent<HTMLInputElement>,
+    ) => {
+        const checked = e.currentTarget.checked;
+        uiOptions.current.checkBackAttestations = checked;
+        setRerenderSidebar((prev) => !prev);
+        applyLinkColors(uiOptions.current, SELECTOR.BACK_LINK, checked);
+    };
+
+    const handleCheckAccountCreations = (
+        e: React.MouseEvent<HTMLInputElement>,
+    ) => {
+        const checked = e.currentTarget.checked;
+        uiOptions.current.checkAccountCreations = checked;
+        setRerenderSidebar((prev) => !prev);
+        applyLinkColors(
+            uiOptions.current,
+            SELECTOR.ACCOUNT_CREATION_LINK,
+            checked,
+        );
+    };
+
+    const handleResetToFullGraph = () => {
+        if (uiOptions.current.graphMode === "subGraph") {
+            d3.selectAll<SVGPathElement, AttestationGraphLink>(
+                ".back-link",
+            ).remove();
+
+            uiOptions.current.graphMode = "fullGraph";
+            uiOptions.current.centeredNodeIdx = -1;
+            setRerenderSidebar((prev) => !prev);
+            deployNewGraph();
+        }
+    };
+    const handleCheckCouncilDistance = (
+        e: React.MouseEvent<HTMLInputElement>,
+    ) => {
+        const checked = e.currentTarget.checked;
+        uiOptions.current.checkCouncilDistance = checked;
+        setRerenderSidebar((prev) => !prev);
+        applyLinkColors(
+            uiOptions.current,
+            SELECTOR.LINK,
+            uiOptions.current.checkAttestations,
+        );
+    };
+
+    return (
+        <div>
+            <Sidebar
+                rerenderSidebar={rerenderSidebar}
+                uiOptions={uiOptions.current}
+                handlers={{
+                    handleLinkColorChange,
+                    handleGraphDepthChange,
+                    handleCouncilDistanceChange,
+                    handleCheckAttestations,
+                    handleCheckBackAttestations,
+                    handleCheckAccountCreations,
+                    handleCheckCouncilDistance,
+                    handleResetToFullGraph,
+                }}
+            />
+            <div className="workspace">
+                <SvgCanvas
+                    width={width}
+                    height={height}
+                    viewBoxWidth={viewBoxWidth}
+                    viewBoxScalar={viewBoxScalar}
+                />
+            </div>
+        </div>
     );
-  };
-
-  const handleCheckAttestations = (e: React.MouseEvent<HTMLInputElement>) => {
-    const checked = e.currentTarget.checked;
-    uiOptions.current.checkAttestations = checked;
-    setRerenderSidebar((prev) => !prev);
-    applyLinkColors(uiOptions.current, SELECTOR.LINK, checked);
-  };
-
-  const handleCheckBackAttestations = (
-    e: React.MouseEvent<HTMLInputElement>
-  ) => {
-    const checked = e.currentTarget.checked;
-    uiOptions.current.checkBackAttestations = checked;
-    setRerenderSidebar((prev) => !prev);
-    applyLinkColors(uiOptions.current, SELECTOR.BACK_LINK, checked);
-  };
-
-  const handleCheckAccountCreations = (
-    e: React.MouseEvent<HTMLInputElement>
-  ) => {
-    const checked = e.currentTarget.checked;
-    uiOptions.current.checkAccountCreations = checked;
-    setRerenderSidebar((prev) => !prev);
-    applyLinkColors(uiOptions.current, SELECTOR.ACCOUNT_CREATION_LINK, checked);
-  };
-
-  const handleResetToFullGraph = () => {
-    if (uiOptions.current.graphMode === "subGraph") {
-      d3.selectAll<SVGPathElement, AttestationGraphLink>(".back-link").remove();
-
-      uiOptions.current.graphMode = "fullGraph";
-      uiOptions.current.centeredNodeIdx = -1;
-      setRerenderSidebar((prev) => !prev);
-      deployNewGraph();
-    }
-  };
-  const handleCheckCouncilDistance = (
-    e: React.MouseEvent<HTMLInputElement>
-  ) => {
-    const checked = e.currentTarget.checked;
-    uiOptions.current.checkCouncilDistance = checked;
-    setRerenderSidebar((prev) => !prev);
-    applyLinkColors(
-      uiOptions.current,
-      SELECTOR.LINK,
-      uiOptions.current.checkAttestations
-    );
-  };
-
-  return (
-    <div>
-      <Sidebar
-        rerenderSidebar={rerenderSidebar}
-        uiOptions={uiOptions.current}
-        handlers={{
-          handleLinkColorChange,
-          handleGraphDepthChange,
-          handleCouncilDistanceChange,
-          handleCheckAttestations,
-          handleCheckBackAttestations,
-          handleCheckAccountCreations,
-          handleCheckCouncilDistance,
-          handleResetToFullGraph,
-        }}
-      />
-      <div className="workspace">
-        <SvgCanvas
-          width={width}
-          height={height}
-          viewBoxWidth={viewBoxWidth}
-          viewBoxScalar={viewBoxScalar}
-        />
-      </div>
-    </div>
-  );
 }
 
 export default App;
