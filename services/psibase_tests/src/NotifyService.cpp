ce37f80d246062e761123c82359e020e92443b70
Steven Watanabe <steven@clarionos.com>
2025-07-17 09:19:02 -0600

    Callbacks that fail should not modify the database
    - In particular, running out of memory while writing to tables with
      multiple indexes can leave broken invariants.

---
+++ services/psibase_tests/src/NotifyService.cpp
@@ -0,0 +1,41 @@
+#include <services/test/NotifyService.hpp>
+
+#include <psibase/dispatch.hpp>
+
+using namespace psibase;
+using namespace TestService;
+
+void NotifyService::onBlock()
+{
+   auto count = WriteOnly{getReceiver()}.open<BlockCountTable>();
+   auto row   = count.get({}).value_or(BlockCountRow{});
+   ++row.count;
+   count.put(row);
+
+   PSIBASE_SUBJECTIVE_TX
+   {
+      if (Subjective{getReceiver()}.open<FailTable>().get({}))
+      {
+         abortMessage("as you wish");
+      }
+   }
+}
+
+void NotifyService::onTrx(const psibase::Checksum256& /*id*/,
+                          psio::view<const psibase::TransactionTrace> /*trace*/)
+{
+   auto count = WriteOnly{getReceiver()}.open<TransactionCountTable>();
+   auto row   = count.get({}).value_or(TransactionCountRow{});
+   ++row.count;
+   count.put(row);
+
+   PSIBASE_SUBJECTIVE_TX
+   {
+      if (Subjective{getReceiver()}.open<FailTable>().get({}))
+      {
+         abortMessage("as you wish");
+      }
+   }
+}
+
+PSIBASE_DISPATCH(NotifyService)
