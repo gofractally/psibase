38754033211b631fcb00912e4dda95ccaaa41189
James-Mart <jmartx93@gmail.com>
2025-07-21 19:00:31 +0000

    integrate new permissions plugin with auth-sig

--- services/system/AuthSig/plugin/src/lib.rs
+++ services/system/AuthSig/plugin/src/lib.rs
@@ -13,7 +13,7 @@ use types::*;
 use bindings::auth_sig::plugin::types::{Keypair, Pem};
 use bindings::host::common::client::get_sender_app;
 use bindings::host::common::types as CommonTypes;
-use bindings::permissions::plugin::api::{is_auth, prompt_auth};
+use bindings::permissions::plugin::{api::authorize, types::Risk};
 use bindings::transact::plugin::intf as Transact;
 
 // Exported interfaces
@@ -28,8 +28,57 @@ use p256::ecdsa::{signature::hazmat::PrehashSigner, Signature, SigningKey, Verif
 use p256::pkcs8::{DecodePrivateKey, EncodePrivateKey, EncodePublicKey, LineEnding};
 use psibase::fracpack::Pack;
 use rand_core::OsRng;
+use std::collections::HashMap;
 
 struct AuthSig;
+
+fn get_risk_description(level: u8) -> &'static str {
+    match level {
+        2 => indoc::indoc! {"
+            - Create new keypairs
+            - Import existing keypairs
+            - Consume account resources
+        "},
+        5 => indoc::indoc! {"
+            - Set the public key for your account
+            - Sign transactions on your behalf
+            - Extract your private key from your public key
+            - Consume account resources
+        "},
+        _ => "",
+    }
+}
+
+fn get_risk_levels() -> HashMap<&'static str, u8> {
+    let mut map = HashMap::new();
+    map.insert("generate_keypair", 2);
+    map.insert("generate_unmanaged_keypair", 0);
+    map.insert("pub_from_priv", 0);
+    map.insert("priv_from_pub", 5);
+    map.insert("to_der", 0);
+    map.insert("sign", 5);
+    map.insert("import_key", 2);
+    map.insert("set_key", 5);
+    map
+}
+
+lazy_static::lazy_static! {
+    static ref RISK_LEVELS: HashMap<&'static str, u8> = get_risk_levels();
+}
+
+fn check_authorization(fn_name: &str) -> Result<(), Error> {
+    let level = RISK_LEVELS.get(fn_name).unwrap_or(&6);
+    let description = get_risk_description(*level).to_string();
+    let r = Risk {
+        level: *level,
+        description,
+    };
+    let result = authorize(&get_sender_app().app.unwrap(), &r)?;
+    if !result {
+        return Err(Unauthorized("authorize").into());
+    }
+    Ok(())
+}
 
 impl HookUserAuth for AuthSig {
     fn on_user_auth_claim(account_name: String) -> Result<Option<Claim>, Error> {
@@ -65,12 +114,15 @@ impl HookUserAuth for AuthSig {
 
 impl KeyVault for AuthSig {
     fn generate_keypair() -> Result<String, CommonTypes::Error> {
+        check_authorization("generate_keypair")?;
         let keypair = AuthSig::generate_unmanaged_keypair()?;
         ManagedKeys::add(&keypair.public_key, &AuthSig::to_der(keypair.private_key)?);
         Ok(keypair.public_key)
     }
 
     fn generate_unmanaged_keypair() -> Result<Keypair, CommonTypes::Error> {
+        check_authorization("generate_unmanaged_keypair")?;
+
         let signing_key = SigningKey::random(&mut OsRng);
         let verifying_key: &VerifyingKey = signing_key.verifying_key();
 
@@ -89,6 +141,8 @@ impl KeyVault for AuthSig {
     }
 
     fn pub_from_priv(private_key: Pem) -> Result<Pem, CommonTypes::Error> {
+        check_authorization("pub_from_priv")?;
+
         let pem = pem::Pem::try_from_pem_str(&private_key)?;
         let signing_key =
             SigningKey::from_pkcs8_der(&pem.contents()).map_err(|e| CryptoError(e.to_string()))?;
@@ -100,12 +154,9 @@ impl KeyVault for AuthSig {
     }
 
     fn priv_from_pub(public_key: Pem) -> Result<Pem, CommonTypes::Error> {
-        if !from_auth_sig_ui() {
-            return Err(Unauthorized("priv_from_pub").into());
-        }
+        check_authorization("priv_from_pub")?;
 
         let private_key = ManagedKeys::get(&public_key);
-
         Ok(SigningKey::from_pkcs8_der(&private_key)
             .map_err(|e| CryptoError(e.to_string()))?
             .to_pkcs8_pem(LineEnding::LF)
@@ -114,11 +165,15 @@ impl KeyVault for AuthSig {
     }
 
     fn to_der(key: Pem) -> Result<Vec<u8>, CommonTypes::Error> {
+        check_authorization("to_der")?;
+
         let pem = pem::Pem::try_from_pem_str(&key)?;
         Ok(pem.contents().to_vec())
     }
 
     fn sign(hashed_message: Vec<u8>, private_key: Vec<u8>) -> Result<Vec<u8>, CommonTypes::Error> {
+        check_authorization("sign")?;
+
         let signing_key =
             SigningKey::from_pkcs8_der(&private_key).map_err(|e| CryptoError(e.to_string()))?;
         let signature: Signature = signing_key
@@ -128,6 +183,8 @@ impl KeyVault for AuthSig {
     }
 
     fn import_key(private_key: Pem) -> Result<Pem, CommonTypes::Error> {
+        check_authorization("import_key")?;
+
         let public_key = AuthSig::pub_from_priv(private_key.clone())?;
         ManagedKeys::add(&public_key, &AuthSig::to_der(private_key)?);
         Ok(public_key)
@@ -136,14 +193,7 @@ impl KeyVault for AuthSig {
 
 impl Actions for AuthSig {
     fn set_key(public_key: Pem) -> Result<(), CommonTypes::Error> {
-        let caller = get_sender_app().app.unwrap();
-
-        if !from_auth_sig_ui() {
-            if !is_auth(&caller)? {
-                prompt_auth(&caller)?;
-                return Err(Unauthorized("set_key").into());
-            }
-        }
+        check_authorization("set_key")?;
 
         Transact::add_action_to_transaction(
             MyService::setKey::ACTION_NAME,
