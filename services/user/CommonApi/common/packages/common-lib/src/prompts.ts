e262591456ae70b547c4ccda43c4ce242edd5e2c
James-Mart <jmartx93@gmail.com>
2025-07-22 19:17:30 +0000

    pr feedback - comment improvements

--- services/user/CommonApi/common/packages/common-lib/src/prompts.ts
+++ services/user/CommonApi/common/packages/common-lib/src/prompts.ts
@@ -6,29 +6,39 @@ import { siblingUrl } from "./rpc";
 
 const supervisorUrl = siblingUrl(null, "supervisor", null, true);
 
-/// This interface extracts some common functionality needed by the developer of a plugin prompt UI.
+/**
+ * Extracts common functionality needed by the developer of a plugin prompt UI.
+ */
 export const prompt = {
-
-  /// Get the contextId specified by your plugin when it triggered the prompt.
-  /// A contextId is often used to look up (within your plugin) additional inforamtion about
-  ///    the context of this prompt. 
+  /**
+   * Gets the contextId specified by your plugin when it triggered the prompt.
+   * A contextId is often used to look up (within your plugin) additional information about
+   * the context of this prompt.
+   * @returns {string | null} The contextId or null if not present.
+   */
   getContextId(): string | null {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get("context_id") || null;
   },
 
-  /// Call this when the user has finished interacting with your prompt. It will redirect the 
-  /// user back to the app from which they came.
+  /**
+   * Call this when the user has finished interacting with your prompt.
+   * It will redirect the user back to the app from which they came.
+   */
   finished(): void {
       window.parent.postMessage('finished', supervisorUrl);
   }
 };
 
-/// This function is used by root app UIs to automatically facilitate redirecting the 
-///     user to a prompt UI when triggered by a plugin.
-///
-/// The caller may specify a `returnPath`, which is used as the destination to
-/// which the user is returned when they are finished with the prompt.
+/**
+ * Used by root app UIs to automatically facilitate redirecting the
+ * user to a prompt UI when triggered by a plugin.
+ *
+ * The caller may specify a `returnPath`, which is used as the destination to
+ * which the user is returned when they are finished with the prompt.
+ * @param {unknown} e - The error or event object.
+ * @param {string} returnPath - The path to return the user to after the prompt.
+ */
 export const handlePluginUserPrompt = (e: unknown, returnPath: string) => {
     if (isRedirectErrorObject(e)) {
         if (
