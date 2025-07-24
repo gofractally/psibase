c2377a1a969b01c5e088f051870cf19ba044f91d
James-Mart <jmartx93@gmail.com>
2025-07-24 20:22:34 +0000

    prefer 'trust' wording over 'risk' wording

--- services/user/Permissions/plugin/src/errors.rs
+++ services/user/Permissions/plugin/src/errors.rs
@@ -3,5 +3,5 @@ use psibase::plugin_error;
 plugin_error! {
     pub ErrorType
     LoggedInUserDNE(caller: String, callee: String, debug_label: String) => "[{caller} -> {callee}] - {debug_label} - User must be logged in to perform this action",
-    InvalidRiskLevel(callee: String, level: u8, debug_label: String) => "[{callee}] - {debug_label} - Invalid risk level: {level}. Must be between 1 and 5 inclusive.",
+    InvalidTrustLevel(callee: String, level: u8, debug_label: String) => "[{callee}] - {debug_label} - Invalid trust level: {level}. Must be between 1 and 5 inclusive.",
 }
