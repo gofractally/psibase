3a5f0fb41e5155d73340e7a66a5e2c64cee94d34
James-Mart <jmartx93@gmail.com>
2025-07-26 06:17:40 +0000

    add helpful queries

--- services/user/Registry/query/src/lib.rs
+++ services/user/Registry/query/src/lib.rs
@@ -10,9 +10,19 @@ fn increment_last_char(s: String) -> String {
 #[allow(non_snake_case)]
 mod service {
     use super::increment_last_char;
-    use ::registry::service::{self, AppMetadataTable, TagsTable};
-    use async_graphql::*;
+    use ::registry::service::{self, AppMetadataTable, TagRecord, TagsTable};
+    use async_graphql::{connection::Connection, *};
     use psibase::*;
+    use serde::Deserialize;
+    use serde_aux::field_attributes::deserialize_number_from_string;
+    use std::str::FromStr;
+
+    #[derive(SimpleObject, Deserialize, Debug)]
+    struct StatusUpdate {
+        app_account_id: AccountNumber,
+        #[serde(deserialize_with = "deserialize_number_from_string")]
+        status: u32,
+    }
 
     pub struct Query;
 
@@ -26,7 +36,7 @@ mod service {
         }
 
         /// Get a list of tags that contain the given substring
-        async fn all_related_tags(&self, tag: String) -> Vec<String> {
+        async fn related_tags(&self, tag: String) -> Vec<String> {
             let from = tag.to_lowercase();
             let excluded_to = increment_last_char(from.clone());
 
@@ -36,6 +46,42 @@ mod service {
                 .take(10)
                 .map(|tag_row| tag_row.tag)
                 .collect()
+        }
+
+        /// Paginated query for retrieving all tags
+        async fn get_all_tags(
+            &self,
+            first: Option<i32>,
+            last: Option<i32>,
+            before: Option<String>,
+            after: Option<String>,
+        ) -> async_graphql::Result<Connection<RawKey, TagRecord>> {
+            TableQuery::new(TagsTable::with_service(service::SERVICE).get_index_by_tags())
+                .first(first)
+                .last(last)
+                .before(before)
+                .after(after)
+                .query()
+                .await
+        }
+
+        /// Paginated query for retrieving app status history
+        async fn status_history(
+            &self,
+            app: String,
+            first: Option<i32>,
+            last: Option<i32>,
+            before: Option<String>,
+            after: Option<String>,
+        ) -> async_graphql::Result<Connection<u64, StatusUpdate>> {
+            let app = ExactAccountNumber::from_str(&app).unwrap();
+            EventQuery::new("history.registry.appStatusChanged")
+                .condition(format!("app_account_id = '{}'", app))
+                .first(first)
+                .last(last)
+                .before(before)
+                .after(after)
+                .query()
         }
     }
 
