f0777352b3dbfbf64144d93e8be96481451785da
James-Mart <jmartx93@gmail.com>
2025-08-05 02:51:55 +0000

    rework the macro to use the new permissions api

--- rust/psibase/src/trust.rs
+++ rust/psibase/src/trust.rs
@@ -11,25 +11,25 @@
 ///
 /// Example usage:
 /// ```rust,ignore
-/// define_trust! {
+/// psibase::define_trust! {
 ///     descriptions {
-///         1 => "
-///             Level 1 trust grants these abilities:
+///         Low => "
+///         Low trust grants these abilities:
 ///             - Create new keypairs
 ///             - Import existing keypairs
 ///         ",
-///         3 => "
-///             Level 3 trust grants these abilities:
+///         Medium => "",
+///         High => "
+///         High trust grants the abilities of all lower trust levels, plus these abilities:
 ///             - Set the public key for your account
 ///             - Sign transactions on your behalf
 ///             - Read the private key for a given public key
-///             - Create new keypairs
-///             - Import existing keypairs
 ///         ",
 ///     }
 ///     functions {
-///         1 => [generate_keypair, import_key],
-///         3 => [set_key],
+///         None => [generate_unmanaged_keypair, pub_from_priv, to_der, sign],
+///         Low => [generate_keypair, import_key],
+///         High => [priv_from_pub, set_key],
 ///     }
 /// }
 ///
@@ -45,67 +45,57 @@
 macro_rules! define_trust {
     (
         descriptions {
-            $($desc_level:literal => $desc:literal,)*
+            $($desc_level:ident => $desc:literal,)*
         }
         functions {
-            $($func_level:literal => [$($func_name:ident),* $(,)?],)*
+            $($func_level:ident => [$($func_name:ident),* $(,)?],)*
         }
     ) => {
-        use crate::bindings::permissions::plugin::types::Trust;
+        use crate::bindings::permissions::plugin::types::{Descriptions, TrustLevel};
         use std::collections::HashMap;
 
         $crate::lazy_static::lazy_static! {
-            static ref TRUST_LEVELS: HashMap<&'static str, u8> = TrustRequirement::get_trust_levels();
+            static ref TRUST_LEVELS: HashMap<&'static str, TrustLevel> = TrustRequirement::get_trust_levels();
         }
 
         struct TrustRequirement;
 
         impl TrustRequirement {
-            fn get_trust_levels() -> HashMap<&'static str, u8> {
+            fn get_trust_levels() -> HashMap<&'static str, TrustLevel> {
                 let mut map = HashMap::new();
                 $(
                     $(
-                        map.insert(stringify!($func_name), $func_level);
+                        map.insert(stringify!($func_name), TrustLevel::$func_level);
                     )*
                 )*
                 map
             }
 
-            fn get_description(level: u8) -> String {
-                match level {
+            fn get_descriptions() -> Descriptions {
+                (
                     $(
-                        $desc_level => $crate::indoc::indoc! { $desc },
+                        $crate::indoc::indoc! { $desc }.to_string(),
                     )*
-                    _ => "",
-                }
-                .to_string()
+                )
             }
 
-            fn get_level(fn_name: &str) -> u8 {
-                *TRUST_LEVELS.get(fn_name).unwrap_or(&4)
-            }
-        }
-
-        fn get_trust_requirement(fn_name: &str) -> Trust {
-            let level = TrustRequirement::get_level(fn_name);
-            Trust {
-                level,
-                description: TrustRequirement::get_description(level),
+            fn get_level(fn_name: &str) -> TrustLevel {
+                *TRUST_LEVELS.get(fn_name).unwrap_or(&TrustLevel::Max)
             }
         }
 
         pub mod trust {
+            use super::TrustRequirement;
             use crate::bindings::host::common::client::get_sender;
             use crate::bindings::host::common::types::Error;
             use crate::bindings::permissions::plugin::api as Permissions;
-            use super::get_trust_requirement;
 
             pub fn authorize(fn_name: &str, whitelist: Option<Vec<String>>) -> Result<(), Error> {
-
                 let whitelist = whitelist.unwrap_or_default();
                 Permissions::authorize(
                     &get_sender(),
-                    &get_trust_requirement(fn_name),
+                    TrustRequirement::get_level(fn_name),
+                    &TrustRequirement::get_descriptions(),
                     fn_name,
                     &whitelist,
                 )?;
