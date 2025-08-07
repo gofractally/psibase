a69ee51dcd50831e8cab7e36180f41907713f596
Steven Watanabe <steven@clarionos.com>
2025-08-07 15:10:39 -0600

    Update verify-sig

--- rust/psibase/src/services/verify_sig.rs
+++ rust/psibase/src/services/verify_sig.rs
@@ -1,5 +1,17 @@
 #[crate::service(name = "verify-sig", dispatch = false, psibase_mod = "crate")]
 #[allow(non_snake_case, unused_variables)]
 mod service {
+    use crate::{Checksum256, Claim, Hex};
+
     const SERVICE_FLAGS: u64 = crate::CodeRow::IS_AUTH_SERVICE;
+
+    #[action]
+    fn verifySys(transactionHash: Checksum256, claim: Claim, proof: Hex<Vec<u8>>) {
+        unimplemented!()
+    }
+}
+
+#[test]
+fn verify_schema() {
+    crate::assert_schema_matches_package::<Wrapper>();
 }
