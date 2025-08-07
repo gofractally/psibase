d518ab52c375c1cf1596257f549ea6d5010f5a13
Steven Watanabe <steven@clarionos.com>
2025-08-07 14:46:28 -0600

    Update symbol

--- rust/psibase/src/services/symbol.rs
+++ rust/psibase/src/services/symbol.rs
@@ -4,11 +4,25 @@ use fracpack::{Pack, ToSchema, Unpack};
 use serde::{Deserialize, Serialize};
 
 use crate::services::{nft, tokens};
-use crate::AccountNumber;
+use crate::{AccountNumber, BlockTime};
 use nft::NID;
 use tokens::Quantity;
 
 pub type SID = AccountNumber;
+
+#[derive(
+    Debug, Copy, Clone, Pack, Unpack, ToSchema, Serialize, Deserialize, SimpleObject, InputObject,
+)]
+#[fracpack(fracpack_mod = "fracpack")]
+struct SymbolLengthRecord {
+    symbolLength: u8,
+    targetCreatedPerDay: u8,
+    floorPrice: Quantity,
+    activePrice: Quantity,
+
+    createCounter: u8,
+    lastPriceUpdateTime: BlockTime,
+}
 
 #[derive(
     Debug, Copy, Clone, Pack, Unpack, ToSchema, Serialize, Deserialize, SimpleObject, InputObject,
@@ -33,8 +47,13 @@ pub struct SymbolRecord {
 #[crate::service(name = "symbol", dispatch = false, psibase_mod = "crate")]
 #[allow(unused_variables)]
 pub mod Service {
-    use crate::services::symbol::SymbolRecord;
-    use crate::{services::tokens::Quantity, AccountNumber};
+    use crate::services::symbol::{SymbolLengthRecord, SymbolRecord, SID};
+    use crate::{services::tokens::Quantity, AccountNumber, HttpReply, HttpRequest};
+
+    #[action]
+    fn init() {
+        unimplemented!()
+    }
 
     #[action]
     fn create(new_symbol: AccountNumber, max_debit: Quantity) {
@@ -65,4 +84,38 @@ pub mod Service {
     fn getSymbol(symbol: AccountNumber) -> SymbolRecord {
         unimplemented!()
     }
+
+    #[action]
+    fn getPrice(num_chars: u8) -> Quantity {
+        unimplemented!()
+    }
+
+    #[action]
+    fn updatePrices() {
+        unimplemented!()
+    }
+
+    #[action]
+    fn getSymbolType(numChars: u8) -> SymbolLengthRecord {
+        unimplemented!()
+    }
+
+    #[action]
+    fn serveSys(request: HttpRequest) -> Option<HttpReply> {
+        unimplemented!()
+    }
+
+    #[event(history)]
+    fn symCreated(symbol: SID, owner: AccountNumber, cost: Quantity) {
+        unimplemented!()
+    }
+    #[event(history)]
+    fn symSold(symbol: SID, buyer: AccountNumber, seller: AccountNumber, cost: Quantity) {
+        unimplemented!()
+    }
+}
+
+#[test]
+fn verify_schema() {
+    crate::assert_schema_matches_package::<Wrapper>();
 }
