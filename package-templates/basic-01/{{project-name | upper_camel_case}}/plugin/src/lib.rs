8b0533c19d98d87024bfde0bc5eaf7b908b58d39
James-Mart <jmartx93@gmail.com>
2025-08-11 18:07:01 +0000

    integrate trust-configs with package-template

--- package-templates/basic-01/{{project-name | upper_camel_case}}/plugin/src/lib.rs
+++ package-templates/basic-01/{{project-name | upper_camel_case}}/plugin/src/lib.rs
@@ -7,17 +7,38 @@ use bindings::host::common::server as CommonServer;
 use bindings::host::common::types::Error;
 use bindings::transact::plugin::intf::add_action_to_transaction;
 
+use psibase::define_trust;
 use psibase::fracpack::Pack;
 
 mod errors;
 use errors::ErrorType;
 
+define_trust! {
+    descriptions {
+        Low => "
+        Low trust grants these abilities:
+            - Reading the value of the example-thing
+        ",
+        Medium => "",
+        High => "
+        High trust grants the abilities of all lower trust levels, plus these abilities:
+            - Setting the example thing
+        ",
+    }
+    functions {
+        Low => [get_example_thing],
+        High => [set_example_thing],
+    }
+}
+
 struct {{project-name | upper_camel_case}}Plugin;
 
 impl Api for {{project-name | upper_camel_case}}Plugin {
-    fn set_example_thing(thing: String) {
+    fn set_example_thing(thing: String) -> Result<(), Error> {
+        trust::authorize(trust::FunctionName::set_example_thing)?;
         let packed_example_thing_args = {{project-name | snake_case}}::action_structs::setExampleThing { thing }.packed();
         add_action_to_transaction("setExampleThing", &packed_example_thing_args).unwrap();
+        Ok(())
     }
 }
 
@@ -34,14 +55,16 @@ struct ExampleThingResponse {
 
 impl Queries for {{project-name | upper_camel_case}}Plugin {
     fn get_example_thing() -> Result<String, Error> {
+        trust::authorize(trust::FunctionName::get_example_thing)?;
+
         let graphql_str = "query { exampleThing }";
 
         let examplething_val = serde_json::from_str::<ExampleThingResponse>(
             &CommonServer::post_graphql_get_json(&graphql_str)?,
         );
 
-        let examplething_val = examplething_val
-            .map_err(|err| ErrorType::QueryResponseParseError(err.to_string()))?;
+        let examplething_val = 
+            examplething_val.map_err(|err| ErrorType::QueryResponseParseError(err.to_string()))?;
 
         Ok(examplething_val.data.example_thing)
     }
