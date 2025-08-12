eb8ad56356848879aeab13f93697b4c1a2d0753c
Mike Manfredi <mike@fractally.com>
2025-08-12 22:39:15 +0000

    removing debug thing

--- packages/system/Accounts/plugin/accounts/src/errors.rs
+++ packages/system/Accounts/plugin/accounts/src/errors.rs
@@ -1,7 +1,6 @@
 use psibase::plugin_error;
 
 plugin_error! {
-    #[derive(Debug)]
     pub ErrorType<'a>
     Unauthorized(msg: &'a str) => "Unauthorized access: {msg}",
     InvalidAccountName(msg: String) => "Invalid account name: {msg}",
