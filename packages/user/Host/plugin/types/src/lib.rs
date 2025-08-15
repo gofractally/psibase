abe0f721eef10bfa92a0335509b1fa794a9d4acf
James-Mart <jmartx93@gmail.com>
2025-08-15 19:12:10 +0000

    finish moving plugin-ref to host:types

--- packages/user/Host/plugin/types/src/lib.rs
+++ packages/user/Host/plugin/types/src/lib.rs
@@ -10,9 +10,26 @@ pub struct PluginRefImpl {
     intf: String,
 }
 
+fn to_camel(s: &str) -> String {
+    s.split('-')
+        .enumerate()
+        .map(|(i, part)| {
+            if i == 0 {
+                part.to_string()
+            } else {
+                part[..1].to_uppercase() + &part[1..]
+            }
+        })
+        .collect()
+}
+
 impl GuestPluginRef for PluginRefImpl {
     fn new(service: String, plugin: String, intf: String) -> Self {
-        Self { service, plugin, intf }
+        Self {
+            service,
+            plugin,
+            intf: to_camel(&intf),
+        }
     }
 
     fn get_service(&self) -> String {
