b8a08fa52a156e4ed0accc315e112334653d6e76
James-Mart <jmartx93@gmail.com>
2025-08-21 04:20:20 +0000

    make prompt interface x-platform

--- packages/user/Host/plugin/prompt/src/errors.rs
+++ packages/user/Host/plugin/prompt/src/errors.rs
@@ -7,4 +7,6 @@ plugin_error! {
     PromptExpired(id: u32) => "[Error] Prompt expired: {id}",
     InvalidSubpath() => "[Error] Invalid subpath. Subpath must not be a full URL.",
     PromptContextNotFound(context_id: String) => "[Error] Prompt context not found: {context_id}",
+    InvalidPromptName() => "[Error] Invalid prompt name. Prompt name must be [a-zA-Z0-9].",
+    OpenPromptError(err: String) = 999999999 => "{err}",
 }
