8e50aec8ade9cbfbc287dc9f9dc4b7cc27c4090a
Brandon Fancher <brandonfancher@gmail.com>
2025-08-25 19:29:35 +0000

    Remove mint and burn.

--- packages/user/Homepage/ui/src/apps/tokens/components/forms/fields/index.ts
+++ packages/user/Homepage/ui/src/apps/tokens/components/forms/fields/index.ts
@@ -1,4 +1,3 @@
 export { default as TokenSelection } from "./TokenSelection";
 export { default as RecipientInput } from "./RecipientInput";
-export { default as FromInput } from "./FromInput";
 export { default as AmountInput } from "./AmountInput";
