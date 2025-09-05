8bfa44bb4f417ba61bb250cbe6778016d797d9c0
John Williamson <johnjwilliamson@live.com>
2025-09-05 04:35:13 +0000

    remove stale error

--- packages/user/TokenStream/plugin/src/errors.rs
+++ packages/user/TokenStream/plugin/src/errors.rs
@@ -2,6 +2,5 @@ use psibase::plugin_error;
 
 plugin_error! {
     pub ErrorType
-    QueryResponseParseError(msg: String) => "Query response parsing error: {msg}",
     NotImplemented(msg: String) => "Feature not implemented: {msg}",
 }
