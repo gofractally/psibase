dd379df212d5d4608796ff917d698a6ed44a2bc7
John Williamson <johnjwilliamson@live.com>
2025-09-20 01:34:54 +0000

    add wrapper info

--- rust/psibase/src/services/rate_limit.rs
+++ rust/psibase/src/services/rate_limit.rs
@@ -1,18 +1,23 @@
 #[crate::service(name = "rate-limit", dispatch = false, psibase_mod = "crate")]
 #[allow(non_snake_case, unused_variables)]
 pub mod Service {
-    use crate::services::tokens::Quantity;
 
-    /// Creates a new rate limit instance
+    /// Creates a new Rate limit
     ///
     /// # Arguments
-    /// * `initial_price` - Sets initial price
+    /// * `initial_difficulty` - Sets initial difficulty
     /// * `window_seconds` - Seconds duration before decay occurs
-    /// * `target` - Difficulty target
-    /// * `floor_price` - Minimum price.
+    /// * `target` - Rate limit target
+    /// * `floor_difficulty` - Minimum price
     /// * `percent_change` - Percent to increment / decrement, 5 = 5%
     #[action]
-    fn create(window_seconds: u32, target: u32, floor_price: Quantity, percent_change: u8) -> u32 {
+    fn create(
+        initial_difficulty: u64,
+        window_seconds: u32,
+        target: u32,
+        floor_difficulty: u64,
+        percent_change: u8,
+    ) -> u32 {
         unimplemented!()
     }
 
@@ -24,18 +29,55 @@ pub mod Service {
     /// # Returns
     /// Price of RateLimit
     #[action]
-    fn get_price(nft_id: u32) -> Quantity {
+    fn get_price(nft_id: u32) -> u64 {
         unimplemented!()
     }
 
-    /// Increment RateLimit instance, potentially increasing difficulty
+    /// Increment RateLimit instance, potentially increasing RateLimit
     ///
     /// * Requires holding administration NFT.
     ///
     /// # Arguments
     /// * `nft_id` - RateLimit / NFT ID
+    /// * `amount` - Amount to increment the counter by
     #[action]
-    fn increment(nft_id: u32) -> Quantity {
+    fn increment(nft_id: u32, amount: u32) -> u64 {
+        unimplemented!()
+    }
+
+    /// Update target
+    ///
+    /// * Requires holding administration NFT.
+    ///
+    /// # Arguments
+    /// * `nft_id` - RateLimit / NFT ID
+    /// * `target` - Target difficulty
+    #[action]
+    fn up_target(nft_id: u32, target: u32) {
+        unimplemented!()
+    }
+
+    /// Update window
+    ///
+    /// * Requires holding administration NFT.
+    ///
+    /// # Arguments
+    /// * `nft_id` - RateLimit / NFT ID
+    /// * `seconds` - Seconds
+    #[action]
+    fn up_window(nft_id: u32, seconds: u32) {
+        unimplemented!()
+    }
+
+    /// Update floor difficulty
+    ///
+    /// * Requires holding administration NFT.
+    ///
+    /// # Arguments
+    /// * `nft_id` - RateLimit / NFT ID
+    /// * `difficulty` - Difficulty
+    #[action]
+    fn up_floor(nft_id: u32, difficulty: u64) {
         unimplemented!()
     }
 
