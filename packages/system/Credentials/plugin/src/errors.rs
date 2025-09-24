2745b1d1c7ad83c1eb7054a2eb29664df4d1b1d5
James-Mart <jmartx93@gmail.com>
2025-09-24 18:32:48 +0000

    misc cleanup

--- packages/system/Credentials/plugin/src/errors.rs
+++ packages/system/Credentials/plugin/src/errors.rs
@@ -3,5 +3,4 @@ use psibase::plugin_error;
 plugin_error! {
     pub ErrorType<'a>
     Unauthorized(msg: &'a str) => "Unauthorized: {msg}",
-    DecodeInviteError(msg: &'a str) => "Decode invite error: {msg}",
 }
