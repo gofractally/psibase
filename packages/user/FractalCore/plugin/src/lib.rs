9a1721109d0c931bd3f48d2097d58ac119e16788
John Williamson <johnjwilliamson@live.com>
2025-09-30 06:35:32 +0000

    remove queries

--- packages/user/FractalCore/plugin/src/lib.rs
+++ packages/user/FractalCore/plugin/src/lib.rs
@@ -2,7 +2,6 @@
 mod bindings;
 
 use bindings::exports::fractal_core::plugin::api::Guest as Api;
-use bindings::exports::fractal_core::plugin::queries::Guest as Queries;
 use bindings::host::common::server as CommonServer;
 use bindings::host::types::types::Error;
 use bindings::transact::plugin::intf::add_action_to_transaction;
@@ -45,7 +44,7 @@ define_trust! {
         ",
     }
     functions {
-        Low => [get_example_thing],
+        Low => [],
         High => [start_eval],
     }
 }
@@ -63,34 +62,6 @@ impl Api for FractalCorePlugin {
         trust::assert_authorized(trust::FunctionName::start_eval)?;
 
         bindings::fractals::plugin::user::join()
-    }
-}
-
-#[derive(serde::Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct ExampleThingData {
-    example_thing: String,
-}
-#[derive(serde::Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct ExampleThingResponse {
-    data: ExampleThingData,
-}
-
-impl Queries for FractalCorePlugin {
-    fn get_example_thing() -> Result<String, Error> {
-        trust::assert_authorized(trust::FunctionName::get_example_thing)?;
-
-        let graphql_str = "query { exampleThing }";
-
-        let examplething_val = serde_json::from_str::<ExampleThingResponse>(
-            &CommonServer::post_graphql_get_json(&graphql_str)?,
-        );
-
-        let examplething_val =
-            examplething_val.map_err(|err| ErrorType::QueryResponseParseError(err.to_string()))?;
-
-        Ok(examplething_val.data.example_thing)
     }
 }
 
