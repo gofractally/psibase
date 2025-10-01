45bb7626b3110d0d6a370529b9a555e5077ed93d
John Williamson <johnjwilliamson@live.com>
2025-10-01 20:25:09 +0000

    trigger rebuild

--- packages/user/Tokens/query-service/src/events.rs
+++ packages/user/Tokens/query-service/src/events.rs
@@ -5,20 +5,19 @@ use serde::Deserialize;
 use serde_aux::field_attributes::deserialize_number_from_string;
 
 #[derive(Deserialize, SimpleObject)]
-pub struct ConfigureEvent {
-    #[serde(deserialize_with = "deserialize_number_from_string")]
-    token_id: TID,
-    action: String,
-    memo: Memo,
-}
-
-#[derive(Deserialize, SimpleObject)]
 pub struct SupplyEvent {
     #[serde(deserialize_with = "deserialize_number_from_string")]
     token_id: TID,
     counter_party: AccountNumber,
     action: String,
     amount: String,
+    memo: Memo,
+}
+#[derive(Deserialize, SimpleObject)]
+pub struct ConfigureEvent {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    token_id: TID,
+    action: String,
     memo: Memo,
 }
 
