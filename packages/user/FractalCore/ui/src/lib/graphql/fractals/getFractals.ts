2392adfb080910ad8aaeaf2d1b2754ce02fe0d54
John Williamson <johnjwilliamson@live.com>
2025-10-03 23:14:32 +0000

    fix spelling

--- packages/user/FractalCore/ui/src/lib/graphql/fractals/getFractals.ts
+++ packages/user/FractalCore/ui/src/lib/graphql/fractals/getFractals.ts
@@ -1,35 +1,39 @@
 import { z } from "zod";
 
+import { siblingUrl } from "@psibase/common-lib";
+
 import { zAccount } from "@/lib/zod/Account";
 
 import { graphql } from "../../graphql";
-import { siblingUrl } from "@psibase/common-lib";
 
-export const zFractaListInstance = z.object({
+export const zFractalListInstance = z.object({
     account: zAccount,
     name: z.string(),
     mission: z.string(),
 });
 
-export type FractalListInstance = z.infer<typeof zFractaListInstance>;
+export type FractalListInstance = z.infer<typeof zFractalListInstance>;
 
 export const getFractals = async () => {
-    const res = await graphql(`
-        {
-            fractals(first: 99) {
-                nodes {
-                    account
-                    name
-                    mission
+    const res = await graphql(
+        `
+            {
+                fractals(first: 99) {
+                    nodes {
+                        account
+                        name
+                        mission
+                    }
                 }
             }
-        }
-    `, siblingUrl(null, 'fractals', '/graphql'));
+        `,
+        siblingUrl(null, "fractals", "/graphql"),
+    );
 
     return z
         .object({
             fractals: z.object({
-                nodes: zFractaListInstance.array(),
+                nodes: zFractalListInstance.array(),
             }),
         })
         .parse(res).fractals.nodes;
