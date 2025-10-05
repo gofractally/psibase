db136f269c525823fdb450ba5559d53c73f54f0a
John Williamson <johnjwilliamson@live.com>
2025-10-05 07:23:55 +0000

    fix label

--- packages/user/FractalCore/ui/src/pages/settings/index.tsx
+++ packages/user/FractalCore/ui/src/pages/settings/index.tsx
@@ -1,6 +1,11 @@
 import { ScheduleDialog } from "@/components/schedule-dialog"
+import { useEvaluationInstance } from "@/hooks/fractals/use-evaluation-instance";
+import { useFractalAccount } from "@/hooks/fractals/use-fractal-account";
 import { useEvaluationStatus } from "@/hooks/use-evaluation-status";
+import { useGuildSlug } from "@/hooks/use-guild-id";
+import { useGuildBySlug } from "@/hooks/use-guild-slug-status";
 import { useNowUnix } from "@/hooks/use-now-unix";
+import dayjs from "dayjs";
 import { useState } from "react";
 
 
@@ -14,6 +19,14 @@ export const Settings = () => {
     const now = useNowUnix();
 
     const status = useEvaluationStatus(now);
+    const currentFractal = useFractalAccount();
+    const guildSlug = useGuildSlug();
+
+    const { data: guild, isPending: isGuildPending } = useGuildBySlug(currentFractal, guildSlug);
+
+    const isUpcomingEvaluation = !!guild?.evalInstance;
+
+    const { evaluation, } = useEvaluationInstance();
 
 
 
@@ -23,9 +36,9 @@ export const Settings = () => {
         <div className="border flex justify-between rounded-sm p-4 ">
             <div>
                 <div>Evaluations</div>
-                <div className="text-sm text-muted-foreground">
-                    Next schedule is for
-                </div>
+                {!isGuildPending && <div className="text-sm text-muted-foreground">
+                    {isUpcomingEvaluation && evaluation ? `Next evaluation at ${dayjs.unix(evaluation.registrationStarts).format('MMM, DD')}` : `No evaluation scheduled`}
+                </div>}
             </div>
             <div>
                 <ScheduleDialog
