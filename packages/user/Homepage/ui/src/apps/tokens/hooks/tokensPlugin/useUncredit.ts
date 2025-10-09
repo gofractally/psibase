5c5097631266ded8fd86fd758c6feda3076b7f52
Brandon Fancher <brandonfancher@gmail.com>
2025-10-09 20:20:46 +0000

    Uncredit and Debit optimistic updates and toasts.

--- packages/user/Homepage/ui/src/apps/tokens/hooks/tokensPlugin/useUncredit.ts
+++ packages/user/Homepage/ui/src/apps/tokens/hooks/tokensPlugin/useUncredit.ts
@@ -3,8 +3,11 @@ import { z } from "zod";
 
 import { supervisor } from "@/supervisor";
 
+import QueryKey from "@/lib/queryKeys";
 import { Account } from "@/lib/zod/Account";
 import { TokenId } from "@/lib/zod/TokenId";
+
+import { updateUserTokenBalancesCache } from "./useUserTokenBalances";
 
 const Args = z.object({
     tokenId: TokenId,
@@ -13,16 +16,56 @@ const Args = z.object({
     memo: z.string().default(""),
 });
 
-export const useUncredit = () =>
+export const useUncredit = (user: string | null) =>
     useMutation<void, Error, z.infer<typeof Args>>({
         mutationKey: ["uncredit"],
         mutationFn: (vars) => {
+            console.log("uncredit", vars);
             const { amount, memo, tokenId, debitor } = Args.parse(vars);
+
+            // Optimistically update the balance
+            updateUserTokenBalancesCache(
+                Account.parse(user),
+                tokenId,
+                amount,
+                "Add",
+            );
+
             return supervisor.functionCall({
                 service: "tokens",
                 method: "uncredit",
                 params: [tokenId, debitor, amount, memo],
                 intf: "transfer",
             });
+        },
+        onSuccess: (_data, vars, _result, context) => {
+            const parsedUser = Account.parse(user);
+
+            // Invalidate queries to update the token balance
+            context.client.invalidateQueries({
+                queryKey: QueryKey.userTokenBalances(parsedUser),
+            });
+
+            // Invalidate queries to update the recent transactions table
+            context.client.invalidateQueries({
+                queryKey: QueryKey.userTokenBalanceChanges(
+                    parsedUser,
+                    Number(vars.tokenId),
+                ),
+            });
+
+            // Invalidate queries to update the pending transactions table
+            context.client.invalidateQueries({
+                queryKey: QueryKey.userLinesOfCredit(parsedUser),
+            });
+        },
+        onError: (_error, vars) => {
+            // Rollback optimistic update on error
+            updateUserTokenBalancesCache(
+                Account.parse(user),
+                vars.tokenId,
+                vars.amount,
+                "Subtract",
+            );
         },
     });
