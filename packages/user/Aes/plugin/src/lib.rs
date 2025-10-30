edd6de1eeefa796b7d33a3c1d0b7e14779a155cf
James Mart <jmartx93@gmail.com>
2025-10-30 04:28:00 +0000

    fix warnings in aes plugin

--- packages/user/Aes/plugin/src/lib.rs
+++ packages/user/Aes/plugin/src/lib.rs
@@ -4,7 +4,7 @@ use bindings::*;
 
 use aes_gcm::{
     aead::{Aead, AeadCore, KeyInit, OsRng},
-    Aes256Gcm, Key, Nonce,
+    Aes256Gcm, Nonce,
 };
 use exports::aes::plugin::with_key::Guest as WithKey;
 use exports::aes::plugin::with_password::Guest as WithPassword;
@@ -15,8 +15,7 @@ const AES_KEY_SIZE: usize = 32;
 const NONCE_SIZE: usize = 12;
 
 fn encrypt_with_aes(key: &[u8; AES_KEY_SIZE], data: &[u8]) -> Vec<u8> {
-    let aes_key = Key::<Aes256Gcm>::from_slice(key);
-    let cipher = Aes256Gcm::new(aes_key);
+    let cipher = Aes256Gcm::new_from_slice(key).unwrap();
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
 
     let ciphertext = match cipher.encrypt(&nonce, data) {
@@ -27,7 +26,7 @@ fn encrypt_with_aes(key: &[u8; AES_KEY_SIZE], data: &[u8]) -> Vec<u8> {
     };
 
     let mut result = Vec::with_capacity(NONCE_SIZE + ciphertext.len());
-    result.extend_from_slice(nonce.as_slice());
+    result.extend_from_slice(nonce.as_ref());
     result.extend_from_slice(&ciphertext);
     result
 }
@@ -39,13 +38,13 @@ fn decrypt_with_aes(key: &[u8; AES_KEY_SIZE], encrypted_data: &[u8]) -> Result<V
 
     // The first NONCE_SIZE bytes are the nonce, the rest is the ciphertext
     let nonce_bytes = &encrypted_data[..NONCE_SIZE];
-    let nonce = Nonce::from_slice(nonce_bytes);
+    let nonce_array: [u8; NONCE_SIZE] = nonce_bytes.try_into().unwrap();
+    let nonce = Nonce::from(nonce_array);
 
-    let aes_key = Key::<Aes256Gcm>::from_slice(key);
-    let cipher = Aes256Gcm::new(aes_key);
+    let cipher = Aes256Gcm::new_from_slice(key).unwrap();
 
     let ciphertext = &encrypted_data[NONCE_SIZE..];
-    let decrypted = match cipher.decrypt(nonce, ciphertext) {
+    let decrypted = match cipher.decrypt(&nonce, ciphertext) {
         Ok(pt) => pt,
         Err(_) => {
             return Err(Error {
