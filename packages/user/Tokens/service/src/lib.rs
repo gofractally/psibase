6aa5342c60c6be28e9216f2195ddfd58a31a5477
John Williamson <johnjwilliamson@live.com>
2025-11-01 04:54:58 +0000

    remove stale

--- packages/user/Tokens/service/src/lib.rs
+++ packages/user/Tokens/service/src/lib.rs
@@ -8,7 +8,6 @@ pub mod service {
     pub use crate::tables::tables::{BalanceFlags, TokenFlags};
     use psibase::services::events;
     use psibase::services::nft::Wrapper as Nfts;
-    use psibase::services::symbol::Service::Wrapper as Symbol;
     use psibase::services::tokens::{Decimal, Precision, Quantity};
     use psibase::{get_sender, AccountNumber, Memo};
 
