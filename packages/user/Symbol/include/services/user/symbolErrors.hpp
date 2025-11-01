609d61a9f1319c899e444de839533c52098b6b46
John Williamson <johnjwilliamson@live.com>
2025-11-01 04:18:14 +0000

    remove space

--- packages/user/Symbol/include/services/user/symbolErrors.hpp
+++ packages/user/Symbol/include/services/user/symbolErrors.hpp
@@ -13,6 +13,5 @@ namespace UserService
       constexpr std::string_view buyerIsSeller        = "Buyer cannot be seller";
       constexpr std::string_view invalidSymbol =
           "Symbol may only contain 3 to 7 lowercase alphabetic characters";
-
    }  // namespace Errors
 }  // namespace UserService
