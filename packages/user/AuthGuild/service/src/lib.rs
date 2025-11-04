5b4868d4079706c03d56f4a4df3ab0f6ece68f66
John Williamson <johnjwilliamson@live.com>
2025-11-04 10:06:21 +0000

    implement function

--- packages/user/AuthGuild/service/src/lib.rs
+++ packages/user/AuthGuild/service/src/lib.rs
@@ -1,16 +1,105 @@
 #[psibase::service(name = "auth-guild")]
 pub mod service {
-    use psibase::services::transact::ServiceMethod;
+    use psibase::fracpack::{Pack, Unpack};
+    use psibase::services::auth_delegate;
+    use psibase::services::fractals::Wrapper as Fractals;
+    use psibase::services::transact::{self, ServiceMethod};
     use psibase::{check_some, *};
+
+    fn council_approval_threshold(len: usize) -> u8 {
+        (len as u8 * 2 + 2) / 3
+    }
+
+    fn check_council_or_delegate(
+        sender: AccountNumber,
+        signers: Vec<AccountNumber>,
+        auth_set: Option<Vec<AccountNumber>>,
+        is_approval: bool,
+    ) -> bool {
+        if auth_set
+            .as_ref()
+            .is_some_and(|auth_set| auth_set.contains(&sender))
+        {
+            return false;
+        }
+
+        let guild_rep = check_some(
+            Fractals::call().get_guild(sender),
+            "account must be a guild",
+        )
+        .rep;
+
+        let council = Fractals::call().get_g_counc(sender).unwrap_or_default();
+
+        let representative_included = guild_rep.is_some_and(|rep| signers.contains(&rep));
+        let council_member_included = council
+            .iter()
+            .any(|council_member| signers.contains(council_member));
+
+        let is_guild_consultation_required = council_member_included && !is_approval;
+
+        if representative_included && !is_guild_consultation_required {
+            return true;
+        }
+
+        let council_success = {
+            if council.is_empty() {
+                false
+            } else {
+                let council_members_signed = signers
+                    .iter()
+                    .filter(|&account| council.contains(account))
+                    .count() as u8;
+
+                let approval_min = council_approval_threshold(council.len());
+
+                let threshold = if is_approval {
+                    approval_min
+                } else {
+                    (council.len() as u8).saturating_sub(approval_min) + 1
+                };
+                council_members_signed >= threshold
+            }
+        };
+
+        if council_success {
+            return true;
+        }
+        if is_guild_consultation_required {
+            return false;
+        }
+
+        let mut auth_set_next = auth_set.unwrap_or_default();
+        auth_set_next.push(sender);
+
+        guild_rep.map_or(false, |rep| {
+            let auth_service = psibase::services::accounts::Wrapper::call().getAuthOf(rep);
+
+            let is_auth_action = Action {
+                service: auth_service,
+                method: auth_delegate::action_structs::isAuthSys::ACTION_NAME.into(),
+                sender: Wrapper::SERVICE,
+                rawData: auth_delegate::action_structs::isAuthSys {
+                    sender: rep,
+                    authorizers: signers,
+                    auth_set: Some(auth_set_next),
+                }
+                .packed()
+                .into(),
+            };
+
+            bool::unpacked(&transact::Wrapper::call().runAs(is_auth_action, vec![])).unwrap()
+        })
+    }
 
     #[action]
     #[allow(non_snake_case)]
-    fn canAuthUserSys(user: AccountNumber) {
+    fn canAuthUserSys(account: AccountNumber) {
         let guild = check_some(
-            psibase::services::fractals::Wrapper::call().get_guild(user),
+            psibase::services::fractals::Wrapper::call().get_guild(account),
             "account must be a guild",
         );
-        let council = psibase::services::fractals::Wrapper::call().get_g_counc(user);
+        let council = psibase::services::fractals::Wrapper::call().get_g_counc(account);
         let is_council = council.is_some_and(|council| council.len() > 0);
         let is_representative = guild.rep.is_some();
         check(
@@ -22,38 +111,14 @@ pub mod service {
     #[action]
     #[allow(non_snake_case)]
     fn checkAuthSys(
-        flags: u32,
+        _flags: u32,
         _requester: AccountNumber,
-        sender: AccountNumber,
-        action: ServiceMethod,
+        _sender: AccountNumber,
+        _action: ServiceMethod,
         _allowedActions: Vec<ServiceMethod>,
-        claims: Vec<Claim>,
+        _claims: Vec<Claim>,
     ) {
         abort_message("checkAuthsys not needed");
-    }
-
-    fn is_sufficient_actors(sender: AccountNumber, authorizers: Vec<AccountNumber>) -> bool {
-        let guild = check_some(
-            psibase::services::fractals::Wrapper::call().get_guild(sender),
-            "account must be a guild",
-        );
-        let includes_representative = guild.rep.is_some_and(|rep| authorizers.contains(&rep));
-        if includes_representative {
-            return true;
-        }
-        let council = check_some(
-            psibase::services::fractals::Wrapper::call().get_g_counc(sender),
-            "no council found",
-        );
-        let required_council_minimum: u8 = (council.len() as u8 * 2 + 2) / 3;
-        let council_approvals = authorizers.into_iter().fold(0, |acc, account| {
-            if council.contains(&account) {
-                acc + 1
-            } else {
-                acc
-            }
-        });
-        council_approvals >= required_council_minimum
     }
 
     #[action]
@@ -63,7 +128,7 @@ pub mod service {
         authorizers: Vec<AccountNumber>,
         auth_set: Option<Vec<AccountNumber>>,
     ) -> bool {
-        is_sufficient_actors(sender, authorizers)
+        check_council_or_delegate(sender, authorizers, auth_set, true)
     }
 
     #[action]
@@ -73,7 +138,7 @@ pub mod service {
         rejecters: Vec<AccountNumber>,
         auth_set: Option<Vec<AccountNumber>>,
     ) -> bool {
-        is_sufficient_actors(sender, rejecters)
+        check_council_or_delegate(sender, rejecters, auth_set, false)
     }
 }
 
