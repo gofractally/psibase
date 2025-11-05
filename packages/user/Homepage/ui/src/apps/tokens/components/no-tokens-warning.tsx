fd59ba1041259ee3fe7d0fe29cb6930008fd1f23
James Mart <jmartx93@gmail.com>
2025-11-05 15:31:17 +0000

    fix typo

--- packages/user/Homepage/ui/src/apps/tokens/components/no-tokens-warning.tsx
+++ packages/user/Homepage/ui/src/apps/tokens/components/no-tokens-warning.tsx
@@ -5,7 +5,7 @@ import { EmptyBlock } from "@shared/components/empty-block";
 export const NoTokensWarning = () => {
     return (
         <EmptyBlock
-            title="Head's up!"
+            title="Heads up!"
             description="You currently have no token balances or tokens to administer. Receive some tokens to continue."
             Icon={TriangleAlert}
             iconClass="text-yellow-500"
