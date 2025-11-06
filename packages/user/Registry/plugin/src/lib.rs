b57b3d64f21ddfe4e6de326eefd8f317a50b1696
Mike Manfredi <mike@fractally.com>
2025-11-06 21:53:21 +0000

    impl

--- packages/user/Registry/plugin/src/lib.rs
+++ packages/user/Registry/plugin/src/lib.rs
@@ -4,16 +4,40 @@ use bindings::*;
 
 mod types;
 
-use ::registry::action_structs::*;
+use crate::trust::*;
 use bindings::registry::plugin::types::AppMetadata;
 use exports::registry::plugin::developer::Guest as Developer;
 use psibase::fracpack::Pack;
+use registry::action_structs::*;
 use transact::plugin::intf as Transact;
+
+psibase::define_trust! {
+    descriptions {
+        Low => "",
+        Medium => "
+        Medium trust grants these abilities:
+            - Set app metadata in registry
+        ",
+        High => "
+        High trust grants the abilities of all lower trust levels, plus these abilities:
+            - Publish apps to registry
+            - Unpublish apps from registry
+        ",
+    }
+    functions {
+        None => [],
+        Low => [],
+        Medium => [set_metadata],
+        High => [publish, unpublish],
+    }
+}
 
 struct RegistryPlugin;
 
 impl Developer for RegistryPlugin {
     fn set_metadata(metadata: AppMetadata) {
+        assert_authorized(FunctionName::set_metadata).unwrap();
+
         Transact::add_action_to_transaction(
             setMetadata::ACTION_NAME,
             &setMetadata::from(metadata).packed(),
@@ -22,10 +46,14 @@ impl Developer for RegistryPlugin {
     }
 
     fn publish() {
+        assert_authorized(FunctionName::publish).unwrap();
+
         Transact::add_action_to_transaction(publish::ACTION_NAME, &publish {}.packed()).unwrap();
     }
 
     fn unpublish() {
+        assert_authorized(FunctionName::unpublish).unwrap();
+
         Transact::add_action_to_transaction(unpublish::ACTION_NAME, &unpublish {}.packed())
             .unwrap();
     }
