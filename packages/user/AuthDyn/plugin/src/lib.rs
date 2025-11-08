0323fbff17415977f7dc3f145ab3fefee603104d
John Williamson <johnjwilliamson@live.com>
2025-11-08 04:02:57 +0000

    rename var

--- packages/user/AuthDyn/plugin/src/lib.rs
+++ packages/user/AuthDyn/plugin/src/lib.rs
@@ -27,14 +27,16 @@ struct AuthDynPlugin;
 impl Api for AuthDynPlugin {
     fn set_policy(account: String, policy: String) -> Result<(), Error> {
         trust::assert_authorized(trust::FunctionName::set_policy)?;
-        let packed_example_thing_args = auth_dyn::action_structs::set_policy {
+
+        let packed_args = auth_dyn::action_structs::set_policy {
             account: account.as_str().into(),
             policy: policy.as_str().into(),
         }
         .packed();
+
         add_action_to_transaction(
             auth_dyn::action_structs::set_policy::ACTION_NAME,
-            &packed_example_thing_args,
+            &packed_args,
         )
     }
 }
