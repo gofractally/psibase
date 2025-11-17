25574ee497ede2cca49e75991e187bbe58ac9e69
John Williamson <johnjwilliamson@live.com>
2025-11-17 23:25:03 +0000

    add plugin functions

--- packages/user/Nft/plugin/src/errors.rs
+++
@@ -1,6 +0,0 @@
-use psibase::plugin_error;
-
-plugin_error! {
-    pub ErrorType
-    QueryResponseParseError(msg: String) => "Query response parsing error: {msg}",
-}
--- packages/user/Nft/plugin/src/lib.rs
+++ packages/user/Nft/plugin/src/lib.rs
@@ -2,57 +2,63 @@
 mod bindings;
 
 use bindings::exports::nft::plugin::api::Guest as Api;
-use bindings::exports::nft::plugin::queries::Guest as Queries;
-use bindings::host::common::server as CommonServer;
 use bindings::host::types::types::Error;
 use bindings::transact::plugin::intf::add_action_to_transaction;
 
 use psibase::define_trust;
 use psibase::fracpack::Pack;
 
-mod errors;
-use errors::ErrorType;
+use psibase::services::nft as Nft;
 
 define_trust! {
     descriptions {
         Low => "
         Low trust grants these abilities:
-            - Reading the value of the example-thing
+            - Minting
         ",
         Medium => "",
         High => "
         High trust grants the abilities of all lower trust levels, plus these abilities:
-            - Setting the example thing
+            - Crediting
+            - Burning
         ",
     }
     functions {
-        Low => [get_example_thing],
-        High => [set_example_thing],
+        Low => [mint],
+        High => [credit, burn],
     }
 }
 
 struct NftPlugin;
 
 impl Api for NftPlugin {
-    fn set_example_thing(thing: String) -> Result<(), Error> {
-        Ok(())
+    fn credit(nft_id: u32, receiver: String, memo: String) -> Result<(), Error> {
+        trust::assert_authorized(trust::FunctionName::credit)?;
+
+        let packed_args = Nft::action_structs::credit {
+            memo,
+            nftId: nft_id,
+            receiver: receiver.as_str().into(),
+        }
+        .packed();
+
+        add_action_to_transaction(Nft::action_structs::credit::ACTION_NAME, &packed_args)
     }
-}
 
-#[derive(serde::Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct ExampleThingData {
-    example_thing: String,
-}
-#[derive(serde::Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct ExampleThingResponse {
-    data: ExampleThingData,
-}
+    fn mint() -> Result<(), Error> {
+        trust::assert_authorized(trust::FunctionName::mint)?;
 
-impl Queries for NftPlugin {
-    fn get_example_thing() -> Result<String, Error> {
-        Ok("examplething_val.data.example_thing".into())
+        let packed_args = Nft::action_structs::mint {}.packed();
+
+        add_action_to_transaction(Nft::action_structs::mint::ACTION_NAME, &packed_args)
+    }
+
+    fn burn(nft_id: u32) -> Result<(), Error> {
+        trust::assert_authorized(trust::FunctionName::burn)?;
+
+        let packed_args = Nft::action_structs::burn { nftId: nft_id }.packed();
+
+        add_action_to_transaction(Nft::action_structs::burn::ACTION_NAME, &packed_args)
     }
 }
 
