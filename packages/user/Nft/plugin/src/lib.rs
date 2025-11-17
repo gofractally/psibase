d44360d9af4906b753dec2c0bc5ead8f6a36e263
John Williamson <johnjwilliamson@live.com>
2025-11-17 10:34:04 +0000

    remove queries

--- packages/user/Nft/plugin/src/lib.rs
+++ packages/user/Nft/plugin/src/lib.rs
@@ -2,7 +2,6 @@
 mod bindings;
 
 use bindings::exports::nft::plugin::api::Guest as Api;
-use bindings::exports::nft::plugin::queries::Guest as Queries;
 use bindings::host::common::server as CommonServer;
 use bindings::host::types::types::Error;
 // use bindings::transact::plugin::intf::add_action_to_transaction;
@@ -26,7 +25,7 @@ define_trust! {
         ",
     }
     functions {
-        Low => [get_example_thing],
+        Low => [],
         High => [set_example_thing],
     }
 }
@@ -36,34 +35,6 @@ struct NftPlugin;
 impl Api for NftPlugin {
     fn set_example_thing(_thing: String) -> Result<(), Error> {
         Ok(())
-    }
-}
-
-#[derive(serde::Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct ExampleThingData {
-    example_thing: String,
-}
-#[derive(serde::Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct ExampleThingResponse {
-    data: ExampleThingData,
-}
-
-impl Queries for NftPlugin {
-    fn get_example_thing() -> Result<String, Error> {
-        trust::assert_authorized(trust::FunctionName::get_example_thing)?;
-
-        let graphql_str = "query { exampleThing }";
-
-        let examplething_val = serde_json::from_str::<ExampleThingResponse>(
-            &CommonServer::post_graphql_get_json(&graphql_str)?,
-        );
-
-        let examplething_val =
-            examplething_val.map_err(|err| ErrorType::QueryResponseParseError(err.to_string()))?;
-
-        Ok(examplething_val.data.example_thing)
     }
 }
 
