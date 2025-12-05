215dda02205ef443db474641038cd803041e3a8d
Steven Watanabe <steven@clarionos.com>
2025-12-05 09:09:01 -0700

    Use the asyncio version of websockets. The threading API is newer and isn't supported on the version for python3-wwebsockets in Ubuntu 22.04. asyncio also turns out to be simpler than managing a separate thread.

--- programs/psinode/tests/test_websocket.py
+++ programs/psinode/tests/test_websocket.py
@@ -6,9 +6,7 @@ import urllib3
 import os
 import threading
 from services import XAdmin
-import websockets.sync.client
-import websockets.sync.server
-import websockets.exceptions
+import websockets
 
 def websocket_url(node, path='/', service=None):
     url = urllib3.util.parse_url(node.url)
@@ -18,44 +16,39 @@ def websocket_url(node, path='/', service=None):
         host = url.host
     return urllib3.util.Url('ws', url.auth, host, url.port, path).url
 
+async def echo(connection):
+    async for msg in connection:
+        await connection.send(msg)
+
 class TestWebSocket(unittest.TestCase):
     @testutil.psinode_test
-    def test_echo(self, cluster):
+    async def test_echo(self, cluster):
         (a,) = cluster.complete(*testutil.generate_names(1))
 
         XAdmin(a).install(os.path.join(testutil.test_packages(), "WSEcho.psi"))
         url = websocket_url(a, '/echo', service='x-echo')
 
-        with websockets.sync.client.unix_connect(a.socketpath, url) as websocket:
+        async with websockets.unix_connect(a.socketpath, url) as websocket:
             for message in ['lorem', 'ipsum', 'dolor', 'sit', 'amet']:
-                websocket.send(message)
-                self.assertEqual(websocket.recv(decode=True), message)
+                await websocket.send(message)
+                self.assertEqual(await websocket.recv(decode=True), message)
 
     @testutil.psinode_test
-    def test_proxy(self, cluster):
+    async def test_proxy(self, cluster):
         (a,) = cluster.complete(*testutil.generate_names(1))
 
         XAdmin(a).install(os.path.join(testutil.test_packages(), "XProxy.psi"))
         XAdmin(a).install(os.path.join(testutil.test_packages(), "XSocketList.psi"))
 
-        done = threading.Event()
-        def echo(connection):
-            for msg in connection:
-                connection.send(msg)
-            done.set()
-        server = websockets.sync.server.serve(echo, host='127.0.0.1', port=0)
-
-        t = threading.Thread(target=server.serve_forever)
-        t.start()
-        try:
-            with a.post('/set_origin_server', service='x-proxy', json={"host":"localhost:%d" % server.socket.getsockname()[1]}) as reply:
+        async with websockets.serve(echo, host='127.0.0.1', port=0) as server:
+            with a.post('/set_origin_server', service='x-proxy', json={"host":"localhost:%d" % server.sockets[0].getsockname()[1]}) as reply:
                 reply.raise_for_status()
 
             url = websocket_url(a, '/', service='x-proxy')
-            with websockets.sync.client.unix_connect(a.socketpath, url, compression=None) as websocket:
+            async with websockets.unix_connect(a.socketpath, url, compression=None) as websocket:
                 for message in ['lorem', 'ipsum', 'dolor', 'sit', 'amet']:
-                    websocket.send(message)
-                    self.assertEqual(websocket.recv(decode=True), message)
+                    await websocket.send(message)
+                    self.assertEqual(await websocket.recv(decode=True), message)
 
                 # There should be one unix and one http websocket
 
@@ -78,16 +71,11 @@ class TestWebSocket(unittest.TestCase):
 
             sockets = [edge['node']['info'].get('endpoint') for edge in sockets['sockets']['edges']]
             sockets = [socket for socket in sockets if socket is not None]
-            expected = ['', "127.0.0.1:%d" % server.socket.getsockname()[1]]
+            expected = ['', "127.0.0.1:%d" % server.sockets[0].getsockname()[1]]
             self.assertCountEqual(sockets, expected)
 
-        finally:
-            done.wait(timeout=10)
-            server.shutdown()
-            t.join()
-
     @testutil.psinode_test
-    def test_deflate(self, cluster):
+    async def test_deflate(self, cluster):
         '''
         This checks that the permessage-deflate extension can be requested from
         the server. If the implementation does not support this extension, it
@@ -97,29 +85,15 @@ class TestWebSocket(unittest.TestCase):
 
         XAdmin(a).install(os.path.join(testutil.test_packages(), "XProxy.psi"))
 
-        done = threading.Event()
-        def echo(connection):
-            for msg in connection:
-                connection.send(msg)
-            done.set()
-        server = websockets.sync.server.serve(echo, host='127.0.0.1', port=0)
-
-        t = threading.Thread(target=server.serve_forever)
-        t.start()
-        try:
-            with a.post('/set_origin_server', service='x-proxy', json={"host":"localhost:%d" % server.socket.getsockname()[1]}) as reply:
+        async with websockets.serve(echo, host='127.0.0.1', port=0) as server:
+            with a.post('/set_origin_server', service='x-proxy', json={"host":"localhost:%d" % server.sockets[0].getsockname()[1]}) as reply:
                 reply.raise_for_status()
 
             url = websocket_url(a, '/', service='x-proxy')
-            with websockets.sync.client.unix_connect(a.socketpath, url, compression='deflate') as websocket:
+            async with websockets.unix_connect(a.socketpath, url, compression='deflate') as websocket:
                 for message in ['lorem', 'ipsum', 'dolor', 'sit', 'amet']:
-                    websocket.send(message)
-                    self.assertEqual(websocket.recv(decode=True), message)
-
-        finally:
-            done.wait(timeout=10)
-            server.shutdown()
-            t.join()
+                    await websocket.send(message)
+                    self.assertEqual(await websocket.recv(decode=True), message)
 
 if __name__ == '__main__':
     testutil.main()
