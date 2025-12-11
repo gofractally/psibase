e8a98a65d854b850e0c8964741a106d33e8b0871
John Williamson <johnjwilliamson@live.com>
2025-12-11 11:18:51 +0000

    add reward consensus graphql connection

--- packages/user/Fractals/service/src/tables/reward_consensus.rs
+++ packages/user/Fractals/service/src/tables/reward_consensus.rs
@@ -1,3 +1,4 @@
+use async_graphql::ComplexObject;
 use std::collections::HashSet;
 
 use psibase::{abort_message, check, check_none, check_some, AccountNumber, Memo, Table};
@@ -168,6 +169,13 @@ impl RewardConsensus {
     }
 
     fn reward_stream(&self) -> RewardStream {
+        RewardStream::get_assert(self.reward_stream_id)
+    }
+}
+
+#[ComplexObject]
+impl RewardConsensus {
+    pub async fn stream(&self) -> RewardStream {
         RewardStream::get_assert(self.reward_stream_id)
     }
 }
