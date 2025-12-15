f8a06d5cb6c3fdf4391bba45765aa8251b99a81f
John Williamson <johnjwilliamson@live.com>
2025-12-15 00:29:07 +0000

    revert error code

--- packages/user/Tokens/plugin/src/errors.rs
+++ packages/user/Tokens/plugin/src/errors.rs
@@ -2,7 +2,7 @@ use psibase::plugin_error;
 
 plugin_error! {
     pub ErrorType
-    ConversionError(msg: String) = 500 => "Conversion error: {msg}",
+    ConversionError(msg: String) => "Conversion error: {msg}",
     QueryError(msg: String) => "Query error {msg}",
     TokenMismatch(msg: String) => "Token does not exist: {msg}",
 }
