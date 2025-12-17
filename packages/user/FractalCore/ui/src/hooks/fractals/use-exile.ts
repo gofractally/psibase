4502ae726d81680b6102dfa7e3433505d815cb06
John Williamson <johnjwilliamson@live.com>
2025-12-17 10:05:17 +0000

    use new plugin mutation

--- packages/user/FractalCore/ui/src/hooks/fractals/use-exile.ts
+++ packages/user/FractalCore/ui/src/hooks/fractals/use-exile.ts
@@ -3,18 +3,21 @@ import { queryClient } from "@/queryClient";
 import { fractalCorePlugin } from "@/lib/plugin";
 import QueryKey from "@/lib/queryKeys";
 
-import { usePluginMutation } from "../use-plugin-mutation";
+import { getSubDomain } from "@shared/lib/get-sub-domain";
+
+import { usePluginFunctionCallMutation } from "../use-plugin-function-call-mutation";
 
 export const useExile = () =>
-    usePluginMutation(fractalCorePlugin.adminFractal.exileMember, {
-        error: "Failed exile",
-        loading: "Exiling member",
-        success: "Exiled member",
-        isStagable: true,
-        onSuccess: ([fractal], status) => {
+    usePluginFunctionCallMutation(fractalCorePlugin.adminFractal.exileMember, {
+        toast: {
+            error: "Failed exile",
+            loading: "Exiling member",
+            success: "Exiled member",
+        },
+        onStagedTransaction: (_, status) => {
             if (status.type == "executed") {
                 queryClient.invalidateQueries({
-                    queryKey: QueryKey.members(fractal),
+                    queryKey: QueryKey.members(getSubDomain()),
                 });
             }
         },
--- packages/user/FractalCore/ui/src/hooks/fractals/use-set-guild-display-name.ts
+++ packages/user/FractalCore/ui/src/hooks/fractals/use-set-guild-display-name.ts
@@ -1,10 +1,12 @@
 import { fractalCorePlugin } from "@/lib/plugin";
 
-import { usePluginMutation } from "../use-plugin-mutation";
+import { usePluginFunctionCallMutation } from "../use-plugin-function-call-mutation";
 
 export const useSetGuildDisplayName = () =>
-    usePluginMutation(fractalCorePlugin.adminGuild.setDisplayName, {
-        error: "Failed setting display name",
-        loading: "Setting display name",
-        success: "Set display name",
+    usePluginFunctionCallMutation(fractalCorePlugin.adminGuild.setDisplayName, {
+        toast: {
+            error: "Failed setting display name",
+            loading: "Setting display name",
+            success: "Set display name",
+        },
     });
