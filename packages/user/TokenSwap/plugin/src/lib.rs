e3236df75f258be5fb677d88b4bd14a347a413db
John Williamson <johnjwilliamson@live.com>
2025-12-31 10:12:06 +0000

    use credit

--- packages/user/TokenSwap/plugin/src/lib.rs
+++ packages/user/TokenSwap/plugin/src/lib.rs
@@ -9,8 +9,9 @@ use bindings::host::types::types::Error;
 use bindings::transact::plugin::intf::add_action_to_transaction;
 
 use bindings::tokens::plugin::helpers::decimal_to_u64;
+use bindings::tokens::plugin::user::credit;
+use psibase::define_trust;
 use psibase::fracpack::Pack;
-use psibase::{define_trust, services::token_swap::action_structs::swap};
 
 mod graphql;
 
@@ -62,18 +63,18 @@ impl Api for TokenSwapPlugin {
         let token_a_amount_min: Quantity = decimal_to_u64(token_a, &amount_a_min)?.into();
         let token_b_amount_min: Quantity = decimal_to_u64(token_b, &amount_b_min)?.into();
 
-        bindings::tokens::plugin::user::credit(
+        credit(
             token_a,
             &token_swap::SERVICE.to_string(),
             &amount_a_desired,
-            "".try_into().unwrap(),
+            "",
         )?;
 
-        bindings::tokens::plugin::user::credit(
+        credit(
             token_b,
             &token_swap::SERVICE.to_string(),
             &amount_b_desired,
-            "".try_into().unwrap(),
+            "",
         )?;
 
         let packed_args = token_swap::action_structs::add_liquidity {
@@ -97,19 +98,16 @@ impl Api for TokenSwapPlugin {
         amount_in: String,
         min_return: String,
     ) -> Result<(), Error> {
-        let in_amount: Quantity = decimal_to_u64(token_in, &amount_in)?.into();
-        let min_return: Quantity = decimal_to_u64(token_in, &min_return)?.into();
-
-        bindings::tokens::plugin::user::credit(
+        credit(
             token_in,
             token_swap::SERVICE.to_string().as_str(),
             &amount_in,
-            "swap".try_into().unwrap(),
+            "swap",
         )?;
 
         let packed_args = token_swap::action_structs::swap {
-            amount_in: in_amount,
-            min_return,
+            amount_in: decimal_to_u64(token_in, &amount_in)?.into(),
+            min_return: decimal_to_u64(token_in, &min_return)?.into(),
             pools: pools
                 .into_iter()
                 .map(|pool_id| pool_id.parse::<u32>().unwrap())
@@ -127,28 +125,14 @@ impl Api for TokenSwapPlugin {
         amount_a: String,
         amount_b: String,
     ) -> Result<(), Error> {
-        let token_a_amount: Quantity = decimal_to_u64(token_a, &amount_a)?.into();
-        let token_b_amount: Quantity = decimal_to_u64(token_b, &amount_b)?.into();
-
-        bindings::tokens::plugin::user::credit(
-            token_a,
-            "token-swap",
-            &amount_a,
-            "".try_into().unwrap(),
-        )?;
-
-        bindings::tokens::plugin::user::credit(
-            token_b,
-            "token-swap",
-            &amount_b,
-            "".try_into().unwrap(),
-        )?;
+        credit(token_a, "token-swap", &amount_a, "")?;
+        credit(token_b, "token-swap", &amount_b, "")?;
 
         let packed_args = token_swap::action_structs::new_pool {
             token_a,
             token_b,
-            token_a_amount,
-            token_b_amount,
+            token_a_amount: decimal_to_u64(token_a, &amount_a)?.into(),
+            token_b_amount: decimal_to_u64(token_b, &amount_b)?.into(),
         }
         .packed();
 
