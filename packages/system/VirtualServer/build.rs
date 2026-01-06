32e37d983ae87251de688567fa00dc0d80661bef
James Mart <jmartx93@gmail.com>
2026-01-06 15:58:19 -0500

    update build script

--- packages/system/VirtualServer/build.rs
+++ packages/system/VirtualServer/build.rs
@@ -7,16 +7,12 @@ use virtual_server::rpc::Query;
 fn main() {
     println!("cargo:rerun-if-changed=service/src/rpc.rs");
     println!("cargo:rerun-if-changed=service/src/lib.rs");
+    println!("cargo:rerun-if-changed=service/src/tables.rs");
 
     let schema = Schema::new(Query { user: None }, EmptyMutation, EmptySubscription);
-
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let schema_path = PathBuf::from(&manifest_dir).join("service").join("schema.gql");
 
     fs::write(&schema_path, schema.sdl()).expect("Failed to write schema.gql");
 
-    println!(
-        "✅ Schema generated successfully at {}",
-        schema_path.display()
-    );
 }
