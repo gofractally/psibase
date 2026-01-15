fcf6702fb51f153b3cd35f956f2a3c43488b7e8d
Steven Watanabe <steven@clarionos.com>
2026-01-15 12:11:22 -0700

    Add test with multiple timers active at the same time

--- programs/psinode/tests/test_timer.py
+++ programs/psinode/tests/test_timer.py
@@ -4,6 +4,7 @@ import testutil
 import unittest
 import os
 import time
+import threading
 from services import XAdmin
 
 class TestTimer(unittest.TestCase):
@@ -26,6 +27,24 @@ class TestTimer(unittest.TestCase):
             after = time.monotonic_ns()
             print("elapsed time: %g" % ((after - before)/1e9))
             self.assertGreaterEqual(after - before, 1000000000)
+
+        numThreads = 3
+        results = [0] * numThreads
+        threads = []
+        for i in range(numThreads):
+            def run(i):
+                start = time.monotonic_ns()
+                with a.get('/wait?seconds=%d' % (i + 1), service='x-delay') as reply:
+                    reply.raise_for_status()
+                    results[i] = time.monotonic_ns() - start
+            threads.append(threading.Thread(target=run, args=(i,)))
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+        for i in range(3):
+            print("elapsed time: %g" % (results[i]/1e9))
+            self.assertGreaterEqual(results[i], (i + 1) * 1000000000)
 
 if __name__ == '__main__':
     testutil.main()
