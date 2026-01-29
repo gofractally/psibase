a9ebe638c7e6ce5f8b2118f19796de9c3a20005b
John Williamson <johnjwilliamson@live.com>
2026-01-29 00:21:34 +0000

    remove unused error

--- packages/user/TokenSwap/plugin/src/errors.rs
+++ packages/user/TokenSwap/plugin/src/errors.rs
@@ -7,7 +7,6 @@ plugin_error! {
     QueryError(msg: String) => "Query error: {msg}",
     InvalidFormat(msg: String) => "Invalid format: {msg}",
     SlippageTooHigh(slippage: u32) => "Slippage must be in ppm format: received {slippage}, must be between 0 - 1,000,000",
-    NoPathFound => "Failed to find pools to fulfil trade of these tokens",
     InsufficientPools => "Pools vector is empty",
     InsufficientLiquidity => "Insufficient liquidity to perform trade",
     DesiredHigherThanReserve => "Desired amount is higher than reserve balance"
