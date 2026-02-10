488837e76ea4801fd92e0a08ad11fa55946abb8e
Brandon Fancher <brandonfancher@gmail.com>
2026-02-10 21:05:18 +0000

    typo fix

--- packages/user/FractalCore/ui/src/pages/leadership/index.tsx
+++ packages/user/FractalCore/ui/src/pages/leadership/index.tsx
@@ -1,3 +1,8 @@
+import { useBoolean } from "usehooks-ts";
+
+import { RegisterCandidacyModal } from "@/components/modals/register-candidady-modal";
+
+import { Button } from "@shared/shadcn/ui/button";
 import {
     Item,
     ItemActions,
@@ -5,23 +10,25 @@ import {
     ItemDescription,
     ItemTitle,
 } from "@shared/shadcn/ui/item";
-import { RegisterCandidacyModal } from "@/components/modals/register-candidady-modal";
-import { useBoolean } from "usehooks-ts";
-
-import { Button } from "@shared/shadcn/ui/button";
 
 export const Leadership = () => {
-    const { setTrue, value, setValue } = useBoolean()
+    const { setTrue, value, setValue } = useBoolean();
 
     return (
         <div className="mx-auto w-full max-w-5xl p-4 px-6">
-            <RegisterCandidacyModal openChange={(value) => { setValue(value) }} show={value} />
+            <RegisterCandidacyModal
+                openChange={(value) => {
+                    setValue(value);
+                }}
+                show={value}
+            />
             <div className="flex flex-col gap-3">
                 <Item variant="outline">
                     <ItemContent>
-                        <ItemTitle>Register cadidacy</ItemTitle>
+                        <ItemTitle>Register candidacy</ItemTitle>
                         <ItemDescription>
-                            Toggle your intention to sit on the council of the fractal.
+                            Toggle your intention to sit on the council of the
+                            fractal.
                         </ItemDescription>
                     </ItemContent>
                     <ItemActions>
