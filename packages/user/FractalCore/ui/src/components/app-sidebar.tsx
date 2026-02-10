ddacefb8e2c6730d8fe4bbe3277bae76e0c66d20
Brandon Fancher <brandonfancher@gmail.com>
2026-02-10 18:20:21 +0000

    Adjust padding and sidebar header

--- packages/user/FractalCore/ui/src/components/app-sidebar.tsx
+++ packages/user/FractalCore/ui/src/components/app-sidebar.tsx
@@ -1,22 +1,40 @@
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
 
-import { GuildSwitcher } from "@/components/guild-switcher";
+// import { GuildSwitcher } from "@/components/guild-switcher";
 import { NavMain } from "@/components/nav-main";
 import { NavUser } from "@/components/nav-user";
 
+import { NetworkLogo } from "@shared/components/network-logo";
 import {
     Sidebar,
     SidebarContent,
     SidebarFooter,
     SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
     SidebarRail,
 } from "@shared/shadcn/ui/sidebar";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    const navigate = useNavigate();
+
     return (
-        <Sidebar collapsible="icon" {...props}>
+        <Sidebar variant="inset" collapsible="icon" {...props}>
             <SidebarHeader>
-                <GuildSwitcher />
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton
+                            size="lg"
+                            asChild
+                            onClick={() => navigate("/")}
+                        >
+                            <NetworkLogo />
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+                {/* <GuildSwitcher /> */}
             </SidebarHeader>
             <SidebarContent>
                 <NavMain />
