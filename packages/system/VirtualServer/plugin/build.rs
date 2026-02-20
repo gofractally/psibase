86c0aad28edd082f47e359da1efcd9fe40c8a5be
James Mart <jmartx93@gmail.com>
2026-02-20 19:48:49 +0000

    fix build order for vserver gql schema

--- packages/system/VirtualServer/plugin/build.rs
+++ packages/system/VirtualServer/plugin/build.rs
@@ -5,14 +5,16 @@ use std::path::PathBuf;
 use virtual_server::rpc::Query;
 
 fn main() {
-    println!("cargo:rerun-if-changed=service/src/rpc.rs");
-    println!("cargo:rerun-if-changed=service/src/lib.rs");
-    println!("cargo:rerun-if-changed=service/src/tables.rs");
+    println!("cargo:rerun-if-changed=../service/src/rpc.rs");
+    println!("cargo:rerun-if-changed=../service/src/lib.rs");
+    println!("cargo:rerun-if-changed=../service/src/tables.rs");
 
     let schema = Schema::new(Query { user: None }, EmptyMutation, EmptySubscription);
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let schema_path = PathBuf::from(&manifest_dir).join("service").join("schema.gql");
+    let schema_path = PathBuf::from(&manifest_dir)
+        .join("..")
+        .join("service")
+        .join("schema.gql");
 
     fs::write(&schema_path, schema.sdl()).expect("Failed to write schema.gql");
-
 }
