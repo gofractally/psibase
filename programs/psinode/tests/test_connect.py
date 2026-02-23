742ca7f66fc2bae220d8fa484d6f11dababd415f
Steven Watanabe <steven@clarionos.com>
2026-02-23 14:05:55 -0700

    Add test using a token for p2p

--- programs/psinode/tests/test_connect.py
+++ programs/psinode/tests/test_connect.py
@@ -3,9 +3,11 @@
 
 from psinode import Cluster
 from predicates import *
+from services import Transact
 import testutil
 import time
 import unittest
+from requests.exceptions import HTTPError
 
 class TestConnect(unittest.TestCase):
     @testutil.psinode_test
@@ -26,6 +28,27 @@ class TestConnect(unittest.TestCase):
             return fn
         a.wait(one_connection(b))
         b.wait(one_connection(a))
+
+    @testutil.psinode_test
+    def test_auth(self, cluster):
+        a = cluster.make_node('a', p2p=False)
+        b = cluster.make_node('b', p2p=False)
+
+        with self.assertRaises(HTTPError):
+            b.connect(a)
+
+        a.boot(packages=['Minimal', 'Explorer'])
+
+        with a.post('/users', service='x-peers', json={"account":"a","accept":True}) as reply:
+            reply.raise_for_status()
+        # For now, p2p always uses the hostname psibase.localhost for local sockets.
+        # This means the token also needs to use psibase.localhost.
+        a.url = "http://psibase.localhost/"
+        token = Transact(a).login('a')
+        with b.post('/authorization', service='x-peers', json={"url": a.socketpath, "token": token}) as reply:
+            reply.raise_for_status()
+
+        b.connect(a)
 
 if __name__ == '__main__':
     testutil.main()
