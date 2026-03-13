c8f1cc940700b04ef6afc3ef02c4fe4d0f3cdbb7
Steven Watanabe <steven@clarionos.com>
2026-03-13 12:47:49 -0600

    Remove unused include

--- packages/local/XSites/test/XSites-test.cpp
+++ packages/local/XSites/test/XSites-test.cpp
@@ -4,7 +4,6 @@
 #include <services/system/HttpServer.hpp>
 
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_templated.hpp>
 
 using namespace psibase;
 using namespace LocalService;
