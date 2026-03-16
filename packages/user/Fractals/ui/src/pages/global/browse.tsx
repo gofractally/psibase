d844020ea4f1742e6751e4d449a15be335307f6a
Brandon Fancher <brandonfancher@gmail.com>
2026-03-16 19:48:16 +0000

    Fractals app layout updates

--- packages/user/Fractals/ui/src/pages/global/browse.tsx
+++ packages/user/Fractals/ui/src/pages/global/browse.tsx
@@ -3,12 +3,16 @@ import { useNavigate } from "react-router-dom";
 
 import { CreateFractalModal } from "@/components/create-fractal-modal";
 import { EmptyBlock } from "@/components/empty-block";
+import { PageContainer } from "@/components/page-container";
 
 import { useFractals } from "@/hooks/fractals/use-fractals";
 
+import { GlowingCard } from "@shared/components/glowing-card";
+import { CardContent, CardHeader, CardTitle } from "@shared/shadcn/ui/card";
 import {
     Table,
     TableBody,
+    TableCaption,
     TableCell,
     TableHead,
     TableHeader,
@@ -23,52 +27,57 @@ export const Browse = () => {
     const [showModal, setShowModal] = useState(false);
 
     return (
-        <div className="mx-auto w-full max-w-5xl p-4 px-6">
-            <div className="flex h-9 items-center">
-                <h1 className="text-lg font-semibold">Fractals</h1>
-            </div>
-            <div className="mt-3">
-                {fractals && fractals.length > 0 && (
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                <TableHead>Fractal</TableHead>
-                                <TableHead>Mission</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {fractals?.map((fractal) => (
-                                <TableRow
-                                    key={fractal.account}
-                                    onClick={() => {
-                                        navigate(`/fractal/${fractal.account}`);
-                                    }}
-                                    className="cursor-pointer"
-                                >
-                                    <TableCell className="font-medium">
-                                        {fractal.name || fractal.account}
-                                    </TableCell>
-                                    <TableCell>{fractal.mission}</TableCell>
+        <PageContainer>
+            <GlowingCard>
+                <CardHeader>
+                    <CardTitle>Fractals</CardTitle>
+                </CardHeader>
+                <CardContent className="@container">
+                    {fractals && fractals.length > 0 && (
+                        <Table>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>Fractal</TableHead>
+                                    <TableHead>Mission</TableHead>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                )}
-                {fractals?.length == 0 && (
-                    <EmptyBlock
-                        title="No fractals"
-                        description="No fractals have been created yet"
-                        buttonLabel="Create the first fractal"
-                        onButtonClick={() => {
-                            setShowModal(true);
-                        }}
-                    />
-                )}
-                <CreateFractalModal
-                    openChange={(e) => setShowModal(e)}
-                    show={showModal}
+                            </TableHeader>
+                            <TableBody>
+                                {fractals?.map((fractal) => (
+                                    <TableRow
+                                        key={fractal.account}
+                                        onClick={() => {
+                                            navigate(
+                                                `/fractal/${fractal.account}`,
+                                            );
+                                        }}
+                                        className="cursor-pointer"
+                                    >
+                                        <TableCell className="font-medium">
+                                            {fractal.name || fractal.account}
+                                        </TableCell>
+                                        <TableCell>{fractal.mission}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                            <TableCaption>A list of all fractals.</TableCaption>
+                        </Table>
+                    )}
+                </CardContent>
+            </GlowingCard>
+            {fractals?.length == 0 && (
+                <EmptyBlock
+                    title="No fractals"
+                    description="No fractals have been created yet"
+                    buttonLabel="Create the first fractal"
+                    onButtonClick={() => {
+                        setShowModal(true);
+                    }}
                 />
-            </div>
-        </div>
+            )}
+            <CreateFractalModal
+                openChange={(e) => setShowModal(e)}
+                show={showModal}
+            />
+        </PageContainer>
     );
 };
