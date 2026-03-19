8790f0d8cad64cef8ef3ecef4ab0a60e54a844ca
Brandon Fancher <brandonfancher@gmail.com>
2026-03-19 20:00:44 +0000

    Hoist useConnectAccount enhancements and remove duplicate hook at app level

--- packages/shared-ui/hooks/use-connect-account.ts
+++ packages/shared-ui/hooks/use-connect-account.ts
@@ -1,19 +1,27 @@
 import { type UseMutationOptions, useMutation } from "@tanstack/react-query";
 
+import { AutoRedirectConfig } from "@psibase/common-lib";
+
 import { supervisor } from "@shared/lib/supervisor";
 
 export const useConnectAccount = (
-    options: UseMutationOptions<void, Error> = {},
-) => {
-    return useMutation({
-        mutationFn: async () => {
-            await supervisor.functionCall({
-                service: "accounts",
-                intf: "activeApp",
-                method: "connectAccount",
-                params: [],
-            });
+    options: UseMutationOptions<
+        void,
+        Error,
+        AutoRedirectConfig | undefined
+    > = {},
+) =>
+    useMutation({
+        mutationFn: async (autoRedirectConfig?) => {
+            await supervisor.functionCall(
+                {
+                    service: "accounts",
+                    intf: "activeApp",
+                    method: "connectAccount",
+                    params: [],
+                },
+                autoRedirectConfig,
+            );
         },
         ...options,
     });
-};
