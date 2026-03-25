04d3e401f6fa812850dd3ab9e1586449b8657ded
Brandon Fancher <brandonfancher@gmail.com>
2026-03-25 17:37:26 +0000

    Update Identity and Packages

--- packages/user/Identity/ui/src/main.tsx
+++ packages/user/Identity/ui/src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "@shared/styles/globals.css";
+
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
