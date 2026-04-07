239c2085b7bd1b279a35fd94c944fe239d0ce534
Brandon Fancher <brandonfancher@gmail.com>
2026-04-07 18:21:22 +0000

    Config UI kebab case

--- packages/user/Config/ui/src/hooks/use-set-network-variables.tsx
+++ packages/user/Config/ui/src/hooks/use-set-network-variables.tsx
@@ -1,6 +1,6 @@
 import type { VirtualServerResources } from "./use-virtual-server-resources";
 
-import QueryKey from "@/lib/queryKeys";
+import QueryKey from "@/lib/query-keys";
 import { CONFIG } from "@/lib/services";
 
 import { queryClient } from "@shared/lib/query-client";
