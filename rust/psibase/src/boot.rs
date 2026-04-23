0836b8990075dfe004ceb6a3c0e75130237427f8
Steven Watanabe <steven@fractally.com>
2026-04-23 09:31:14 -0600

    Add diagnositics

--- rust/psibase/src/boot.rs
+++ rust/psibase/src/boot.rs
@@ -51,6 +51,7 @@ fn genesis_transaction<R: Read + Seek>(
     let mut essential = EssentialServices::new();
     for s in service_packages {
         s.get_genesis(&mut services)?;
+        println!("genesis package: {}", s.name());
         // Only install the transact service and its dependencies
         essential.remove(s.get_accounts());
         if essential.is_empty() {
