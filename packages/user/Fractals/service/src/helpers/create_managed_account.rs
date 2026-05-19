d799169b0429512cec622065797fa73ac97a5ea3
John Williamson <johnjwilliamson@live.com>
2026-05-19 06:49:54 +0000

    drop use of auth-any

--- packages/user/Fractals/service/src/helpers/create_managed_account.rs
+++ packages/user/Fractals/service/src/helpers/create_managed_account.rs
@@ -1,14 +1,15 @@
-use psibase::services::{accounts, auth_dyn};
+use psibase::services::{accounts, auth_delegate, auth_dyn};
 use psibase::AccountNumber;
 
 pub fn create_managed_account<F>(new_account: AccountNumber, f: F)
 where
     F: Fn(),
 {
-    accounts::Wrapper::call().newAccount(new_account, "auth-any".into(), true);
+    let self_service = crate::Wrapper::SERVICE;
+    auth_delegate::Wrapper::call().newAccount(new_account, self_service, true);
 
     f();
 
-    auth_dyn::Wrapper::call_as(new_account).set_mgmt(new_account, crate::Wrapper::SERVICE);
+    auth_dyn::Wrapper::call_as(new_account).set_mgmt(new_account, self_service);
     accounts::Wrapper::call_as(new_account).setAuthServ(auth_dyn::Wrapper::SERVICE)
 }
