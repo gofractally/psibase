251968428bef0ced1be881352015b48f99c35b6d
Brandon Fancher <brandonfancher@gmail.com>
2026-05-27 17:02:29 +0000

    Add tokenDecimalToBigInt function and helpers intf to TS token plugin class

--- packages/shared-ui/lib/plugins/tokens.ts
+++ packages/shared-ui/lib/plugins/tokens.ts
@@ -28,18 +28,35 @@ class User extends PluginInterface {
     }
 }
 
+class Helpers extends PluginInterface {
+    protected override readonly _intf = "helpers" as const;
+
+    get decimalToU64() {
+        return this._call<
+            [tokenId: TID, amount: Decimal],
+            number | string | bigint
+        >("decimalToU64");
+    }
+}
+
 export class Plugin {
     readonly issuer: Issuer;
     readonly user: User;
+    readonly helpers: Helpers;
 
     constructor(readonly service: Account) {
         // Initialize all interfaces with the correct service
         this.issuer = new Issuer();
         this.user = new User();
+        this.helpers = new Helpers();
 
         // Set the protected _service on each instance
         // This avoids the "used before initialization" error
-        const instances = [this.issuer, this.user] as PluginInterface[];
+        const instances = [
+            this.issuer,
+            this.user,
+            this.helpers,
+        ] as PluginInterface[];
 
         for (const instance of instances) {
             Object.assign(instance, { _service: service });
