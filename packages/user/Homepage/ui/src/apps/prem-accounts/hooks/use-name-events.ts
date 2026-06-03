a5666f930c9ef37829f15d61f3ef18c0e8da5665
Brandon Fancher <brandonfancher@gmail.com>
2026-06-03 18:59:08 +0000

    Homepage: Skip expensive nameEvents check if premAccounts market is enabled

--- packages/user/Homepage/ui/src/apps/prem-accounts/hooks/use-name-events.ts
+++ packages/user/Homepage/ui/src/apps/prem-accounts/hooks/use-name-events.ts
@@ -1,13 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { fetchAllNameEvents } from "@/apps/prem-accounts/lib/graphql/prem-accounts-api";
+
 import QueryKey from "@/lib/query-keys";
 
 import { Account } from "@shared/lib/schemas/account";
 
-export const useNameEvents = (user?: Account | null) =>
+export const useNameEvents = (
+    user?: Account | null,
+    { enabled }: { enabled: boolean } = { enabled: true },
+) =>
     useQuery({
         queryKey: QueryKey.premNameEvents(user),
         queryFn: () => fetchAllNameEvents(user!),
-        enabled: !!user,
+        enabled: !!user && enabled,
     });
