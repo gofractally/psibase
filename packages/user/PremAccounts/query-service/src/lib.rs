6d0f328dc5074828d7923ba27258f0ab08592d04
Steven Watanabe <steven@fractally.com>
2026-06-11 08:17:56 -0600

    Use account constant

--- packages/user/PremAccounts/query-service/src/lib.rs
+++ packages/user/PremAccounts/query-service/src/lib.rs
@@ -202,7 +202,7 @@ mod service {
         user: Option<AccountNumber>,
     ) -> Option<HttpReply> {
         check(
-            get_sender() == AccountNumber::from("http-server"),
+            get_sender() == services::http_server::SERVICE,
             "permission denied: serveSys only callable by 'http-server'",
         );
 
