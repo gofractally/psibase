e6571e64b4eb84ad8b0f118cef7eda8981711154
Steven Watanabe <steven@fractally.com>
2026-06-23 09:50:02 -0600

    Use wasm events from rust

--- rust/psibase/src/services/events.rs
+++ rust/psibase/src/services/events.rs
@@ -3,8 +3,16 @@
 mod service {
     use crate::{AccountNumber, DbId, MethodNumber};
 
+    pub type EventDb = DbId;
+
     #[action]
     fn addIndex(db_id: DbId, service: AccountNumber, event: MethodNumber, column: u8) {
+        unimplemented!()
+    }
+
+    /// Writes an event to the event log
+    #[action]
+    fn event(db: EventDb, type_: MethodNumber, rawData: Vec<u8>) -> u64 {
         unimplemented!()
     }
 
