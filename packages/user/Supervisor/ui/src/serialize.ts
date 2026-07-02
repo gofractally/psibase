c868675c58d9f0498f77c689bc205505b894775c
James Mart <jmartx93@gmail.com>
2026-07-02 19:56:07 +0000

    run serialize fn regardless of prior lock rejection

--- packages/user/Supervisor/ui/src/serialize.ts
+++ packages/user/Supervisor/ui/src/serialize.ts
@@ -3,7 +3,7 @@
 let queue: Promise<unknown> = Promise.resolve();
 
 export function serialize<T>(fn: () => Promise<T>): Promise<T> {
-    const res = queue.then(() => fn());
+    const res = queue.catch(() => {}).then(() => fn());
     queue = res.catch(() => {});
     return res;
 }
