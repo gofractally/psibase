e798217eeaf0f07edc87ef790c96b9f67b3cac41
Steven Watanabe <steven@fractally.com>
2026-07-28 14:07:06 -0600

    Add test with both incoming and outgoing requests

--- programs/psinode/tests/test_exception_exit.py
+++ programs/psinode/tests/test_exception_exit.py
@@ -4,18 +4,22 @@ from predicates import *
 import testutil
 import unittest
 import os
+import time
 from fracpack import *
 from psibase import *
-from threading import Thread
+from threading import Thread, Semaphore
 from requests import ConnectionError
+from http.server import *
 
 # Since this test stops psinode with an error, we want
 # sanitizer errors to cause a signal instead to fail the test.
 ENV = {'ASAN_OPTIONS': 'abort_on_error=1'}
 
+expected = "Lorem ipsum dolor sit amet"
+
 class TestExceptionExit(unittest.TestCase):
     @testutil.psinode_test
-    def test_socket(self, cluster):
+    def test_incoming_request(self, cluster):
         (a,) = cluster.complete('a', env=ENV)
         a.boot(packages=['Minimal', 'Explorer'])
         a.install(packages=['KeepSocket'], sources=[testutil.test_packages()])
@@ -31,6 +35,47 @@ class TestExceptionExit(unittest.TestCase):
         t.start()
         self.cause_exception(a)
         t.join()
+
+    @testutil.psinode_test
+    def test_proxy_request(self, cluster):
+        (a,) = cluster.complete('a', env=ENV)
+        a.boot(packages=['Minimal', 'Explorer'])
+        a.install_local(['XProxy'])
+
+        sem = Semaphore(0)
+        class RequestHandler(BaseHTTPRequestHandler):
+            def do_GET(self):
+                sem.acquire()
+                body = expected.encode('utf-8')
+                self.send_response(200)
+                self.send_header('Content-Type', 'text/plain')
+                self.send_header('Content-Length', str(len(body)))
+                self.end_headers()
+                self.wfile.write(body)
+
+        server = HTTPServer(('', 0), RequestHandler)
+        t = Thread(target=server.serve_forever)
+        t.start()
+        try:
+            with a.post('/set_origin_server', service='x-proxy', json={"subdomain":"x-proxy", "host":"localhost:%d" % server.server_address[1]}) as reply:
+                reply.raise_for_status()
+
+            def long_query(api):
+                try:
+                    api.get('/', service='x-proxy')
+                except ConnectionError:
+                    pass
+
+            t2 = Thread(target=long_query, args=(a.new_api(),))
+            t2.start()
+            time.sleep(0.5)
+            self.cause_exception(a)
+            t2.join()
+            sem.release()
+        finally:
+            server.shutdown()
+            t.join()
+            server.server_close()
 
     @testutil.psinode_test
     def test_incoming_p2p(self, cluster):
