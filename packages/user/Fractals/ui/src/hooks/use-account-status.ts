313d94b494abb6ab43932f2be4ff43b0299987e4
Michael Manfredi <mike@fractally.com>
2026-08-18 16:53:28 -0500

    decycle the next thing

--- packages/user/Fractals/ui/src/hooks/use-account-status.ts
+++ packages/user/Fractals/ui/src/hooks/use-account-status.ts
@@ -23,7 +23,7 @@ export const isAccountAvailable = async (
                 method: "getAccount",
                 params: [accountName],
                 service: "accounts",
-                plugin: "query",
+                plugin: "chain-query",
                 intf: "api",
             }),
         );
--- packages/user/Permissions/plugin/src/lib.rs
+++ packages/user/Permissions/plugin/src/lib.rs
@@ -6,7 +6,7 @@ use types::*;
 
 use bindings::*;
 
-use accounts::query::api as Accounts;
+use accounts::client_query::api as Accounts;
 use exports::permissions::plugin::{
     admin::{ApprovalDuration, Guest as PermsAdmin, PromptContext},
     api::Guest as Api,
