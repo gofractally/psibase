30b68e9acd1378abf55cb399728b0289f0676c01
John Williamson <johnjwilliamson@live.com>
2026-08-25 13:17:07 +1000

    add full stop

--- packages/local/XAdmin/ui/src/components/app-sidebar.tsx
+++ packages/local/XAdmin/ui/src/components/app-sidebar.tsx
@@ -79,7 +79,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                     </SidebarMenu>
                 </SidebarGroup>
                 <SidebarGroup>
-                    <SidebarGroupLabel>Network Admin</SidebarGroupLabel>
+                    <SidebarGroupLabel>Network Admin.</SidebarGroupLabel>
                     <SidebarMenu>
                         <SidebarMenuItem>
                             <Link
